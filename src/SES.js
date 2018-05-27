1fa6331567ff51d46300504d75c772448ea5d2e5
Brian Warner <warner@lothar.com>
2018-05-27 16:06:28 -0700

    dummy startSES function, test to load it

---
+++ src/SES.js
@@ -0,0 +1,3 @@
+function startSES() {
+}
+exports.startSES = startSES;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var test = require('tape');
 var Realm = require('../proposal-realms/shim/src/realm.js').default;
+var SES = require('../index.js');
 
 test('hello', function(t) {
   t.plan(2); // need t.plan or t.end, but both is ok too
@@ -8,9 +9,24 @@ test('hello', function(t) {
   t.end();
 });
 
-test('load', function(t) {
+test('realm smoketest', function(t) {
   const r = new Realm();
   let o = r.evaluate('123+4');
   t.equal(o, 127);
+
+  var captured = 0;
+  r.global.other = function(a, b) { captured = a; return a+b; };
+  o = r.evaluate('other(1,2)');
+  t.equal(o, 3);
+  t.equal(captured, 1);
+
   t.end();
 });
+
+test('activate', function(t) {
+  const r = new Realm();
+  r.global.startSES = SES.startSES;
+  r.evaluate('startSES()');
+  t.end();
+});
+
