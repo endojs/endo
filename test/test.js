bd1f0fddd56c13d13f6ca8c4a5ff8e6946e1a981
Brian Warner <warner@lothar.com>
2018-05-27 15:39:45 -0700

    make sure Realm can be imported, basic smoke test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,16 @@
-function test() {
-  return 1;
-}
+var test = require('tape');
+var Realm = require('../proposal-realms/shim/src/realm.js').default;
 
-test();
+test('hello', function(t) {
+  t.plan(2); // need t.plan or t.end, but both is ok too
+  t.equal(1, 1);
+  t.equal(1+1, 2);
+  t.end();
+});
 
+test('load', function(t) {
+  const r = new Realm();
+  let o = r.evaluate('123+4');
+  t.equal(o, 127);
+  t.end();
+});
