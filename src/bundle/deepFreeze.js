f3130ae07203a4bdfd8429bd6f37e2563c716e93
Brian Warner <warner@lothar.com>
2018-07-19 12:39:55 -0700

    update/amend copyright headers, cite Apache-2.0 in each file

--- src/bundle/deepFreeze.js
+++ src/bundle/deepFreeze.js
@@ -1,7 +1,21 @@
 // Adapted from SES/Caja - Copyright (C) 2011 Google Inc.
+// Copyright (C) 2018 Agoric
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// based upon:
 // https://github.com/google/caja/blob/master/src/com/google/caja/ses/startSES.js
 // https://github.com/google/caja/blob/master/src/com/google/caja/ses/repairES5.js
-
 // then copied from proposal-frozen-realms deep-freeze.js
 
 function deepFreeze(primordialRoots) {
--- src/bundle/removeProperties.js
+++ src/bundle/removeProperties.js
@@ -1,4 +1,5 @@
 // Copyright (C) 2011 Google Inc.
+// Copyright (C) 2018 Agoric
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
