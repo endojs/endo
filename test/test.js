6bec541fcc9b5e6c03a649076e5e0e897335261a
Kate Sills <katelynsills@gmail.com>
2019-02-06 17:42:59 -0800

    Install tape, add basic tests from the SES repo, switch to CommonJS modules temporarily

---
+++ test/test.js
@@ -0,0 +1,14 @@
+const test = require('tape');
+const { Nat } = require('../index.js');
+
+test('Nat() throws when not a natural number', function (t) {
+  t.equal(Nat(0), 0);
+  t.equal(Nat(1), 1);
+  t.equal(Nat(999), 999);
+  t.throws(() => Nat("not a number"), RangeError);
+  t.throws(() => Nat(-1), RangeError);
+  t.throws(() => Nat(0.5), RangeError);
+  t.throws(() => Nat(2**60), RangeError);
+  t.throws(() => Nat(NaN), RangeError);
+  t.end();
+});
