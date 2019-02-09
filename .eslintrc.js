1c44efb4963dee9782a59fc48958f7975dc2de63
Kate Sills <katelynsills@gmail.com>
2019-02-08 17:36:13 -0800

    install eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'airbnb',
+  env: {
+    es6: true,
+    mocha: true,
+  },
+  rules: {
+    'implicit-arrow-linebreak': 'off',
+    'function-paren-newline': 'off',
+    'arrow-parens': 'off',
+  },
+};
