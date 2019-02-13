49d1cc04c98557bd09c5710c44130102adde4d76
Brian Warner <warner@lothar.com>
2019-02-13 14:43:32 -0800

    initial implementation. doesn't work.
    Since MakeHardener isn't published yet, this source tree must live next to a
    copy of the MakeHardener source (it imports `../../MakeHardener/index.js`).
    
    Also, the tests fail. We aren't getting the right things (prototypes) into
    the initialRoots table.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Adapted from SES/Caja - Copyright (C) 2011 Google Inc.
+// Copyright (C) 2018 Agoric
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//const makeHardener = require('@agoric/make-hardener');
+const makeHardener = require('../../MakeHardener/index.js').makeHardener;
+const buildTable = require('./buildTable');
+
+// this use of 'global' is why Harden is a "resource module", whereas
+// MakeHardener is "pure".
+const initialRoots = buildTable(global);
+console.log("initialRoots are", initialRoots);
+
+const harden = makeHardener(initialRoots);
+
+module.exports = { harden };
---
+++ test/test.js
@@ -0,0 +1,71 @@
+const test = require('tape');
+const { harden } = require('../src/index.js');
+
+test('harden', t => {
+  const o = { a: {} };
+  t.equal(harden(o), o);
+  t.ok(Object.isFrozen(o));
+  t.ok(Object.isFrozen(o.a));
+  t.end();
+});
+
+test.skip('do not freeze roots', t => {
+  const parent = { I_AM_YOUR: 'parent' };
+  const h = makeHardener(parent, Object.prototype);
+  const o = { a: {} };
+  Object.setPrototypeOf(o, parent);
+  t.equal(h(o), o);
+  t.ok(Object.isFrozen(o));
+  t.ok(Object.isFrozen(o.a));
+  t.notOk(Object.isFrozen(parent));
+  t.end();
+});
+
+test.skip('complain about prototype not in roots', t => {
+  const parent = { I_AM_YOUR: 'parent' };
+  // at least one prototype is missing in each hardener
+  const h1 = makeHardener(Object.prototype);
+  const h2 = makeHardener(parent);
+  const o = { a: {} };
+  Object.setPrototypeOf(o, parent);
+  t.throws(() => h1(o), TypeError);
+  t.throws(() => h2(o), TypeError);
+  // if harden() throws TypeError, we make no claims about what properties
+  // got frozen. However, we know for sure that the prototype shouldn't be
+  // frozen: this is what saves us from the "ice-9" freeze-the-world scenario
+  t.notOk(Object.isFrozen(parent));
+  t.end();
+});
+
+test.skip('harden the same thing twice', t => {
+  const h = makeHardener(Object.prototype);
+  const o = { a: {} };
+  t.equal(h(o), o);
+  t.equal(h(o), o);
+  t.ok(Object.isFrozen(o));
+  t.ok(Object.isFrozen(o.a));
+  t.end();
+});
+
+test.skip('harden objects with cycles', t => {
+  const h = makeHardener(Object.prototype);
+  const o = { a: {} };
+  o.a.foo = o;
+  t.equal(h(o), o);
+  t.ok(Object.isFrozen(o));
+  t.ok(Object.isFrozen(o.a));
+  t.end();
+});
+
+test.skip('harden overlapping objects', t => {
+  const h = makeHardener(Object.prototype);
+  const o1 = { a: {} };
+  const o2 = { a: o1.a };
+  t.equal(h(o1), o1);
+  t.ok(Object.isFrozen(o1));
+  t.ok(Object.isFrozen(o1.a));
+  t.notOk(Object.isFrozen(o2));
+  t.equal(h(o2), o2);
+  t.ok(Object.isFrozen(o2));
+  t.end();
+});
