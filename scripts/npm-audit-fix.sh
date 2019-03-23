c5e4548f0760bfcd86bcb7ec7b3f0c1b50df7e36
Kate Sills <katelynsills@gmail.com>
2019-03-22 18:51:09 -0700

    Add CircleCI job to automatically submit PR if npm audit fix changes (#82)

---
+++ scripts/npm-audit-fix.sh
@@ -0,0 +1,25 @@
+git clone https://github.com/AgoricBot/SES.git
+cd SES
+git remote add upstream https://github.com/Agoric/SES.git
+git fetch upstream
+git checkout master
+git rebase upstream/master
+git config user.email "kate+agoricbot@agoric.com"
+git config user.name "AgoricBot"
+hub push origin master
+git checkout -b npm-audit-fix
+
+if npm audit ; then
+    echo "Nothing to fix"
+else
+  npm audit fix
+  files_changed=true
+fi
+
+if [ "$files_changed" = true ] ; then
+  git add . 
+  git commit -m "results of running npm audit fix"
+  git remote set-url origin https://AgoricBot:$GITHUB_TOKEN@github.com/AgoricBot/SES.git
+  git push origin npm-audit-fix
+  hub pull-request --no-edit --base Agoric/SES:master
+fi
