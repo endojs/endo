f9c3f1c8f8c3e7dc6fe44e879a25b1ca4651068b
Kate Sills <katelynsills@gmail.com>
2019-03-25 17:44:31 -0700

    make sure that the AgoricBot fork is up to date before doing npm audit fix (#84)

--- scripts/npm-audit-fix.sh
+++ scripts/npm-audit-fix.sh
@@ -1,12 +1,19 @@
 git clone https://github.com/AgoricBot/SES.git
 cd SES
 git remote add upstream https://github.com/Agoric/SES.git
+git remote set-url origin https://AgoricBot:$GITHUB_TOKEN@github.com/AgoricBot/SES.git
 git fetch upstream
 git checkout master
 git rebase upstream/master
 git config user.email "kate+agoricbot@agoric.com"
 git config user.name "AgoricBot"
+git config --global hub.protocol https
 hub push origin master
+
+if git ls-remote --heads --exit-code origin npm-audit-fix ; then
+  git push --delete origin npm-audit-fix
+fi
+
 git checkout -b npm-audit-fix
 
 if npm audit ; then
@@ -19,7 +26,6 @@ fi
 if [ "$files_changed" = true ] ; then
   git add . 
   git commit -m "results of running npm audit fix"
-  git remote set-url origin https://AgoricBot:$GITHUB_TOKEN@github.com/AgoricBot/SES.git
   git push origin npm-audit-fix
   hub pull-request --no-edit --base Agoric/SES:master
 fi
