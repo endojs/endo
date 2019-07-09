085a2e0e54ae08f8d50952e9a8397a2fc1e9c883
Michael FIG <mfig@agoric.com>
2019-07-09 11:17:46 -0600

    test/sample-test.js: async robustness (#3)

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,6 +1,12 @@
 import { test } from 'tape-promise/tape';
 
-test('unique test description', t => {
-  t.true('various tests');
-  t.end();
+test('unique test description', async t => {
+  try {
+    t.true('various tests');
+    t.equal(await Promise.resolve(123), 123);
+  } catch (e) {
+    t.assert(false, e);
+  } finally {
+    t.end();
+  }
 });
