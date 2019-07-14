f39133ae707900bf177463c0c2752deec1b44ebc
Michael FIG <michael+github@fig.org>
2019-07-13 23:01:25 -0600

    export evaluateExpr, evaluateProgram, makeEvaluators

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
-function evaluate(source, endowments = {}) {
-  const names = Object.getOwnPropertyNames(endowments);
-  const s = `(function(endowments) {
-    const { ${names.join(',')} } = endowments;
-    return ${source};
-  })`;
-  // eslint-disable-next-line no-eval
-  return eval(s)(endowments);
-}
+// The evaluate maker, which curries the makerOptions.
+export const makeEvaluators = (makerOptions = {}) => {
+  const makeEvaluator = isExpr => (source, endowments = {}, options = {}) => {
+    const fullTransforms = [
+      ...(options.transforms || []),
+      ...(makerOptions.transforms || []),
+    ];
+    const fullEndowments = {
+      ...(makerOptions.endowments || {}),
+      ...endowments,
+    };
 
-module.exports = evaluate;
+    const endowmentState = fullTransforms.reduce(
+      (es, transform) => (transform.endow ? transform.endow(es) : es),
+      { endowments: fullEndowments },
+    );
+
+    const sourceState = fullTransforms.reduce(
+      (ss, transform) => (transform.rewrite ? transform.rewrite(ss) : ss),
+      { src: source },
+    );
+
+    // Generate the expression context, if necessary.
+    const src = isExpr ? `(${sourceState.src}\n)` : sourceState.src;
+    const names = Object.getOwnPropertyNames(endowmentState.endowments);
+
+    // This function's first argument is the endowments.
+    // The second argument is the source string to evaluate.
+    const scopedEval = `(function() {
+      'use strict';
+      const { ${names.join(',')} } = arguments[0];
+      return eval(arguments[1]);
+    })`;
+
+    // eslint-disable-next-line no-eval
+    return eval(scopedEval)(endowmentState.endowments, src);
+  };
+
+  return {
+    evaluateProgram: makeEvaluator(false),
+    evaluateExpr: makeEvaluator(true),
+  };
+};
+
+// Export the default evaluators.
+export const { evaluateExpr, evaluateProgram } = makeEvaluators();
--- test/test-evaluate.js
+++ test/test-evaluate.js
@@ -1,21 +1,98 @@
-// eslint doesn't realize that this file
-// (agoric-evaluate/test/test-evaluate.js) is a test, since it lives in a
-// non-"test" subdirectory.
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { test } from 'tape';
-import evaluate from '../src/main';
+import { test } from 'tape-promise/tape';
+import { evaluateExpr, evaluateProgram, makeEvaluators } from '../src/index';
 
 test('basic', t => {
-  t.deepEqual(evaluate('1+2'), 3);
-  t.deepEqual(evaluate('(a,b) => a+b')(1, 2), 3);
-  t.deepEqual(evaluate('(function(a,b) { return a+b; })')(1, 2), 3);
-  t.end();
+  try {
+    t.equal(evaluateExpr('1+2'), 3, 'addition');
+    t.equal(evaluateExpr('(a,b) => a+b')(1, 2), 3, 'arrow expr');
+    t.equal(
+      evaluateExpr('function(a,b) { return a+b; }')(1, 2),
+      3,
+      'function expr',
+    );
+    t.throws(
+      () => evaluateExpr('123; 234'),
+      SyntaxError,
+      'evaluateExpr fails program',
+    );
+    t.equal(evaluateProgram('123; 234'), 234, 'evaluateProgram succeeds');
+    t.equal(evaluateExpr(`(1,eval)('123')`), 123, 'indirect eval succeeds');
+  } catch (e) {
+    t.assert(false, e);
+  } finally {
+    t.end();
+  }
 });
 
 test('endowments', t => {
-  t.deepEqual(evaluate('1+a', { a: 2 }), 3);
-  t.deepEqual(evaluate('(a,b) => a+b+c', { c: 3 })(1, 2), 6);
-  t.deepEqual(evaluate('(function(a,b) { return a+b+c; })', { c: 3 })(1, 2), 6);
-  t.deepEqual(evaluate('1+a+b', { a: 2, b: 3 }), 6);
-  t.end();
+  try {
+    t.equal(evaluateExpr('1+a', { a: 2 }), 3, 'endowment addition');
+    t.equal(
+      evaluateExpr('(a,b) => a+b+c', { c: 3 })(1, 2),
+      6,
+      'endowment arrow expr',
+    );
+    t.equal(
+      evaluateExpr('function(a,b) { return a+b+c; }', { c: 3 })(1, 2),
+      6,
+      'endowment function expr',
+    );
+    t.equal(evaluateExpr('1+a+b', { a: 2, b: 3 }), 6, 'multiple endowments');
+  } catch (e) {
+    t.assert(false, e);
+  } finally {
+    t.end();
+  }
+});
+
+test('options.transforms', t => {
+  try {
+    const endowments = Object.create(null, {
+      foo: { value: 1 },
+      bar: { value: 2, writable: true },
+    });
+
+    const evalTransforms = [
+      {
+        endow(es) {
+          return { ...es, endowments: { ...es.endowments, abc: 123 } };
+        },
+        rewrite(ss) {
+          return { ...ss, src: ss.src === 'ABC' ? 'abc' : ss.src };
+        },
+      },
+    ];
+
+    const options = {
+      transforms: [
+        {
+          rewrite(ss) {
+            return { ...ss, src: ss.src === 'ABC' ? 'def' : ss.src };
+          },
+        },
+      ],
+    };
+
+    const { evaluateExpr: myEval } = makeEvaluators({
+      endowments,
+      transforms: evalTransforms,
+    });
+
+    t.equal(myEval('abc', {}), 123, `evalTransforms don't rewrite`);
+    t.equal(myEval('ABC', { ABC: 234 }), 123, `evalTransforms rewrite ABC`);
+    t.equal(
+      myEval('ABC', { ABC: 234, abc: 'notused' }),
+      123,
+      `endowments.abc is overridden`,
+    );
+    t.equal(
+      myEval('ABC', { def: 789 }, options),
+      789,
+      `options.transforms go first`,
+    );
+  } catch (e) {
+    t.assert(false, e);
+  } finally {
+    t.end();
+  }
 });
