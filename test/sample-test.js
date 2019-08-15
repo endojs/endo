7a5d2cfff114336be67d46703c173ae7a8c442eb
Michael FIG <mfig@agoric.com>
2019-08-15 17:52:55 -0600

    Test unexpected exception (#8)
    * sample-test.js: use isNot to print out the unexpected exception
    
    * test-sample.js: use test.only as the default

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,12 +1,12 @@
 import { test } from 'tape-promise/tape';
 
-test('unique test description', async t => {
+// TODO: remove .only when you have this test working
+test.only('unique test description', async t => {
   try {
     t.true('various tests');
     t.equal(await Promise.resolve(123), 123);
   } catch (e) {
-    console.log('unexpected exception', e);
-    t.assert(false, e);
+    t.isNot(e, e, 'unexpected exception');
   } finally {
     t.end();
   }
