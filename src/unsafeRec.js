c85956ac2852774ec408593b7ec941d49c855163
Michael FIG <michael+github@fig.org>
2019-09-19 21:18:12 -0700

    repairFunctions: use globalEval binding to placate rollup
    rollup is apparently allergic to the `(1, eval)(...)` trick to
    avoid direct eval.  Binding eval to a different name is the only
    way it supports usage without mangling.

--- src/unsafeRec.js
+++ src/unsafeRec.js
@@ -94,7 +94,7 @@ const repairAccessorsShim = cleanupSource(
   `"use strict"; (${repairAccessors})();`
 );
 const repairFunctionsShim = cleanupSource(
-  `"use strict"; (${repairFunctions})();`
+  `"use strict"; const globalEval = eval; (${repairFunctions})();`
 );
 
 // Create a new unsafeRec from a brand new context, with new intrinsics and a
