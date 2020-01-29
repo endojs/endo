39133c2897d23cd87b153569105e6d9028ca66ed
JF Paradis <jfparadis@users.noreply.github.com>
2020-01-28 18:20:03 -0800

    Make Math.random() not have a prototype

--- packages/tame-global-math-object/src/main.js
+++ packages/tame-global-math-object/src/main.js
@@ -1,11 +1,15 @@
 const { defineProperties } = Object;
 
 export default function tameGlobalMathObject() {
+  const throwingRandom = {
+    random() {
+      throw Error('disabled');
+    }
+  }.random;
+
   defineProperties(Math, {
     random: {
-      value: function random() {
-        throw Error('disabled');
-      },
+      value: throwingRandom,
       enumerable: false,
       configurable: true,
       writable: true,
