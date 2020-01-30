e2c86a697f47e03720e8ce61d1e11e16a2f2ca1e
JF Paradis <jfparadis@users.noreply.github.com>
2020-01-29 16:30:13 -0800

    Fix lockdown shim name

--- packages/lockdown-shim/src/main.js
+++ packages/lockdown-shim/src/main.js
@@ -12,20 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright (C) 2018 Agoric
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import { getIntrinsics } from '@agoric/intrinsics';
 import whitelistIntrinsics from '@agoric/intrinsics-whitelist';
 import repairLegacyAccessors from '@agoric/repair-legacy-accessors';
@@ -67,7 +53,7 @@ export function lockdown(options = {}) {
     }
 
     /**
-     * 2. SHIM to expose the new APIs.
+     * 2. SHIM to expose the proposed APIs.
      */
 
     // Build a harden() with an empty fringe.
@@ -106,7 +92,7 @@ export function lockdown(options = {}) {
     repairLegacyAccessors();
 
     /**
-     * 4. Freeze to share the intrinsics.
+     * 4. HARDEN to share the intrinsics.
      */
 
     // Circumvent the override mistake.
