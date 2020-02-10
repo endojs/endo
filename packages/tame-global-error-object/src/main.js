fcd0159150618f8dbf2e88ef8adb3fb0a6939045
JF Paradis <jfparadis@users.noreply.github.com>
2020-02-10 11:46:06 -0800

    Improve taming of error object

--- packages/tame-global-error-object/src/main.js
+++ packages/tame-global-error-object/src/main.js
@@ -1,16 +1,59 @@
-const { getOwnPropertyDescriptor } = Object;
+const { freeze, getOwnPropertyDescriptor, defineProperty } = Object;
+
+/**
+ * hasOwnProperty()
+ * Mimics Object.prototype.hasOwnProperty which uses internally
+ * GetOwnProperty: see https://tc39.es/ecma262/#sec-hasownproperty
+ */
+function hasOwnProperty(obj, prop) {
+  return getOwnPropertyDescriptor(obj, prop) !== undefined;
+}
+
+function assert(condition, message) {
+  if (!condition) {
+    throw new TypeError(message);
+  }
+}
 
 export default function tameGlobalErrorObject() {
-  // Tame static properties.
-  delete Error.captureStackTrace;
+  // Tame static methods
 
-  if (getOwnPropertyDescriptor(Error, 'captureStackTrace')) {
-    throw Error('Cannot remove Error.captureStackTrace');
+  if (hasOwnProperty(Error, 'captureStackTrace')) {
+    const { captureStackTrace } = {
+      captureStackTrace(targetObject, _constructorOpt) {
+        // Creates a .stack property on targetObject,
+        // which when accessed returns a string.
+        Reflect.set(targetObject, 'stack', '');
+      },
+    };
+
+    Error.captureStackTrace = captureStackTrace;
+
+    // Validate that the value property has been replaced.
+    const desc = getOwnPropertyDescriptor(Error, 'captureStackTrace');
+    assert(
+      desc.value === captureStackTrace,
+      'Cannot tame Error.captureStackTrace',
+    );
   }
 
-  delete Error.stackTraceLimit;
+  if (hasOwnProperty(Error, 'stackTraceLimit')) {
+    defineProperty(Error, 'stackTraceLimit', {
+      get() {
+        return 0;
+      },
+      set() {
+        /* ignored silently */
+      },
+      enumerable: false,
+      configurable: true,
+    });
 
-  if (getOwnPropertyDescriptor(Error, 'stackTraceLimit')) {
-    throw Error('Cannot remove Error.stackTraceLimit');
+    // Validate that the value property has been converted.
+    const desc = getOwnPropertyDescriptor(Error, 'stackTraceLimit');
+    assert(
+      hasOwnProperty(desc, 'get') && hasOwnProperty(desc, 'set'),
+      'Cannot tame Error.stackTraceLimit',
+    );
   }
 }
--- packages/tame-global-error-object/test/tameGlobalErrorObject.test.js
+++ packages/tame-global-error-object/test/tameGlobalErrorObject.test.js
@@ -4,18 +4,43 @@ import tameGlobalErrorObject from '../src/main.js';
 
 const { test } = tap;
 
-test('tameGlobalErrorObject', t => {
-  t.plan(3);
+test('tameGlobalErrorObject - captureStackTrace', t => {
+  t.plan(6);
 
   const restore = captureGlobals('Error');
 
   tameGlobalErrorObject();
 
-  t.equal(Error.captureStackTrace, undefined);
-  t.equal(Error.stackTraceLimit, undefined);
+  const desc = Object.getOwnPropertyDescriptor(Error, 'captureStackTrace');
+  t.ok('value' in desc);
+  t.equal(typeof desc.value, 'function');
+  t.equal(desc.enumerable, false);
+  t.equal(desc.configurable, true);
+
+  t.equal(Error.stackTraceLimit, 0);
+  Error.stackTraceLimit = 10;
+  t.equal(Error.stackTraceLimit, 0);
+
+  restore();
+});
+
+test('tameGlobalErrorObject - stackTraceLimit', t => {
+  t.plan(7);
+
+  const restore = captureGlobals('Error');
+
+  const desc = Object.getOwnPropertyDescriptor(Error, 'stackTraceLimit');
+  t.ok('set' in desc);
+  t.ok('get' in desc);
+  t.equal(desc.enumerable, false);
+  t.equal(desc.configurable, true);
 
   const error = new Error();
   t.equal(error.stack, undefined);
+  error.stack = false;
+  t.equal(error.stack, false);
+  Error.captureStackTrace(error);
+  t.equal(error.stack, '');
 
   restore();
 });
