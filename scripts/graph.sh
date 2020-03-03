7a9d9dacfa474de229eb003a10acdde20d409578
Kris Kowal <kris@cixar.com>
2020-03-03 15:08:56 -0800

    Add script to generate dependency graph

---
+++ scripts/graph.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# scripts/graph.sh generates packages.png, a visualization of the internal
+# package dependency graph.
+set -ueo pipefail
+DIR=$(dirname -- "${BASH_SOURCE[0]}")
+{
+    echo 'digraph {'
+    echo 'rankdir=LR'
+    cat "$DIR"/../packages/*/package.json | jq -r '
+        .name as $from |
+        (.dependencies // {}) |
+        keys[] |
+        {$from, to: .} |
+        "\"\(.from)\" -> \"\(.to)\""
+    '
+    echo '}'
+} | dot -Tpng > "$DIR"/../packages.png
