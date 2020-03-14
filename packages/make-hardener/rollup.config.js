2522dfcb1731fa359b02eb6843b2fe92292340a2
Kris Kowal <kris@cixar.com>
2020-03-14 11:39:54 -0700

    make-hardener: Fix direct Node.js ESM support

--- packages/make-hardener/rollup.config.js
+++ packages/make-hardener/rollup.config.js
@@ -1,16 +1,37 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
 export default [
   {
     input: 'src/main.js',
     output: [
       {
-        file: 'dist/make-hardener.umd.js',
-        format: 'umd',
-        name: 'makeHardener',
+        file: 'dist/make-hardener.mjs',
+        format: 'esm',
       },
       {
-        file: 'dist/make-hardener.cjs.js',
+        file: 'dist/make-hardener.cjs',
         format: 'cjs',
       },
     ],
+    plugins: [resolve()],
+  },
+  {
+    input: 'src/main.js',
+    output: {
+      file: 'dist/make-hardener.umd.js',
+      format: 'umd',
+      name: 'make-hardener',
+    },
+    plugins: [resolve()],
+  },
+  {
+    input: 'src/main.js',
+    output: {
+      file: 'dist/make-hardener.umd.min.js',
+      format: 'umd',
+      name: 'make-hardener',
+    },
+    plugins: [resolve(), terser()],
   },
 ];
