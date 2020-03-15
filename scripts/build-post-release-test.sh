247c5c6769dddf0a3dce14f15252fe8c4010ba01
Michael FIG <mfig@agoric.com>
2020-03-15 12:39:00 -0600

    build: add integration-test test:pre-release to ci

--- scripts/build-post-release-test.sh
+++ scripts/build-post-release-test.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 set -e
 DIR=$(dirname -- "${BASH_SOURCE[0]}")
-cd "$DIR"
+cd "$DIR/.."
 npm run-script build
 cd packages/ses-integration-test
-npm install --no-save $( npm pack .. )
+npm install --no-save "$( npm pack ../ses )"
 npm run create-test-file-no-lib-cjs
-npm run create-test-file-esm
-npm run create-test-file-cjs
 npm run create-test-file-browserified-tape
--- scripts/build-pre-release-test.sh
+++ scripts/build-pre-release-test.sh
@@ -1,7 +1,10 @@
-#! /bin/sh -e
+#! /bin/bash
+set -e
+DIR=$(dirname -- "${BASH_SOURCE[0]}")
+cd "$DIR/.."
 npm run-script build
-cd packages/ses-integration-test 
-npm install $( npm pack ../ses )
+cd packages/ses-integration-test
+npm install --no-save "$( npm pack ../ses )"
 npm run create-test-file-no-lib-cjs
 npm run create-test-file-esm
 npm run create-test-file-cjs
