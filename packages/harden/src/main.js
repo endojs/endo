6f0358535c20f7a7b81c9bf72ff6cd27a858da26
Kris Kowal <kris@cixar.com>
2020-03-16 10:43:04 -0700

    Patch a hole for make-hardener lint no-unresolved

--- packages/harden/src/main.js
+++ packages/harden/src/main.js
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// eslint-disable-next-line import/no-unresolved
 import makeHardener from '@agoric/make-hardener';
 import buildTable from './buildTable.js';
 
