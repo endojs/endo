b010f8a1212aa949be8aa4970eb4ebc25aa83517
Mark S. Miller <erights@users.noreply.github.com>
2020-03-23 13:42:43 -0700

    fix(ses): regexp taming
    Bad RegExp taming revealed by #237 
    
    Exclude similar but new test failures

--- packages/ses/test/tame-global-reg-exp-object.test.js
+++ packages/ses/test/tame-global-reg-exp-object.test.js
@@ -4,9 +4,60 @@ import tameGlobalRegExpObject from '../src/tame-global-reg-exp-object.js';
 
 const { test } = tap;
 
-test('tameGlobalRegExpObject - constructor', t => {
-  t.plan(8);
+test('tameGlobalRegExpObject - unsafeRegExp denied', t => {
+  const unsafeRegExp = RegExp;
+  const restore = captureGlobals('RegExp');
+  tameGlobalRegExpObject();
 
+  t.ok(unsafeRegExp !== RegExp, 'constructor not replaced');
+  const regexp = /./;
+  t.ok(regexp.constructor === RegExp, 'tamed constructor not reached');
+  // Don't leak the unsafe constructor
+  // https://github.com/Agoric/SES-shim/issues/237
+  t.ok(regexp.constructor !== unsafeRegExp, 'unsafe constructor reachable!');
+
+  restore();
+  t.end();
+});
+
+test('tameGlobalRegExpObject - undeniable prototype', t => {
+  const restore = captureGlobals('RegExp');
+  tameGlobalRegExpObject();
+
+  // Don't try to deny the undeniable
+  // https://github.com/Agoric/SES-shim/issues/237
+  const regexp1 = new RegExp('.');
+  const regexp2 = RegExp('.');
+  const regexp3 = /./;
+  t.ok(
+    // eslint-disable-next-line no-proto
+    regexp1.__proto__ === regexp2.__proto__,
+    'new vs non-new instances differ',
+  );
+  t.ok(
+    // eslint-disable-next-line no-proto
+    regexp1.__proto__ === regexp3.__proto__,
+    'new vs literal instances differ',
+  );
+
+  t.ok(
+    regexp1 instanceof RegExp,
+    'new instance not instanceof tamed constructor',
+  );
+  t.ok(
+    regexp2 instanceof RegExp,
+    'non-new instance not instanceof tamed constructor',
+  );
+  t.ok(
+    regexp3 instanceof RegExp,
+    'literal instance not instanceof tamed constructor',
+  );
+
+  restore();
+  t.end();
+});
+
+test('tameGlobalRegExpObject - constructor', t => {
   const restore = captureGlobals('RegExp');
   tameGlobalRegExpObject();
 
@@ -20,6 +71,7 @@ test('tameGlobalRegExpObject - constructor', t => {
   const properties = Reflect.ownKeys(RegExp);
   t.deepEqual(
     properties.sort(),
+    // See https://github.com/Agoric/SES-shim/issues/239
     ['length', 'name', 'prototype'].sort(),
     'RegExp should not have static properties',
   );
@@ -35,4 +87,5 @@ test('tameGlobalRegExpObject - constructor', t => {
   t.equal(RegExp('foo').test('foobar'), true);
 
   restore();
+  t.end();
 });
