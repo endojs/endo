808734d7e7b2d97681dbf569825b19dd6d2c4365
Kris Kowal <kris@cixar.com>
2020-04-22 18:34:29 -0700

    Allow privileged Compartments before lockdown (#260)
    This is a preliminary change to decouple Compartment and lockdown. #259

--- packages/ses/test/lockdown.test.js
+++ packages/ses/test/lockdown.test.js
@@ -1,8 +1,18 @@
 import test from 'tape';
+import Compartment from '../src/compartment-shim.js';
 import { lockdown } from '../src/lockdown-shim.js';
 
 test('lockdown returns boolean or throws in SES', t => {
-  t.plan(6);
+  // Compartment constructor does not throw before lockdown.
+  (() => {
+    const c = new Compartment({}, {}, {});
+    const functionConstructor = c.evaluate('Function.prototype.constructor');
+    t.equal(
+      functionConstructor,
+      Function,
+      'before lockdown, compartments have the same privileged Function.prototype.constructor as the start compartment',
+    );
+  })();
 
   t.ok(lockdown(), 'return true when called from JS without options');
   t.notOk(
@@ -25,4 +35,25 @@ test('lockdown returns boolean or throws in SES', t => {
     () => lockdown({ noTameRegExp: true }),
     'throws when attempting to untame RegExp',
   );
+
+  // Compartment constructor does not throw after lockdown.
+  (() => {
+    const c = new Compartment({}, {}, {});
+    const cf = c.evaluate('Function.prototype.constructor');
+    t.notEqual(
+      cf,
+      Function,
+      'after lockdown, Function.prototype.constructor must be tamed inside compartments',
+    );
+
+    const d = new Compartment({}, {}, {});
+    const df = d.evaluate('Function.prototype.constructor');
+    t.equal(
+      cf,
+      df,
+      'after lockdown, every compartment must have the same tamed Function.prototype.constructor',
+    );
+  })();
+
+  t.end();
 });
