4010a69638e68b830c39fdcf710b22c2c0a70db6
Kris Kowal <kris@cixar.com>
2020-06-11 09:06:33 -0700

    feat(endo): Add main
    Export only the public API: import, archive, and import from archive.

---
+++ packages/endo/src/main.js
@@ -0,0 +1,3 @@
+export { loadPath, importPath } from "./import.js";
+export { makeArchive, writeArchive } from "./archive.js";
+export { parseArchive, loadArchive, importArchive } from "./import-archive.js";
