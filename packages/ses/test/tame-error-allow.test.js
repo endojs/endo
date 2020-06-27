80bebd530e5293d66797574a7dc4b7260f610ba6
Mark S. Miller <erights@gmail.com>
2020-06-27 16:20:31 -0700

    fix: tame Error constructor

--- packages/ses/test/tame-error-allow.test.js
+++ packages/ses/test/tame-error-allow.test.js
@@ -4,7 +4,14 @@ import '../src/main.js';
 
 lockdown({ errorTaming: 'unsafe' });
 
-test('lockdown allow Error - Error is not tamed', t => {
+test('lockdown allow Error - Error is tamed unsafe', t => {
+  t.equal(typeof Error.captureStackTrace, 'function');
+  t.equal(typeof Error.stackTraceLimit, 'number');
+  t.equal(typeof new Error().stack, 'string');
+  t.end();
+});
+
+test('lockdown allow Error - Error in Compartment is tamed unsafe', t => {
   const c = new Compartment();
   t.equal(c.evaluate('typeof Error.captureStackTrace'), 'function');
   t.equal(c.evaluate('typeof Error.stackTraceLimit'), 'number');
@@ -12,7 +19,7 @@ test('lockdown allow Error - Error is not tamed', t => {
   t.end();
 });
 
-test('lockdown allow Error - Error in nested Compartment is not tamed', t => {
+test('lockdown allow Error - Error in nested Compartment is tamed unsafe', t => {
   const c = new Compartment().evaluate('new Compartment()');
   t.equal(c.evaluate('typeof Error.captureStackTrace'), 'function');
   t.equal(c.evaluate('typeof Error.stackTraceLimit'), 'number');
--- packages/ses/test/tame-error.test.js
+++ packages/ses/test/tame-error.test.js
@@ -4,18 +4,25 @@ import '../src/main.js';
 
 lockdown();
 
-test('lockdown default - Error is tamed', t => {
+test('lockdown default - Error is safe', t => {
+  t.equal(typeof Error.captureStackTrace, 'function');
+  t.equal(Error.stackTraceLimit, undefined);
+  t.equal(typeof new Error().stack, 'string');
+  t.end();
+});
+
+test('lockdown default - Error in Compartment is safe', t => {
   const c = new Compartment();
   t.equal(c.evaluate('typeof Error.captureStackTrace'), 'function');
-  t.equal(c.evaluate('typeof Error.stackTraceLimit'), 'number');
+  t.equal(c.evaluate('Error.stackTraceLimit'), undefined);
   t.equal(c.evaluate('typeof new Error().stack'), 'string');
   t.end();
 });
 
-test('lockdown default - Error in nested Compartment is tamed', t => {
+test('lockdown default - Error in nested Compartment is safe', t => {
   const c = new Compartment().evaluate('new Compartment()');
   t.equal(c.evaluate('typeof Error.captureStackTrace'), 'function');
-  t.equal(c.evaluate('typeof Error.stackTraceLimit'), 'number');
+  t.equal(c.evaluate('Error.stackTraceLimit'), undefined);
   t.equal(c.evaluate('typeof new Error().stack'), 'string');
   t.end();
 });
