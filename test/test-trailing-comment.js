b5c6eab5ab23324e764b319f1de8abfd41c3e79f
Michael FIG <mfig@agoric.com>
2020-06-30 20:33:24 -0600

    test: add test for trailing comment

---
+++ test/test-trailing-comment.js
@@ -0,0 +1,39 @@
+/* global Compartment */
+import '@agoric/install-ses';
+import { test } from 'tape-promise/tape';
+import bundleSource from '..';
+
+function evaluate(src, endowments) {
+  const c = new Compartment(endowments, {}, {});
+  return c.evaluate(src);
+}
+
+test('trailing comment', async t => {
+  try {
+    const { source: src1 } = await bundleSource(
+      `${__dirname}/../demo/trailing-comment.js`,
+      'nestedEvaluate',
+    );
+
+    // Fake out `require('@agoric/harden')`.
+    const require = _ => o => o;
+    const nestedEvaluate = src => {
+      // console.log('========== evaluating', src);
+      return evaluate(src, { require, nestedEvaluate });
+    };
+    // console.log(src1);
+    const srcMap1 = `(${src1})`;
+    const ex1 = nestedEvaluate(srcMap1)();
+
+    // console.log(err.stack);
+    t.equals(
+      typeof ex1.buildRootObject,
+      'function',
+      `buildRootObject is exported`,
+    );
+  } catch (e) {
+    t.isNot(e, e, 'unexpected exception');
+  } finally {
+    t.end();
+  }
+});
