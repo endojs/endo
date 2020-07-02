d54f72359db0a6b5391856ba6967561e67bf12da
Kris Kowal <kris@cixar.com>
2020-07-02 10:38:32 -0700

    Address lint

--- packages/ses-integration-test/test/sanity.test.js
+++ packages/ses-integration-test/test/sanity.test.js
@@ -7,10 +7,6 @@ test("sanity", t => {
   t.equal(lockdown(), true, "lockdown runs successfully");
   const c = new Compartment({ abc: 456 });
   t.equal(c.evaluate("123"), 123, "simple evaluate succeeds");
-  t.equal(
-    c.evaluate("abc"),
-    456,
-    "endowment succeeds"
-  );
+  t.equal(c.evaluate("abc"), 456, "endowment succeeds");
   t.end();
 });
