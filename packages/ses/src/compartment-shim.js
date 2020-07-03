0c0d41c1f327187db70a93f0457891e1d18817fe
Kris Kowal <kris@cixar.com>
2020-07-03 14:04:16 -0700

    Fix immutability of lexical globals under eval

--- packages/ses/src/compartment-shim.js
+++ packages/ses/src/compartment-shim.js
@@ -13,7 +13,7 @@ import * as babel from '@agoric/babel-standalone';
 // Both produce:
 //   Error: 'default' is not exported by .../@agoric/babel-standalone/babel.js
 import { makeModuleAnalyzer } from '@agoric/transform-module';
-import { assign, getOwnPropertyNames, entries } from './commons.js';
+import { assign, freeze, getOwnPropertyNames, entries } from './commons.js';
 import { createGlobalObject } from './global-object.js';
 import { performEval } from './evaluate.js';
 import { getCurrentRealmRec } from './realm-rec.js';
@@ -201,7 +201,11 @@ export class Compartment {
 
     // TODO just pass globalLexicals as globalObject
     // https://github.com/Agoric/SES-shim/issues/365
-    const localObject = { __proto__: null, ...globalLexicals, ...endowments };
+    const localObject = freeze({
+      __proto__: null,
+      ...globalLexicals,
+      ...endowments,
+    });
 
     return performEval(realmRec, source, globalObject, localObject, {
       globalTransforms,
