dfa100fc5ebd2265360cba7d25c8c4f077a7681d
Brian Warner <warner@lothar.com>
2020-07-16 18:31:02 -0700

    chore(install-ses): update to use ses-0.9.1
    This also changes the `errorTaming` mode to `unsafe`, which enables things
    like mutating `Error.prepareStackTrace` and `Error.stackTraceLimit`, which
    are used by several start-compartment packages (including our test
    frameworks). With this change, `tap` assertions like `t.equal` will correctly
    print line numbers and stack traces when they fail.
    
    Previously, `tap` was unable to get this information with `errorTaming:
    'safe'`, but they failed even worse with `errorTaming: 'unsafe'` (tests
    couldn't run at all). SES-0.9.0 changed the way taming works to allow us to
    use `unsafe`.
    
    We don't really want to allow these mutations in non-start-compartments, but
    we'll need more support from SES to do this right (allow the mutations in the
    start compartment, for tests, but forbid them from non-start compartments).
    
    SES-0.9.1 restores `String.toLocaleLowerCase` (which was removed in 0.9.0,
    and it turns out we need it for our `rollup` dependency).
    
    closes #1290

--- install-ses.js
+++ install-ses.js
@@ -5,7 +5,7 @@ import 'ses';
 // we need to enable Math.random as a workaround for 'brace-expansion' module
 // (dep chain: cosmic-swingset/ag-solo->temp->glob->minimatch->brace-expansion)
 // we need Date.now to build a timer device for cosmic-swingset
-lockdown({ mathTaming: 'unsafe', dateTaming: 'unsafe' });
+lockdown({ mathTaming: 'unsafe', dateTaming: 'unsafe', errorTaming: 'unsafe' });
 // We are now in the "Start Compartment". Our global has all the same
 // powerful things it had before, but the primordials have changed to make
 // them safe to use in the arguments of API calls we make into more limited
