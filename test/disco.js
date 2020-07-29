2ae0c6d112299b6f60458b2c26d7ee2da0554ea5
Michael FIG <mfig@agoric.com>
2020-07-29 14:28:15 -0600

    fix(captp): make more code paths fail on disconnect

--- test/disco.js
+++ test/disco.js
@@ -2,7 +2,7 @@
 
 import '@agoric/install-ses';
 import { test } from 'tape-promise/tape';
-import { makeCapTP } from '../lib/captp';
+import { E, makeCapTP } from '../lib/captp';
 
 test('try disconnecting captp', async t => {
   try {
@@ -10,24 +10,35 @@ test('try disconnecting captp', async t => {
     const { getBootstrap, abort } = makeCapTP(
       'us',
       obj => objs.push(obj),
-      () => harden({}),
+      () =>
+        harden({
+          method() {
+            return 'hello';
+          },
+        }),
     );
     t.deepEqual(objs, [], 'expected no messages');
     const bs = getBootstrap();
+    const ps = [];
+    ps.push(t.rejects(E.G(bs).prop, Error, 'rejected get after disconnect'));
+    ps.push(
+      t.rejects(E(bs).method(), Error, 'rejected method after disconnect'),
+    );
     t.deepEqual(
       objs,
       [{ type: 'CTP_BOOTSTRAP', questionID: 1 }],
       'expected bootstrap messages',
     );
-    const pr = t.rejects(bs, Error, 'rejected after disconnect');
+    ps.push(t.rejects(bs, Error, 'rejected after disconnect'));
     const abortMsg = { type: 'CTP_ABORT', exception: Error('disconnect') };
     abort(abortMsg.exception);
+    await t.rejects(getBootstrap(), Error, 'rejected disconnected bootstrap');
     t.deepEqual(
       objs,
       [{ type: 'CTP_BOOTSTRAP', questionID: 1 }, abortMsg],
       'expected disconnect messages',
     );
-    await pr;
+    await ps;
   } catch (e) {
     t.isNot(e, e, 'unexpected exception');
   } finally {
