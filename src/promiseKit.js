eda4e1afff73d80e67e863c835afd221c6457a57
Mark S. Miller <erights@users.noreply.github.com>
2020-08-06 01:54:57 -0700

    fix: `ERef<T>` is `T | PromiseLike<T>` (#1383)

--- src/promiseKit.js
+++ src/promiseKit.js
@@ -15,6 +15,15 @@ import { HandledPromise } from '@agoric/eventual-send';
  */
 
 /**
+ * @template T
+ * @typedef {T | PromiseLike<T>} ERef
+ * A reference of some kind for to an object of type T. It may be a direct
+ * reference to a local T. It may be a local presence for a remote T. It may
+ * be a promise for a local or remote T. Or it may even be a thenable
+ * (a promise-like non-promise with a "then" method) for a T.
+ */
+
+/**
  * Needed to prevent type errors where functions are detected to be undefined.
  */
 const NOOP_INITIALIZER = harden(_ => {});
