54bfebf4e41a450eddbc75f5acbc848f37a3ba18
Kris Kowal <kris@agoric.com>
2020-08-10 17:03:06 -0700

    endo/main Use subcommands, remove dubious ones

--- packages/endo/src/cli.js
+++ packages/endo/src/cli.js
@@ -5,121 +5,99 @@ import { loadLocation, writeArchive, loadArchive } from "./main.js";
 import { search } from "./search.js";
 import { compartmentMapForNodeModules } from "./compartmap.js";
 
-const iterate = sequence => sequence[Symbol.iterator]();
+async function subcommand([arg, ...rest], handlers) {
+  const keys = Object.keys(handlers);
+  if (arg === undefined || !keys.includes(arg))  {
+    return usage(`expected one of ${keys}`);
+  }
+  return handlers[arg](rest);
+}
+
+async function parameter(args, handle, usage) {
+  const [ arg, ...rest ] = args;
+  if (arg === undefined) {
+    return usage(`expected an argument`);
+  }
+  if (arg.startsWith('-')) {
+    return usage(`unexpected flag: ${arg}`);
+  }
+  return handle(arg, rest);
+}
 
 function usage(message) {
   console.error(message);
   return 1;
 }
 
-async function execute(application) {
-  const endowments = harden({
-    console: {
-      log(...args) {
-        console.log(...args);
+async function noEntryUsage() {
+  return usage(`expected path to program`);
+}
+
+async function noArchiveUsage() {
+  return usage(`expected path for archive`);
+}
+
+async function run(args, { cwd, read, write, stdout }) {
+
+  async function compartmap(args) {
+    async function handleEntry(applicationPath, args) {
+      if (args.length) {
+        return usage(`unexpected arguments: ${JSON.stringify(args)}`);
       }
+      const currentLocation = new URL(`${cwd()}/`, "file:///");
+      const applicationLocation = new URL(applicationPath, currentLocation);
+      const { packageLocation } = await search(read, applicationLocation);
+      const compartmentMap = await compartmentMapForNodeModules(
+        read,
+        packageLocation
+      );
+      stdout.write(`${JSON.stringify(compartmentMap, null, 2)}\n`);
+      return 0;
     }
-  });
-
-  await application.execute(endowments);
-  return 0;
-}
-
-async function executeLocation(applicationPath, { cwd, read }) {
-  const currentLocation = new URL(`${cwd()}/`, "file:///");
-  const applicationLocation = new URL(applicationPath, currentLocation);
-  const application = await loadLocation(read, applicationLocation);
-  return execute(application);
-}
-
-async function archive(archivePath, applicationPath, { cwd, read, write }) {
-  const currentLocation = new URL(`${cwd()}/`, "file:///");
-  const archiveLocation = new URL(archivePath, currentLocation);
-  const applicationLocation = new URL(applicationPath, currentLocation);
-  await writeArchive(write, read, archiveLocation, applicationLocation);
-  return 0;
-}
-
-async function executeArchive(archivePath, { read, cwd }) {
-  const currentLocation = new URL(`${cwd()}/`, "file:///");
-  const archiveLocation = new URL(archivePath, currentLocation);
-  const application = await loadArchive(read, archiveLocation);
-  return execute(application);
-}
-
-async function compartmap(applicationPath, { read, cwd, stdout }) {
-  const currentLocation = new URL(`${cwd()}/`, "file:///");
-  const applicationLocation = new URL(applicationPath, currentLocation);
-  const { packageLocation } = await search(read, applicationLocation);
-  const compartmentMap = await compartmentMapForNodeModules(
-    read,
-    packageLocation
-  );
-  stdout.write(`${JSON.stringify(compartmentMap, null, 2)}\n`);
-  return 0;
-}
-
-// run parses command line arguments and dispatches to a subcommand.
-async function run(args, powers) {
-  for (const arg of args) {
-    if (arg === "--") {
-      for (const applicationLocation of args) {
-        return executeLocation(applicationLocation, powers);
-      }
-    } else if (arg === "-m" || arg === "--compartmap") {
-      for (const applicationPath of args) {
-        const rem = Array.from(args);
-        if (rem.length > 0) {
-          return usage(
-            `Unexpected arguments after --compartmap ${applicationPath}: ${rem}`
-          );
-        }
-        return compartmap(applicationPath, powers);
-      }
-    } else if (arg === "-c" || arg === "--create") {
-      for (const archivePath of args) {
-        for (const applicationPath of args) {
-          const rem = Array.from(args);
-          if (rem.length > 0) {
-            return usage(
-              `Unexpected arguments after --archive ${archivePath} ${applicationPath}: ${rem}`
-            );
-          }
-          return archive(archivePath, applicationPath, powers);
-        }
-        return usage(`Expected application path`);
-      }
-      return usage(`Expected archive path`);
-    } else if (arg === "-e" || arg === "--execute") {
-      for (const archivePath of args) {
-        const rem = Array.from(args);
-        if (rem.length > 0) {
-          return usage(`Unexpected arguments after -e ${archivePath}: ${rem}`);
-        }
-        return executeArchive(archivePath, powers);
-      }
-      return usage(`Expected archive path`);
-    } else if (arg.startsWith("-")) {
-      return usage(`Unrecognized flag ${arg}`);
-    } else {
-      const rem = Array.from(args);
-      if (rem.length > 0) {
-        return usage(`Unexpected arguments after ${arg}: ${rem}`);
-      }
-      return executeLocation(arg, powers);
-    }
+    return parameter(args, handleEntry, noEntryUsage);
   }
-  return usage(`Expected script path or flag`);
+
+  async function archive(args) {
+    async function handleArchive(archivePath, args) {
+      async function handleEntry(applicationPath, args) {
+        if (args.length) {
+          return usage(`unexpected arguments: ${JSON.stringify(args)}`);
+        }
+        const currentLocation = new URL(`${cwd()}/`, "file:///");
+        const archiveLocation = new URL(archivePath, currentLocation);
+        const applicationLocation = new URL(applicationPath, currentLocation);
+        await writeArchive(write, read, archiveLocation, applicationLocation);
+        return 0;
+      }
+      return parameter(args, handleEntry, noEntryUsage);
+    }
+    return parameter(args, handleArchive, noArchiveUsage);
+  }
+
+  return subcommand(args, { compartmap, archive });
 }
 
 export async function main(process, modules) {
   const { fs } = modules;
-  const read = async location => fs.readFile(new URL(location).pathname);
-  const write = async (location, content) =>
-    fs.writeFile(new URL(location).pathname, content);
+
+  async function read(location) {
+    try {
+      return await fs.readFile(new URL(location).pathname);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async function write(location, content) {
+    try {
+      return await fs.writeFile(new URL(location).pathname, content);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
   try {
-    const args = iterate(process.argv.slice(2));
-    process.exitCode = await run(args, {
+    process.exitCode = await run(process.argv.slice(2), {
       read,
       write,
       cwd: process.cwd,
