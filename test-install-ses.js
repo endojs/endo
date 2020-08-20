8359bfb134bcdc8e4a47547f1695d856f02f752e
Brian Warner <warner@lothar.com>
2020-08-20 13:41:02 -0700

    chore: update 'install-ses' package to use AVA for testing

--- test-install-ses.js
+++
@@ -1,9 +0,0 @@
-/* global Compartment harden */
-import './install-ses';
-import test from 'tape';
-
-test('globals are present', t => {
-  t.equal(typeof Compartment, 'function');
-  t.equal(typeof harden, 'function');
-  t.end();
-});
---
+++ test/test-install-ses.js
@@ -0,0 +1,8 @@
+/* global Compartment harden */
+import '../install-ses';
+import test from 'ava';
+
+test('globals are present', t => {
+  t.is(typeof Compartment, 'function');
+  t.is(typeof harden, 'function');
+});
