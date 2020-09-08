131e7da2e27417aa4b0f85c37b889d6b612c9bb8
Michael FIG <mfig@agoric.com>
2020-09-08 09:04:21 -0600

    fix: silence normal disconnects

--- test/test-disco.js
+++ test/test-disco.js
@@ -47,8 +47,7 @@ test('try disconnecting captp', async t => {
   ps.push(
     t.throwsAsync(bs, { instanceOf: Error }, 'rejected after disconnect'),
   );
-  const abortMsg = { type: 'CTP_ABORT', exception: Error('disconnect') };
-  abort(abortMsg.exception);
+  abort();
   await t.throwsAsync(
     getBootstrap(),
     { instanceOf: Error },
@@ -56,9 +55,70 @@ test('try disconnecting captp', async t => {
   );
   t.deepEqual(
     objs,
-    [{ type: 'CTP_BOOTSTRAP', questionID: 1 }, abortMsg],
-    'expected disconnect messages',
+    [
+      { type: 'CTP_BOOTSTRAP', questionID: 1 },
+      { type: 'CTP_DISCONNECT', reason: undefined },
+    ],
+    'expected clean disconnect',
   );
-  await ps;
-  t.deepEqual(rejected, [abortMsg.exception], 'exactly one disconnect error');
+  await Promise.all(ps);
+});
+
+test('try aborting captp with reason', async t => {
+  const objs = [];
+  const rejected = [];
+  const { getBootstrap, abort } = makeCapTP(
+    'us',
+    obj => objs.push(obj),
+    () =>
+      harden({
+        method() {
+          return 'hello';
+        },
+      }),
+    {
+      onReject(e) {
+        rejected.push(e);
+      },
+    },
+  );
+  t.deepEqual(objs, [], 'expected no messages');
+  const bs = getBootstrap();
+  const ps = [];
+  ps.push(
+    t.throwsAsync(
+      E.G(bs).prop,
+      { instanceOf: Error },
+      'rejected get after disconnect',
+    ),
+  );
+  ps.push(
+    t.throwsAsync(
+      E(bs).method(),
+      { instanceOf: Error },
+      'rejected method after disconnect',
+    ),
+  );
+  t.deepEqual(
+    objs,
+    [{ type: 'CTP_BOOTSTRAP', questionID: 1 }],
+    'expected bootstrap messages',
+  );
+  ps.push(
+    t.throwsAsync(bs, { instanceOf: Error }, 'rejected after disconnect'),
+  );
+  const aborted = { type: 'CTP_DISCONNECT', reason: Error('something') };
+  abort(aborted.reason);
+  await t.throwsAsync(
+    getBootstrap(),
+    { instanceOf: Error },
+    'rejected disconnected bootstrap',
+  );
+  t.deepEqual(
+    objs,
+    [{ type: 'CTP_BOOTSTRAP', questionID: 1 }, aborted],
+    'expected unclean disconnect',
+  );
+  await Promise.all(ps);
+  t.deepEqual(rejected, [aborted.reason], 'error disconnect');
 });
