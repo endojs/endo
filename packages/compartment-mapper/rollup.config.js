3d70c23c2fb6c7fa176ed25b99d1e8b88c062bbd
Kris Kowal <kris@agoric.com>
2020-09-17 20:32:25 -0700

    fix(compartment-mapper): Withdraw UMD Rollup
    We have no coherent story for threading UMD dependencies on Node.js built-in modules.
    
    We also have a mystery as to why Rollup feels that it needs to link any Node.js built-in modules.  The dependencies may be coming from using Babel parser and translate.

--- packages/compartment-mapper/rollup.config.js
+++ packages/compartment-mapper/rollup.config.js
@@ -1,45 +1,25 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import { terser } from "rollup-plugin-terser";
 import fs from "fs";
 
 const metaPath = new URL("package.json", import.meta.url).pathname;
 const meta = JSON.parse(fs.readFileSync(metaPath, "utf-8"));
 const name = meta.name.split("/").pop();
-const umd = meta.umd || name;
+
+const resolveWithBuiltins = () => resolve({ preferBuiltins: true });
+const external = ["buffer", "events", "os", "stream", "tty", "util"];
 
 export default [
   {
     input: "src/main.js",
     output: [
       {
-        file: `dist/${name}.mjs`,
-        format: "esm"
-      },
-      {
         file: `dist/${name}.cjs`,
         format: "cjs"
       }
     ],
-    plugins: [resolve(), commonjs(), json()]
-  },
-  {
-    input: "src/main.js",
-    output: {
-      file: `dist/${name}.umd.js`,
-      format: "umd",
-      name: umd
-    },
-    plugins: [resolve(), commonjs(), json()]
-  },
-  {
-    input: "src/main.js",
-    output: {
-      file: `dist/${name}.umd.min.js`,
-      format: "umd",
-      name: umd
-    },
-    plugins: [resolve(), terser(), commonjs(), json()]
+    external,
+    plugins: [resolveWithBuiltins(), commonjs(), json()]
   }
 ];
