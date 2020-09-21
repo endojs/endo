037c82a327ab235316edffe6c314d3b4c1d443d6
kumavis <aaron@kumavis.me>
2020-09-21 10:57:27 +0800

    platform compatibility - document application of globalThis polyfill

--- packages/ses/test/package/test.cjs
+++ packages/ses/test/package/test.cjs
@@ -1,7 +1,13 @@
 // node test.cjs
-global.globalThis = global;
+
+// this is a polyfill for node v10 and earlier
+if (!global.globalThis) {
+  global.globalThis = global;
+}
+
 require('ses');
 
 lockdown();
+
 // eslint-disable-next-line no-undef
 console.log(Compartment);
