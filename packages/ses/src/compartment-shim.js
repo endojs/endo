5196521a17ac4b28f9aaaef2aa312eebe9edcbb7
Kris Kowal <kris@agoric.com>
2020-10-21 20:47:43 -0700

    feat(ses): Add __shimTransforms__ Compartment option (#485)

--- packages/ses/src/compartment-shim.js
+++ packages/ses/src/compartment-shim.js
@@ -51,14 +51,22 @@ export const CompartmentPrototype = {
     } = options;
     const localTransforms = [...transforms];
 
-    const {
-      globalTransforms,
-      globalObject,
-      globalLexicals,
-    } = privateFields.get(this);
+    const compartmentFields = privateFields.get(this);
+    let { globalTransforms } = compartmentFields;
+    const { globalObject, globalLexicals } = compartmentFields;
 
     let localObject = globalLexicals;
     if (__moduleShimLexicals__ !== undefined) {
+      // When using `evaluate` for ESM modules, as should only occur from the
+      // module-shim's module-instance.js, we do not reveal the SES-shim's
+      // module-to-program translation, as this is not standardizable behavior.
+      // However, the `localTransforms` will come from the `__shimTransforms__`
+      // Compartment option in this case, which is a non-standardizable escape
+      // hatch so programs designed specifically for the SES-shim
+      // implementation may opt-in to use the same transforms for `evaluate`
+      // and `import`, at the expense of being tightly coupled to SES-shim.
+      globalTransforms = undefined;
+
       localObject = create(null, getOwnPropertyDescriptors(globalLexicals));
       defineProperties(
         localObject,
@@ -103,6 +111,7 @@ export const makeCompartmentConstructor = (
     const {
       name = '<unknown>',
       transforms = [],
+      __shimTransforms__ = [],
       globalLexicals = {},
     } = options;
     const globalTransforms = [...transforms];
---
+++ packages/ses/test/compartment-transforms.test.js
@@ -0,0 +1,93 @@
+import tap from 'tap';
+import '../ses.js';
+
+const { test } = tap;
+
+lockdown();
+
+test('transforms apply to evaluated expressions', t => {
+  t.plan(1);
+
+  const transform = source => source.replace(/Farewell/g, 'Hello');
+  const transforms = [transform];
+  const c = new Compartment({}, {}, { transforms });
+  const greeting = c.evaluate('"Farewell, World!"');
+
+  t.equal(greeting, 'Hello, World!');
+});
+
+test('transforms apply to dynamic eval in compartments', t => {
+  t.plan(1);
+
+  const transform = source => source.replace(/Farewell/g, 'Hello');
+  const transforms = [transform];
+  const c = new Compartment(
+    {
+      greeting: '"Farewell, World!"',
+    },
+    {},
+    { transforms },
+  );
+  const greeting = c.evaluate('(0, eval)(greeting)');
+
+  t.equal(greeting, 'Hello, World!');
+});
+
+test('transforms do not apply to dynamic eval in compartments within compartments', t => {
+  t.plan(1);
+
+  const transform = source => source.replace(/Farewell/g, 'Hello');
+  const transforms = [transform];
+  const c = new Compartment({}, {}, { transforms });
+  const d = c.evaluate('new Compartment()');
+  const greeting = d.evaluate('"Farewell, World!"');
+
+  t.equal(greeting, 'Farewell, World!');
+});
+
+test('transforms do not apply to imported modules', async t => {
+  t.plan(1);
+
+  const transform = source => source.replace(/Farewell/g, 'Hello');
+  const transforms = [transform];
+  const resolveHook = () => '';
+  const importHook = () =>
+    new StaticModuleRecord('export default "Farewell, World!";');
+  const c = new Compartment({}, {}, { transforms, resolveHook, importHook });
+
+  const { namespace } = await c.import('any-string-here');
+  const { default: greeting } = namespace;
+
+  t.equal(greeting, 'Farewell, World!');
+});
+
+test('__shimTransforms__ apply to evaluated expressions', t => {
+  t.plan(1);
+
+  const transform = source => source.replace(/Farewell/g, 'Hello');
+  const transforms = [transform];
+  const c = new Compartment({}, {}, { transforms });
+  const greeting = c.evaluate('"Farewell, World!"');
+
+  t.equal(greeting, 'Hello, World!');
+});
+
+test('__shimTransforms__ do apply to imported modules', async t => {
+  t.plan(1);
+
+  const transform = source => source.replace(/Farewell/g, 'Hello');
+  const transforms = [transform];
+  const resolveHook = () => '';
+  const importHook = () =>
+    new StaticModuleRecord('export default "Farewell, World!";');
+  const c = new Compartment(
+    {},
+    {},
+    { __shimTransforms__: transforms, resolveHook, importHook },
+  );
+
+  const { namespace } = await c.import('any-string-here');
+  const { default: greeting } = namespace;
+
+  t.equal(greeting, 'Hello, World!');
+});
