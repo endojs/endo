e0324cc77d8bb9bd7e1db515f41b2883e28da6e0
Michael FIG <mfig@agoric.com>
2020-10-29 20:05:43 -0600

    fix: correct types for PromiseRecord.resolve

--- src/promiseKit.js
+++ src/promiseKit.js
@@ -9,7 +9,7 @@ const BestPipelinablePromise = globalThis.HandledPromise || Promise;
 /**
  * @template T
  * @typedef {Object} PromiseRecord A reified Promise
- * @property {(value: T) => void} resolve
+ * @property {(value: ERef<T>) => void} resolve
  * @property {(reason: any) => void} reject
  * @property {Promise<T>} promise
  */
