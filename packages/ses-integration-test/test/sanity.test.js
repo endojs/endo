9108bcec6b53f48dec4f4ff74a99b368b6a3dacd
Kris Kowal <kris@agoric.com>
2020-11-24 17:59:24 -0800

    fix: Revert ava changes in ses-integration-test

--- packages/ses-integration-test/test/sanity.test.js
+++ packages/ses-integration-test/test/sanity.test.js
@@ -3,8 +3,9 @@ import test from "tape";
 import "ses";
 
 test("sanity", t => {
-  t.is(lockdown(), true, "lockdown runs successfully");
+  t.equal(lockdown(), true, "lockdown runs successfully");
   const c = new Compartment({ abc: 456 });
-  t.is(c.evaluate("123"), 123, "simple evaluate succeeds");
-  t.is(c.evaluate("abc"), 456, "endowment succeeds");
+  t.equal(c.evaluate("123"), 123, "simple evaluate succeeds");
+  t.equal(c.evaluate("abc"), 456, "endowment succeeds");
+  t.end();
 });
