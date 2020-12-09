7a5418c23ce46eca3ed97961cf06a604075f3a33
Mark S. Miller <erights@gmail.com>
2020-12-08 23:22:10 -0800

    fix: simplify harden to climb prototype chain

--- packages/make-hardener/test/makeHardener.test.js
+++ packages/make-hardener/test/makeHardener.test.js
@@ -1,14 +1,8 @@
 import test from 'ava';
 import makeHardener from '../src/main.js';
 
-// `harden` is only intended to work after `lockdown`. However,
-// to test it standalone, we need to freeze at least these ahead
-// of time.
-const hardenFirst = [Array.prototype, Object.prototype, Function.prototype];
-
 test('makeHardener', t => {
   const h = makeHardener();
-  h(hardenFirst);
   const o = { a: {} };
   t.is(h(o), o);
   t.truthy(Object.isFrozen(o));
@@ -17,7 +11,6 @@ test('makeHardener', t => {
 
 test('harden the same thing twice', t => {
   const h = makeHardener();
-  h(hardenFirst);
   const o = { a: {} };
   t.is(h(o), o);
   t.is(h(o), o);
@@ -27,7 +20,6 @@ test('harden the same thing twice', t => {
 
 test('harden objects with cycles', t => {
   const h = makeHardener();
-  h(hardenFirst);
   const o = { a: {} };
   o.a.foo = o;
   t.is(h(o), o);
@@ -37,7 +29,6 @@ test('harden objects with cycles', t => {
 
 test('harden overlapping objects', t => {
   const h = makeHardener();
-  h(hardenFirst);
   const o1 = { a: {} };
   const o2 = { a: o1.a };
   t.is(h(o1), o1);
@@ -48,23 +39,18 @@ test('harden overlapping objects', t => {
   t.truthy(Object.isFrozen(o2));
 });
 
-test('do not commit early', t => {
-  // refs #4
+test('harden up prototype chain', t => {
   const h = makeHardener();
-  h(hardenFirst);
   const a = { a: 1 };
   const b = { b: 1, __proto__: a };
   const c = { c: 1, __proto__: b };
 
-  t.throws(() => h(b), { instanceOf: TypeError });
-  // the bug is that 'b' is marked as hardened. If that happens, harden(c)
-  // will pass when it was supposed to throw.
-  t.throws(() => h(c), { instanceOf: TypeError });
+  h(c);
+  t.truthy(Object.isFrozen(a));
 });
 
 test('harden() tolerates objects with null prototypes', t => {
   const h = makeHardener();
-  h(hardenFirst);
   const o = { a: 1 };
   Object.setPrototypeOf(o, null);
   t.is(h(o), o);
