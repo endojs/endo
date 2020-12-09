2650ff21231b9971aa82a76316435602a9988451
Kris Kowal <kris@agoric.com>
2020-12-08 16:17:46 -0800

    wip: fix main.js

--- packages/xscm/main.js
+++ packages/xscm/main.js
@@ -1,21 +1,19 @@
 /* global trace */
 
 // eslint-disable-next-line import/no-unresolved
+import "./src/text-shim";
 import { File } from "file";
-
 import { harden } from "./src/harden";
 import { makeConsole } from "./src/console";
+import { importLocation } from "@agoric/compartment-mapper/main";
+
 // TODO: import { loadMain } from "./src/endo-load";
 import xscm from "./src/xscm";
 
 globalThis.console = harden(makeConsole(trace));
 
-export default async function main(argv) {
-  trace(`argv.length: ${argv.length}\n`);
-
-  const f = new File(argv[0]);
+export default async function main() {
+  const f = new File("/Users/kris/ses/packages/xscm/main.js");
   const contents = f.read(String);
   trace(`file ${f} contains ${contents}\n`);
-
-  xscm(argv);
 }
