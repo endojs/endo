ecf3ab61cce3952327b92314bc50bbdbe4c02d71
Kris Kowal <kris@agoric.com>
2020-12-08 17:00:46 -0800

    wip: moar progress

--- packages/xscm/main.js
+++ packages/xscm/main.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line import/no-unresolved
 import "./src/text-shim";
+import URL from "@agoric/compartment-mapper/url";
 import { File } from "file";
 import { harden } from "./src/harden";
 import { makeConsole } from "./src/console";
@@ -12,8 +13,15 @@ import xscm from "./src/xscm";
 
 globalThis.console = harden(makeConsole(trace));
 
+const read = async url => {
+  const path = new URL(url).pathname;
+  const file = new File(path);
+  const buffer = file.read(ArrayBuffer);
+  const bytes = new Uint8Array(buffer);
+  return bytes;
+};
+
 export default async function main() {
-  const f = new File("/Users/kris/ses/packages/xscm/main.js");
-  const contents = f.read(String);
-  trace(`file ${f} contains ${contents}\n`);
+  const bytes = await read("file:///Users/kris/ses/packages/xscm/main.js");
+  trace(`file ${f} contains ${bytes}\n`);
 }
