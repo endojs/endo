e35169871e58389314bd0428868f0d7a030e54c5
Mark S. Miller <erights@gmail.com>
2021-01-22 17:54:45 -0800

    fix! Nat accepts safe JavaScript numbers as well as bigints.

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,13 @@
+## Next Release
+
+* BREAKING CHANGE: In addition to non-negative bigints, `Nat` will also accept a
+  non-negative number with the range of
+  [integers safely representable in floating point](https://tc39.es/ecma262/#sec-number.issafeinteger).
+  Its output will always be a non-negative bigint or a thrown error.
+* Also exports an `isNum` function which can be used to test anything without
+  error. It returns a boolean indicating whether the argument is accepted by
+  `Nat` as input.
+
 ## Release v3.0.1 (12/4/2019)
 
 * Re-release v3.0.0 after running `npm run build` (previous release
--- test/test-nat.js
+++ test/test-nat.js
@@ -1,7 +1,7 @@
 import test from 'ava';
-import Nat from '../src/index';
+import { isNat, Nat } from '../src/index';
 
-test('Nat() works for natural BigInts', (t) => {
+test('Nat() works for natural BigInts', t => {
   t.is(Nat(1n), 1n);
   t.is(Nat(BigInt('1')), BigInt('1'));
   t.is(Nat(BigInt('1')), 1n);
@@ -9,64 +9,48 @@ test('Nat() works for natural BigInts', (t) => {
   t.is(Nat(9007199254741000n), 9007199254741000n);
 });
 
-test('Nat() throws for non-natural BigInts', (t) => {
+test('Nat() throws for non-natural BigInts', t => {
   t.throws(() => Nat(-1n), {
     message: `-1 is negative`,
     instanceOf: RangeError,
   });
 });
 
-test('Nat() throws for non-BigInts', (t) => {
-  t.throws(() => Nat(0), {
-    message: `0 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(1), {
-    message: `1 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(999), {
-    message: `999 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(3.0), {
-    message: `3 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat('not a number'), {
-    message: `not a number is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(-1), {
-    message: `-1 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(0.5), {
-    message: `0.5 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(2 ** 60), {
-    message: `1152921504606847000 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(NaN), {
-    message: `NaN is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(Infinity), {
-    message: `Infinity is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat('3'), {
-    message: `3 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(3.1), {
-    message: `3.1 is not a BigInt`,
-    instanceOf: TypeError,
-  });
-  t.throws(() => Nat(2 ** 53 - 1), {
-    message: `9007199254740991 is not a BigInt`,
-    instanceOf: TypeError,
-  });
+test('Nat() throws when not a natural number', t => {
+  t.is(Nat(0), 0n);
+  t.is(Nat(1), 1n);
+  t.is(Nat(999), 999n);
+  t.is(Nat(3.0), 3n);
+  t.throws(() => Nat('not a number'), { instanceOf: TypeError });
+  t.throws(() => Nat(-1), { instanceOf: RangeError });
+  t.throws(() => Nat(0.5), { instanceOf: RangeError });
+  t.throws(() => Nat(2 ** 60), { instanceOf: RangeError });
+  t.throws(() => Nat(NaN), { instanceOf: RangeError });
+  t.throws(() => Nat(Infinity), { instanceOf: RangeError });
+  t.throws(() => Nat('3'), { instanceOf: TypeError });
+  t.throws(() => Nat(3.1), { instanceOf: RangeError });
+
+  // works for safe integers only
+  t.is(Nat(2 ** 53 - 1), 2n ** 53n - 1n);
+  t.throws(() => Nat(2 ** 53), { instanceOf: RangeError });
+});
+
+test('isNat examples from the README', t => {
+  t.assert(isNat(3));
+  t.assert(isNat(3n));
+  t.false(isNat('3'));
+  t.false(isNat(2 ** 70));
+  t.assert(isNat(2n ** 70n));
+  t.false(isNat(-3n));
+  t.false(isNat(3.1));
+});
+
+test('Nat examples from the README', t => {
+  t.is(Nat(3), 3n);
+  t.is(Nat(3n), 3n);
+  t.throws(() => Nat('3'), { instanceOf: TypeError });
+  t.throws(() => Nat(2 ** 70), { instanceOf: RangeError });
+  t.is(Nat(2n ** 70n), 1180591620717411303424n);
+  t.throws(() => Nat(-3n), { instanceOf: RangeError });
+  t.throws(() => Nat(3.1), { instanceOf: RangeError });
 });
