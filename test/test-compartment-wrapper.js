fcd410e113bb42b727789f11632ae4fec24f0fce
Mark S. Miller <erights@users.noreply.github.com>
2021-03-28 21:37:28 -0700

    More ses-ava (#2740)
    * fix: more ses-ava

--- test/test-compartment-wrapper.js
+++ test/test-compartment-wrapper.js
@@ -1,5 +1,6 @@
-import '@agoric/install-ses';
-import test from 'ava';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava';
+
 import { assert, details as X } from '@agoric/assert';
 import { wrapInescapableCompartment } from '../src/compartment-wrapper.js';
 
