a75054ce8bdb60e8479288808b9269068da68e77
Kris Kowal <kris@agoric.com>
2021-03-31 16:20:02 -0700

    feat(bundle-source): Specific ModuleFormat type (#2767)
    This surfaces invalid bundle formats with a TypeScript string enum.

--- src/types.js
+++ src/types.js
@@ -1,6 +1,10 @@
 /**
+ * @typedef {'getExport' | 'nestedEvaluate' | 'endoZipBase64'} ModuleFormat
+ */
+
+/**
  * @callback BundleSource
  * @param {string} startFilename - the filepath to start the bundling from
- * @param {string=} moduleFormat
+ * @param {ModuleFormat=} moduleFormat
  * @param {Object=} powers
  */
