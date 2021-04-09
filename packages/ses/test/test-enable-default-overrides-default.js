5486a156a034bbdd8a33364a09db7fd67e7b043e
Dan Connolly <connolly@agoric.com>
2021-04-09 15:19:17 -0500

    test(ses): prune uninteresting override test
    The test as written depends on stringifying functions, which XS
    doesn't do. erights says the test is not novel and not worth
    maintaining.

--- packages/ses/test/test-enable-default-overrides-default.js
+++ packages/ses/test/test-enable-default-overrides-default.js
@@ -13,17 +13,3 @@ test('enable default overrides of Uint8Array', t => {
     Buffer.prototype.toLocaleString = Buffer.prototype.toString;
   });
 });
-
-test('enable default overrides of Uint8Array in evaluation', t => {
-  const c = new Compartment();
-  t.notThrows(() =>
-    c.evaluate(
-      `(${function foo() {
-        function Buffer(_arg, _encodingOrOffset, _length) {}
-        Object.setPrototypeOf(Buffer.prototype, Uint8Array.prototype);
-        Object.setPrototypeOf(Buffer, Uint8Array);
-        Buffer.prototype.toLocaleString = Buffer.prototype.toString;
-      }})()`,
-    ),
-  );
-});
