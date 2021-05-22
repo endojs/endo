edda729b237d971ecd7b5e68aed864d7c2f3c43c
Kris Kowal <kris@agoric.com>
2021-05-22 15:38:29 -0700

    refactor(zip): Use DataView

--- packages/zip/src/buffer-writer.js
+++ packages/zip/src/buffer-writer.js
@@ -29,8 +29,10 @@ export class BufferWriter {
    * @param {number=} capacity
    */
   constructor(capacity = 16) {
-    const data = new Uint8Array(capacity);
+    const bytes = new Uint8Array(capacity);
+    const data = new DataView(bytes.buffer);
     privateFields.set(this, {
+      bytes,
       data,
       index: 0,
       length: 0,
@@ -47,8 +49,10 @@ export class BufferWriter {
     while (capacity < required) {
       capacity *= 2;
     }
-    const data = new Uint8Array(capacity);
-    data.set(fields.data.subarray(0, fields.length));
+    const bytes = new Uint8Array(capacity);
+    const data = new DataView(bytes.buffer);
+    bytes.set(fields.bytes.subarray(0, fields.length));
+    fields.bytes = bytes;
     fields.data = data;
     fields.capacity = capacity;
   }
@@ -77,7 +81,7 @@ export class BufferWriter {
   write(bytes) {
     const fields = privateFields.get(this);
     this.ensureCanWrite(bytes.length);
-    fields.data.set(bytes, fields.index);
+    fields.bytes.set(bytes, fields.index);
     fields.index += bytes.length;
     fields.length = Math.max(fields.index, fields.length);
   }
@@ -90,7 +94,7 @@ export class BufferWriter {
     const fields = privateFields.get(this);
     const size = end - start;
     this.ensureCanWrite(size);
-    fields.data.copyWithin(fields.index, start, end);
+    fields.bytes.copyWithin(fields.index, start, end);
     fields.index += size;
     fields.length = Math.max(fields.index, fields.length);
   }
@@ -101,35 +105,33 @@ export class BufferWriter {
   writeUint8(value) {
     const fields = privateFields.get(this);
     this.ensureCanWrite(1);
-    fields.data[fields.index] = value;
+    fields.data.setUint8(fields.index, value);
     fields.index += 1;
     fields.length = Math.max(fields.index, fields.length);
   }
 
   /**
    * @param {number} value
+   * @param {boolean=} littleEndian
    */
-  writeUint16LE(value) {
+  writeUint16(value, littleEndian) {
     const fields = privateFields.get(this);
     this.ensureCanWrite(2);
     const index = fields.index;
-    fields.data[index + 0] = value >>> 0;
-    fields.data[index + 1] = value >>> 8;
+    fields.data.setUint16(index, value, littleEndian);
     fields.index += 2;
     fields.length = Math.max(fields.index, fields.length);
   }
 
   /**
    * @param {number} value
+   * @param {boolean=} littleEndian
    */
-  writeUint32LE(value) {
+  writeUint32(value, littleEndian) {
     const fields = privateFields.get(this);
     this.ensureCanWrite(4);
     const index = fields.index;
-    fields.data[index + 0] = value >>> 0;
-    fields.data[index + 1] = value >>> 8;
-    fields.data[index + 2] = value >>> 16;
-    fields.data[index + 3] = value >>> 24;
+    fields.data.setUint32(index, value, littleEndian);
     fields.index += 4;
     fields.length = Math.max(fields.index, fields.length);
   }
@@ -141,7 +143,7 @@ export class BufferWriter {
    */
   subarray(begin, end) {
     const fields = privateFields.get(this);
-    return fields.data.subarray(0, fields.length).subarray(begin, end);
+    return fields.bytes.subarray(0, fields.length).subarray(begin, end);
   }
 
   /**
