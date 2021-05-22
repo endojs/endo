7badc82a851b9afe75c532c587a57b7f15d7a317
Kris Kowal <kris@agoric.com>
2021-05-21 21:46:12 -0700

    fix: Preinitialize Babel

--- test/test-external-fs.js
+++ test/test-external-fs.js
@@ -1,4 +1,7 @@
 /* global __dirname */
+// TODO Remove babel-standalone preinitialization
+// https://github.com/endojs/endo/issues/768
+import '@agoric/babel-standalone';
 import '@agoric/install-ses';
 import test from 'ava';
 import bundleSource from '..';
