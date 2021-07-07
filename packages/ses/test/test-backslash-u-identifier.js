a2b4f561945b4dddba81904b72cea0c02477e6cd
Mark S. Miller <erights@gmail.com>
2021-07-07 16:56:00 -0700

    fix: add tests for backslash behavior

---
+++ packages/ses/test/test-backslash-u-identifier.js
@@ -0,0 +1,34 @@
+import '../index.js';
+import './lockdown-safe.js';
+import test from 'ava';
+
+test('backslash-u-varname', t => {
+  const c = new Compartment();
+  t.is(
+    c.evaluate(`
+const \\u0069 = 88;
+i;`),
+    88,
+  );
+});
+
+test('backslash-u-keyward-as-var-fails', t => {
+  const c = new Compartment();
+  t.throws(
+    () => {
+      c.evaluate(`const \\u0069f = 55;`);
+    },
+    { instanceOf: SyntaxError },
+  );
+});
+
+test('backslash-u-keyward-fails', t => {
+  const c = new Compartment();
+  t.is(c.evaluate('if(true){99}'), 99);
+  t.throws(
+    () => {
+      c.evaluate(`u0069f(true){77}`);
+    },
+    { instanceOf: SyntaxError },
+  );
+});
