f8a1bcb4352b92234c2e4486f70169618ab70419
Kris Kowal <kris@agoric.com>
2021-07-07 15:43:14 -0700

    refactor(ses): Add TODO #814 Once-only lockdown

--- packages/ses/src/lockdown-shim.js
+++ packages/ses/src/lockdown-shim.js
@@ -74,6 +74,12 @@ const alreadyHardenedIntrinsics = () => false;
  * @returns {CompartmentConstructor}
  */
 
+// TODO https://github.com/endojs/endo/issues/814
+// Lockdown currently allows multiple calls provided that the specified options
+// of every call agree.  With experience, we have observed that lockdown should
+// only ever need to be called once and that simplifying lockdown will improve
+// the quality of audits.
+
 /**
  * @param {CompartmentConstructorMaker} makeCompartmentConstructor
  * @param {Object} compartmentPrototype
