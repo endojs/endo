9935ad0f40ee52c34a78649d3d0c2a4a4a5670bb
Michael FIG <mfig@agoric.com>
2021-11-18 18:06:51 -0600

    test(prepare-test-env-ava): redo using `@agoric/install-ses`

--- test/prepare-test-env-ava.js
+++ test/prepare-test-env-ava.js
@@ -1,10 +1,4 @@
-import 'ses';
-
-// TODO Remove babel-standalone preinitialization
-// https://github.com/endojs/endo/issues/768
-import '@agoric/babel-standalone';
-
-import './lockdown.js';
+import '@agoric/lockdown/commit-debug.js';
 
 import { wrapTest } from '@endo/ses-ava';
 import rawTest from 'ava';
