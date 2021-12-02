b923cf47f069abd29e3c8ed0a313d2d14e7ce73e
Michael FIG <mfig@agoric.com>
2021-12-02 10:14:42 -0600

    feat(far): export `ERef<T>`

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@ export { Far, getInterfaceOf, passStyleOf } from '@agoric/marshal';
 
 /**
  * @template T
+ * @typedef {import('@agoric/eventual-send').ERef<T>} ERef
+ * Declare that `T` can be either a near or far reference.  This should be used
+ * only for arguments and declarations; return values should specifically be
+ * `Promise<T>` or `T` itself.
+ */
+
+/**
+ * @template T
  * @typedef {import('@agoric/eventual-send').EOnly<T>} EOnly
+ * A type that must only be invoked with E.  It supports the `T` interface
+ * interface but additionally permits functions to return `PromiseLike`s even if
+ * `T` declares them as only synchronous.
  */
