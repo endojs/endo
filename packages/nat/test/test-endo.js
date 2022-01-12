920d478532b0c830962de92a19f02f9be6a8a546
Kris Kowal <kris@agoric.com>
2022-01-12 15:34:33 -0800

    fix(nat): Thread Windows read powers

--- packages/nat/test/test-endo.js
+++ packages/nat/test/test-endo.js
@@ -1,15 +1,16 @@
 import 'ses';
 import fs from 'fs';
 import test from 'ava';
+import url from 'url';
 import { importLocation } from '@endo/compartment-mapper';
+import { makeReadPowers } from '@endo/compartment-mapper/node-powers.js';
 
+const readPowers = makeReadPowers({ fs, url });
 const entry = new URL('../src/index.js', import.meta.url).toString();
-
-const read = async location => fs.promises.readFile(new URL(location).pathname);
 
 test('endo can import nat', async t => {
   const {
     namespace: { isNat },
-  } = await importLocation(read, entry);
+  } = await importLocation(readPowers, entry);
   t.assert(isNat(0n));
 });
