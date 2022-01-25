9d9c3eda65b03359548c6d6d831751610c24c7d1
Kris Kowal <kris@agoric.com>
2022-01-25 15:41:44 -0800

    refactor: Remove former Endo prototype

--- packages/endo/bin/endo.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-/* global process */
-(async () => {
-  const fs = await import('fs');
-  const crypto = await import('crypto');
-  const { main } = await import('../src/cli.js');
-  main(process, { fs, crypto });
-})();
