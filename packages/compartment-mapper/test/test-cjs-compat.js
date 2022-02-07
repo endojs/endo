21de7db653bde4f9defe747213f278584d2ddcb1
naugtur <naugtur@gmail.com>
2022-02-07 11:52:51 +0100

    test(compartment-mapper): Failing test for CommonJS compatibility - export name collisions

--- packages/compartment-mapper/test/test-cjs-compat.js
+++ packages/compartment-mapper/test/test-cjs-compat.js
@@ -1,5 +1,5 @@
 // import "./ses-lockdown.js";
-import 'ses';
+import '../../ses/index.js';
 import fs from 'fs';
 import url from 'url';
 import test from 'ava';
@@ -18,6 +18,25 @@ test('CommonJS referencing exports', async t => {
   ).toString();
 
   const application = await loadLocation(read, fixture);
-  await application.import({});
+  const {
+    namespace: { assertions },
+  } = await application.import({});
+  assertions.packageReferencingItself();
+  t.pass();
+});
+test('CommonJS exports include a field named `default`', async t => {
+  t.plan(1);
+
+  const fixture = new URL(
+    'fixtures-cjs-compat/node_modules/app/index.js',
+    import.meta.url,
+  ).toString();
+
+  const application = await loadLocation(read, fixture);
+  const {
+    namespace: { assertions },
+  } = await application.import({});
+  assertions.packageWithDefaultField();
+  assertions.moduleWithDefaultField();
   t.pass();
 });
