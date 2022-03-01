c85a729792c6e5b4604f9bf9fa67391e03d36a5c
Kris Kowal <kris@agoric.com>
2022-02-28 20:27:10 -0800

    feat(check-bundle): Implement bundle integrity checks

---
+++ packages/check-bundle/test/test-check-bundle.js
@@ -0,0 +1,90 @@
+// @ts-check
+import '@endo/init/pre-bundle-source.js';
+import '@endo/init';
+import test from 'ava';
+import * as url from 'url';
+import * as crypto from 'crypto';
+import bundleSource from '@endo/bundle-source';
+import { checkBundle } from '../lite.js';
+import { checkBundleBytes } from '../index.js';
+
+const fixture = url.fileURLToPath(
+  new URL('fixture/main.js', import.meta.url).toString(),
+);
+
+/** @param {Uint8Array} bytes */
+const computeSha512 = bytes => {
+  const hash = crypto.createHash('sha512');
+  hash.update(bytes);
+  return hash.digest().toString('hex');
+};
+
+test('bundle and check get export package', async t => {
+  const bundle = await bundleSource(fixture, 'getExport');
+  await t.throwsAsync(checkBundle(bundle, computeSha512, 'fixture/main.js'), {
+    message: /checkBundle cannot determine hash of bundle with getExport moduleFormat because it is not necessarily consistent/,
+  });
+});
+
+test('bundle and check nested evaluate package', async t => {
+  const bundle = await bundleSource(fixture, 'nestedEvaluate');
+  await t.throwsAsync(checkBundle(bundle, computeSha512, 'fixture/main.js'), {
+    message: /checkBundle cannot determine hash of bundle with nestedEvaluate moduleFormat because it is not necessarily consistent/,
+  });
+});
+
+test('bundle and check endo zip base64 package', async t => {
+  const bundle = await bundleSource(fixture, 'endoZipBase64');
+  await checkBundle(bundle, computeSha512, 'fixture/main.js');
+  t.pass();
+});
+
+test('bundle and check endo zip base64 package (lite)', async t => {
+  const bundle = await bundleSource(fixture, 'endoZipBase64');
+  await checkBundleBytes(bundle, 'fixture/main.js');
+  t.pass();
+});
+
+test('bundle and check endo zip base64 package absent hash', async t => {
+  const { endoZipBase64Sha512: _, ...lightBundle } = await bundleSource(
+    fixture,
+    'endoZipBase64',
+  );
+  Object.freeze(lightBundle);
+  await t.throwsAsync(
+    checkBundle(lightBundle, computeSha512, 'fixture/main.js'),
+    {
+      message:
+        "checkBundle cannot bundle without the property 'endoZipBase64Sha512', which must be a string, got (a string)",
+    },
+  );
+});
+
+test('bundle and check corrupt endo zip base64 package', async t => {
+  const bundle = await bundleSource(fixture, 'endoZipBase64');
+  const corruptBundle = harden({
+    ...bundle,
+    endoZipBase64Sha512:
+      '00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+  });
+  await t.throwsAsync(
+    checkBundle(corruptBundle, computeSha512, 'fixture/main.js'),
+    {
+      message: /^Archive compartment map failed a SHA-512 integrity check/,
+    },
+  );
+});
+
+test('bundle and hash unfrozen object', async t => {
+  const bundle = {};
+  await t.throwsAsync(checkBundle(bundle, computeSha512, 'fixture/main.js'), {
+    message: `checkBundle cannot vouch for the ongoing integrity of an unfrozen object, got {}`,
+  });
+});
+
+test('bundle and hash bogus package', async t => {
+  const bundle = Object.freeze({ moduleFormat: 'bogus' });
+  await t.throwsAsync(checkBundle(bundle, computeSha512, 'fixture/main.js'), {
+    message: `checkBundle cannot determine hash of bundle with unrecognized moduleFormat "bogus"`,
+  });
+});
