91c5104c03563c81e5024a3171879cd1f8b57ccd
Turadg Aleahmad <turadg@agoric.com>
2022-03-15 16:51:17 -0700

    chore: upgrade TypeScript to 4.6

--- scripts/repackage.sh
+++ scripts/repackage.sh
@@ -97,7 +97,7 @@ NEWPKGJSONHASH=$(
       "eslint-plugin-import": "^2.19.1",
       "eslint-plugin-prettier": "^3.1.2",
       "prettier": "^1.19.1",
-      "typescript": "~4.5.5",
+      "typescript": "~4.6.2",
       "ava": "^3.12.1",
     }) | to_entries | sort_by(.key) | from_entries,
     files: ((.files // []) + [
