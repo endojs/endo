98d1c7923ce849dda2e9935b5a43bbb4fae83544
Mathieu Hofman <mathieu@agoric.com>
2022-03-17 01:39:07 +0000

    Add failing test for @endo/init compat with bundle

---
+++ packages/init/test/test-bundle.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+import fs from 'fs';
+import url, { URL } from 'url';
+import crypto from 'crypto';
+
+import '../debug.js';
+import { wrapTest } from '@endo/ses-ava';
+import rawTest from 'ava';
+
+import { makeArchive } from '@endo/compartment-mapper';
+import { makeReadPowers } from '@endo/compartment-mapper/node-powers.js';
+
+const test = wrapTest(rawTest);
+
+test.failing('Can be bundled', async t => {
+  const powers = makeReadPowers({ fs, url, crypto });
+
+  await t.notThrowsAsync(() =>
+    makeArchive(
+      powers,
+      new URL('./fixture/ses-boot.js', import.meta.url).toString(),
+    ),
+  );
+});
