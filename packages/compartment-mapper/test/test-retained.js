0b8fb008f2478a0778cb8a4b657793f1626e8fb7
Kris Kowal <kris@agoric.com>
2022-04-25 18:17:24 -0700

    test(compartment-mapper): Test compartment map retention

---
+++ packages/compartment-mapper/test/test-retained.js
@@ -0,0 +1,35 @@
+// @ts-check
+import 'ses';
+import test from 'ava';
+import { ZipReader } from '@endo/zip';
+import { makeArchive, parseArchive } from '../index.js';
+import { readPowers } from './scaffold.js';
+
+const fixture = new URL(
+  'fixtures-retained/node_modules/app/app.js',
+  import.meta.url,
+).toString();
+
+test('archives only contain compartments retained by modules', async t => {
+  const bytes = await makeArchive(readPowers, fixture);
+
+  await t.notThrowsAsync(async () => {
+    const app = await parseArchive(bytes);
+    const {
+      namespace: { mark },
+    } = await app.import();
+    t.is(mark, 42);
+  });
+
+  const reader = new ZipReader(bytes);
+  const compartmentMapBytes = reader.files.get('compartment-map.json').content;
+  const compartmentMapText = new TextDecoder().decode(compartmentMapBytes);
+  const compartmentMap = JSON.parse(compartmentMapText);
+  t.deepEqual(Object.keys(compartmentMap.compartments), [
+    'app-v1.0.0-n0',
+    'mk1-v1.0.0-n1',
+    'mk2-v1.0.0-n2',
+    // Notably absent:
+    // 'sweep-v1.0.0-*',
+  ]);
+});
