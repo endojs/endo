03a6e76b22651d341f0acf18b4f3c8ed39d58923
Michael FIG <mfig@agoric.com>
2022-05-08 16:41:09 -0600

    fix(integration-test): report an unhandled rejection (not file://)
    Note that for browsers, you need to use imported code or scripts
    served from the same origin, and not file://... in order to see
    the unhandled rejection reports.
    
    Node.js works fine.

--- packages/ses-integration-test/transform-tests/config/rollup.config.cjs.js
+++ packages/ses-integration-test/transform-tests/config/rollup.config.cjs.js
@@ -5,7 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default [
   {
     input: {
-      include: ['test/**/*.js'],
+      include: ['test/**/*.js', 'test/**/*.mjs'],
     },
     output: {
       file: 'transform-tests/output/test.cjs.js',
