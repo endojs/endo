ab01dedfa27031e8a1150387117ddccfd9a64c0d
Mark S. Miller <erights@gmail.com>
2022-07-17 20:16:25 -0700

    fix: correct mistaken "thunk" comment

--- packages/ses-ava/src/types.js
+++ packages/ses-ava/src/types.js
@@ -7,19 +7,21 @@
 /**
  * @callback LogCallError
  *
- * Calls `thunk()` passing back approximately its outcome, but first
+ * Calls `func(...args)` passing back approximately its outcome, but first
  * logging any erroneous outcome to the `logger`, which defaults to
  * `console.log`.
  *
- * If thunk returns a non-promise, silently return it.
- * If thunk throws, log what was thrown and then rethrow it.
- * If thunk returns a promise, immediately return a new unresolved promise.
- * If the first promise fulfills, silently fulfill the returned promise even if
- * the fulfillment was an error.
- * If the first promise rejects, log the rejection reason and then reject the
- * returned promise with the same reason.
+ *    * If `func(...args)` returns a non-promise, silently return it.
+ *    * If `func(...args)` throws, log what was thrown and then rethrow it.
+ *    * If `func(...args)` returns a promise, immediately return a new
+ *      unresolved promise.
+ *       * If the first promise fulfills, silently fulfill the returned promise
+ *         even if the fulfillment was an error.
+ *       * If the first promise rejects, log the rejection reason and then
+ *         reject the returned promise with the same reason.
+ *
  * The delayed rejection of the returned promise is an observable difference
- * from directly calling `thunk()` but will be equivalent enough for most
+ * from directly calling `func(...args)` but will be equivalent enough for most
  * purposes.
  *
  * TODO This function is useful independent of ava, so consider moving it
