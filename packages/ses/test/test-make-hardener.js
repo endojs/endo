dc82f5d2908b3507965562c7c1b3bf12d852af8f
Richard Gibson <richard.gibson@gmail.com>
2022-10-18 15:51:16 -0400

    fix(ses): Lock down all typed array expando properties
    Catch properties named as number-coercible strings that are not in canonical form,
    such as `""` or `"+0"` or 1000000000000000000000.

--- packages/ses/test/test-make-hardener.js
+++ packages/ses/test/test-make-hardener.js
@@ -2,6 +2,9 @@
 
 import test from 'ava';
 import { makeHardener } from '../src/make-hardener.js';
+import { assert } from '../src/error/assert.js';
+
+const { quote: q } = assert;
 
 test('makeHardener', t => {
   const h = makeHardener();
@@ -102,57 +105,99 @@ test('harden typed arrays', t => {
 test('harden typed arrays and their expandos', t => {
   const h = makeHardener();
   const a = new Uint8Array(1);
-  // @ts-ignore
-  a.x = { y: { z: 10 } };
 
-  t.is(h(a), a);
-  t.truthy(Object.isSealed(a));
+  // TODO: Use fast-check to generate arbitrary input.
+  const expandoKeys = [
+    // number-coercible strings that are not in canonical form
+    // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-canonicalnumericindexstring
+    // https://tc39.es/ecma262/multipage/abstract-operations.html#prod-StringNumericLiteral
+    '',
+    ' ',
+    ' 0',
+    '0 ',
+    '0b0',
+    '0o0',
+    '0x0',
+    '+0',
+    // disabled because V8 misinterprets as canonical -0 anything matched by
+    // /-(0+(\.0*)?|\.0+)/ with optional leading and/or trailing StrWhiteSpace
+    // '-0 ',
+    '00',
+    '.0',
+    '0.',
+    '9007199254740993', // reserializes to "9007199254740992" (Number.MAX_SAFE_INTEGER + 1)
+    '0.0000001', // reserializes to "1e-7"
+    '1000000000000000000000', // reserializes to "1e+21"
 
+    // other strings
+    'x',
+
+    // symbols
+    Symbol('unique symbol'),
+    Symbol.for('registered symbol'),
+    Symbol.match,
+  ];
+  for (const key of expandoKeys) {
+    a[key] = { a: { b: { c: 10 } } };
+  }
+
+  t.is(h(a), a, 'harden() must return typed array input');
+  t.deepEqual(
+    Reflect.ownKeys(a),
+    ['0'].concat(expandoKeys),
+    'hardened typed array keys must exactly match pre-hardened keys',
+  );
+
+  // Index properties remain writable.
   {
     const descriptor = Object.getOwnPropertyDescriptor(a, '0');
-    t.is(descriptor.value, 0);
+    t.like(
+      descriptor,
+      { value: 0, writable: true, enumerable: true },
+      'hardened typed array index property',
+    );
     // Fails in Node.js 14 and earlier due to an engine bug:
     // t.is(descriptor.configurable, true, 'typed array indexed property is configurable');
     // Note that indexes of typed arrays are exceptionally writable for hardened objects:
-    t.is(
-      descriptor.writable,
-      true,
-      'hardened typed array indexed property is writable',
+  }
+
+  // Non-index properties are locked down.
+  for (const key of expandoKeys) {
+    const descriptor = Object.getOwnPropertyDescriptor(a, key);
+    t.like(
+      descriptor,
+      { configurable: false, writable: false, enumerable: true },
+      `hardened typed array expando ${q(key)}`,
     );
     t.is(
-      descriptor.enumerable,
-      true,
-      'hardened typed array indexed property is enumerable',
+      descriptor.value,
+      a[key],
+      `hardened typed array expando ${q(key)} value identity`,
+    );
+    t.deepEqual(
+      descriptor.value,
+      { a: { b: { c: 10 } } },
+      `hardened typed array expando ${q(key)} value shape`,
+    );
+    t.truthy(
+      Object.isFrozen(descriptor.value),
+      `hardened typed array expando ${q(key)} value is frozen`,
+    );
+    t.truthy(
+      Object.isFrozen(descriptor.value.a),
+      `hardened typed array expando ${q(key)} value property is frozen`,
+    );
+    t.truthy(
+      Object.isFrozen(descriptor.value.a.b),
+      `hardened typed array expando ${q(key)} value subproperty is frozen`,
+    );
+    t.truthy(
+      Object.isFrozen(descriptor.value.a.b.c),
+      `hardened typed array expando ${q(key)} value sub-subproperty is frozen`,
     );
   }
 
-  {
-    const descriptor = Object.getOwnPropertyDescriptor(a, 'x');
-    // @ts-ignore
-    t.is(descriptor.value, a.x);
-    t.is(
-      descriptor.configurable,
-      false,
-      'hardened typed array indexed property is configurable',
-    );
-    t.is(
-      descriptor.writable,
-      false,
-      'hardened typed array expando is not writable',
-    );
-    t.is(
-      descriptor.enumerable,
-      true,
-      'hardened typed array expando is enumerable',
-    );
-  }
-
-  // @ts-ignore
-  t.truthy(Object.isFrozen(a.x));
-  // @ts-ignore
-  t.truthy(Object.isFrozen(a.x.y));
-  // @ts-ignore
-  t.truthy(Object.isFrozen(a.x.y.z));
+  t.truthy(Object.isSealed(a), 'hardened typed array is sealed');
 });
 
 test('hardening makes writable properties readonly even if non-configurable', t => {
@@ -166,15 +211,12 @@ test('hardening makes writable properties readonly even if non-configurable', t 
   });
   h(o);
 
-  t.deepEqual(
-    {
-      value: 10,
-      writable: false,
-      configurable: false,
-      enumerable: false,
-    },
-    Object.getOwnPropertyDescriptor(o, 'x'),
-  );
+  t.deepEqual(Object.getOwnPropertyDescriptor(o, 'x'), {
+    value: 10,
+    writable: false,
+    configurable: false,
+    enumerable: false,
+  });
 });
 
 test('harden a typed array with a writable non-configurable expando', t => {
@@ -211,17 +253,15 @@ test('harden a typed array subclass', t => {
   const a = new Ooint8Array(1);
   t.is(h(a), a);
 
-  t.deepEqual(
-    {
-      value: 'ghosts',
-      writable: false,
-      configurable: false,
-      enumerable: true,
-    },
-    Object.getOwnPropertyDescriptor(a, 'oo'),
-  );
+  t.deepEqual(Object.getOwnPropertyDescriptor(a, 'oo'), {
+    value: 'ghosts',
+    writable: false,
+    configurable: false,
+    enumerable: true,
+  });
   t.truthy(Object.isFrozen(Ooint8Array.prototype));
   t.truthy(Object.isFrozen(Object.getPrototypeOf(Ooint8Array.prototype)));
+  t.truthy(Object.isSealed(a));
 });
 
 test('harden depends on invariant: typed arrays have no storage for integer indexes beyond length', t => {
