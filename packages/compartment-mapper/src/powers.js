ca10a0e7c513bb4783feab494ce74219242e83a7
Turadg Aleahmad <turadg@agoric.com>
2022-11-03 09:16:28 -0700

    chore: check powers.js

--- packages/compartment-mapper/src/powers.js
+++ packages/compartment-mapper/src/powers.js
@@ -1,3 +1,8 @@
+// @ts-check
+/** @typedef {import('./types.js').ReadFn} ReadFn */
+/** @typedef {import('./types.js').CanonicalFn} CanonicalFn */
+/** @typedef {import('./types.js').ReadPowers} ReadPowers */
+
 /** @type {CanonicalFn} */
 const canonicalShim = async path => path;
 
