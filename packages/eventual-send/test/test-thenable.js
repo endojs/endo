48f24c093ae9de8c65984df71fd64ea6afce9b29
Mark S. Miller <erights@gmail.com>
2022-12-06 16:10:11 -0800

    fix: review suggestions

--- packages/eventual-send/test/test-thenable.js
+++ packages/eventual-send/test/test-thenable.js
@@ -3,76 +3,75 @@ import { test } from './prepare-test-env-ava.js';
 
 import { E, HandledPromise } from './get-hp.js';
 
-test('E.resolve does not coerce simple promise', async t => {
+const verifySimplePromise = async (t, resolve) => {
   const p = new Promise(_ => {});
-  const p2 = E.resolve(p);
+  const p2 = resolve(p);
   t.is(p, p2, 'simple promise should not coerce');
-});
+};
+test(
+  'E.resolve does not coerce simple promise',
+  verifySimplePromise,
+  E.resolve,
+);
+test(
+  'HandledPromise.resolve does not coerce simple promise',
+  verifySimplePromise,
+  HandledPromise.resolve,
+);
 
-test('HandledPromise.resolve does not coerce simple promise', async t => {
+const verifyThenAttack = async (t, resolve) => {
   const p = new Promise(_ => {});
-  const p2 = HandledPromise.resolve(p);
-  t.is(p, p2, 'simple promise should not coerce');
-});
-
-test('E.resolve protects against "then" attack', async t => {
-  const p = new Promise(_ => {});
+  let getHappened = false;
+  let callHappened = false;
   // Work around the override mistake on SES.
-  Object.defineProperty(p, 'then', { value: (res, _rej) => res('done') });
-  let happened = false;
-  const p2 = E.resolve(p).then(ret => (happened = ret));
+  Object.defineProperty(p, 'then', {
+    get() {
+      getHappened = true;
+      return (res, _rej) => {
+        callHappened = true;
+        res('done');
+      };
+    },
+  });
+  const p2 = resolve(p).then(ret => ret);
   t.not(p, p2, 'an own "then" should cause coercion');
-  t.is(happened, false, `p2 is not yet resolved`);
+  t.is(getHappened, false, `getter not called too early`);
+  t.is(callHappened, false, `then not called too early`);
   t.is(await p2, 'done', `p2 is resolved`);
-});
+  t.is(getHappened, true, `getter not called too early`);
+  t.is(callHappened, true, `then not called too early`);
+};
+test('E.resolve protects against "then" attack', verifyThenAttack, E.resolve);
+test(
+  'HandledPromise.resolve protects against "then" attack',
+  verifyThenAttack,
+  HandledPromise.resolve,
+);
 
-test('HandledPromise.resolve protects against "then" attack', async t => {
+const verifyConstructorAttack = async (t, resolve) => {
   const p = new Promise(_ => {});
-  // Work around the override mistake on SES.
-  Object.defineProperty(p, 'then', { value: (res, _rej) => res('done') });
-  let happened = false;
-  const p2 = HandledPromise.resolve(p).then(ret => (happened = ret));
-  t.not(p, p2, 'an own "then" should cause coercion');
-  t.is(happened, false, `p2 is not yet resolved`);
-  t.is(await p2, 'done', `p2 is resolved`);
-});
-
-test('E.resolve protects against "constructor" attack', async t => {
-  const p = new Promise(_ => {});
-  let happened = false;
+  let getHappened = false;
   // Work around the override mistake on SES.
   Object.defineProperty(p, 'constructor', {
     get() {
-      happened = true;
+      getHappened = true;
       return Promise;
     },
   });
-  const p2 = E.resolve(p);
+  const p2 = resolve(p);
   t.not(p, p2, 'an own "constructor" should cause coercion');
-  // This first 'true' demonstrates our remaining vulnerability to reentrancy.
+  // This 'true' demonstrates our remaining vulnerability to reentrancy.
   // But the fact that p coerced to a fresh p2 means that p2 cannot
   // cause a reentrancy attack
-  t.is(happened, true, `same turn`);
-  await null;
-  t.is(happened, true, `later turn with p still unresolved`);
-});
-
-test('HandledPromise.resolve protects against "constructor" attack', async t => {
-  const p = new Promise(_ => {});
-  let happened = false;
-  // Work around the override mistake on SES.
-  Object.defineProperty(p, 'constructor', {
-    get() {
-      happened = true;
-      return Promise;
-    },
-  });
-  const p2 = HandledPromise.resolve(p);
-  t.not(p, p2, 'an own "constructor" should cause coercion');
-  // This first 'true' demonstrates our remaining vulnerability to reentrancy.
-  // But the fact that p coerced to a fresh p2 means that p2 cannot
-  // cause a reentrancy attack
-  t.is(happened, true, `same turn`);
-  await null;
-  t.is(happened, true, `later turn with p still unresolved`);
-});
+  t.is(getHappened, true, `same turn`);
+};
+test(
+  'E.resolve protects against "constructor" attack',
+  verifyConstructorAttack,
+  E.resolve,
+);
+test(
+  'HandledPromise.resolve protects against "constructor" attack',
+  verifyConstructorAttack,
+  HandledPromise.resolve,
+);
