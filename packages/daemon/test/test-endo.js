7a90d940505d0c26c4413683033f435c749f540d
Kris Kowal <kris@agoric.com>
2023-01-07 16:34:16 -0800

    test(daemon): Test store

--- packages/daemon/test/test-endo.js
+++ packages/daemon/test/test-endo.js
@@ -18,6 +18,7 @@ import {
   clean,
   reset,
   makeEndoClient,
+  makeReaderRef,
 } from '../index.js';
 
 const { raw } = String;
@@ -140,4 +141,36 @@ test.serial('persist spawn and evaluation', async t => {
   }
 
   await stop(locator);
+});
+
+test.serial('store', async t => {
+  const { promise: cancelled } = makePromiseKit();
+  const locator = makeLocator('tmp', 'store');
+
+  await stop(locator).catch(() => {});
+  await reset(locator);
+  await start(locator);
+
+  {
+    const { getBootstrap } = await makeEndoClient(
+      'client',
+      locator.sockPath,
+      cancelled,
+    );
+    const bootstrap = getBootstrap();
+    const readerRef = makeReaderRef([new TextEncoder().encode('hello\n')]);
+    await E(bootstrap).store(readerRef, 'helloText');
+  }
+
+  {
+    const { getBootstrap } = await makeEndoClient(
+      'client',
+      locator.sockPath,
+      cancelled,
+    );
+    const bootstrap = getBootstrap();
+    const readable = await E(bootstrap).provide('helloText');
+    const actualText = await E(readable).text();
+    t.is(actualText, 'hello\n');
+  }
 });
