41e7cf50acfc8dad56fbb429139c589e7446b869
Kris Kowal <kris@agoric.com>
2023-01-07 16:12:29 -0800

    feat(daemon): Export types for makeEndoClient, readers and iterator references

--- packages/daemon/index.d.ts
+++ packages/daemon/index.d.ts
@@ -7,15 +7,13 @@ type Locator = {
   sockPath: string;
 };
 
+export { makeEndoClient } from './src/client.js';
+export { makeRefReader, eventualIterator } from './src/ref-reader.js';
+export { makeReaderRef } from './src/reader-ref.js';
+
 export async function start(locator?: Locator);
 export async function stop(locator?: Locator);
 export async function restart(locator?: Locator);
 export async function terminate(locator?: Locator);
 export async function clean(locator?: Locator);
 export async function reset(locator?: Locator);
-export async function makeEndoClient<TBootstrap>(
-  name: string,
-  sockPath: string,
-  cancelled: Promise<void>,
-  bootstrap?: TBootstrap,
-);
