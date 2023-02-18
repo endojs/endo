5a6500de51b3a7ac40231961d252346fdc8d9664
Mathieu Hofman <mathieu@agoric.com>
2023-02-18 23:09:56 +0000

    Make SES challenge async Release a Zalgo side channel

--- packages/ses/demos/challenge/main.js
+++ packages/ses/demos/challenge/main.js
@@ -113,6 +113,7 @@ lockdown();
   // ********************
 
   let enableAttacker = false;
+  let attackerExecutionResult = Promise.resolve();
   function stopAttacker() {
     enableAttacker = false;
     setLaunch(false);
@@ -122,7 +123,7 @@ lockdown();
     setLaunch(true);
   }
 
-  function guess(guessedCode) {
+  async function guess(guessedCode) {
     // guess() is an exposed API: we must cast into a String to avoid TOCTOU.
     guessedCode = `${guessedCode}`;
 
@@ -133,8 +134,8 @@ lockdown();
     // right, enabling a linear-time guessing attack.
 
     setAttackerGuess(guessedCode);
-    for (let i = 0; i < 10; i++) {
-      if (secretCode.slice(i, i + 1) !== guessedCode.slice(i, i + 1)) {
+    for await (const [i, char] of [...secretCode].entries()) {
+      if (guessedCode.at(i) !== char) {
         return false;
       }
       delayMS(10);
@@ -153,30 +154,43 @@ lockdown();
     ...dateEndowment,
   });
 
-  function submitProgram(program) {
+  async function submitProgram(program) {
     // the attacker's code will be submitted here. We expect it to be a
     // generator function, starting with 'function*' and ending with the
     // closing curly brace
 
     startAttacker();
 
-    const attacker = compartment.evaluate(`(${program})`);
-    const attackGen = attacker(); // build the generator
-    function nextGuess() {
+    attackerExecutionResult = new Promise(resolve => {
+      const attacker = compartment.evaluate(`(${program})`);
+      resolve(attacker()); // build the generator
+    }).catch(err => {
+      console.log('Failed to execute attacker:', err);
+      stopAttacker();
+    });
+    const attackAsyncGen = await attackerExecutionResult;
+    async function nextGuess() {
       if (!enableAttacker) {
-        return; // attacker was interrupted, so don't ask
+        return undefined; // attacker was interrupted, so don't ask
       }
       // give the attacker another chance to run
-      if (attackGen.next().done) {
-        return; // attacker gave up, so stop asking
+      attackerExecutionResult = Promise.resolve()
+        .then(() => attackAsyncGen.next())
+        .catch(err => {
+          console.log('Failed to execute attacker guess:', err);
+          return { done: true };
+        });
+      const guessResult = await attackerExecutionResult;
+      if (guessResult.done) {
+        return undefined; // attacker gave up, so stop asking
       }
       if (!enableAttacker) {
-        return; // attacker was correct, so stop asking
+        return undefined; // attacker was correct or was interrupted, so stop asking
       }
       // now let the UI refresh before we call attacker again
-      refreshUI().then(nextGuess);
+      return refreshUI().then(nextGuess);
     }
-    nextGuess();
+    return nextGuess();
   }
 
   // ********************
@@ -191,10 +205,7 @@ lockdown();
     stopAttacker();
 
     // wait a moment to make sure the running program notices the stop request
-    const wait = new Promise((resolve, _reject) =>
-      window.setTimeout(resolve, 10, undefined),
-    );
-    wait.then(() => submitProgram(code));
+    attackerExecutionResult.then(() => submitProgram(code));
   });
 
   $('#attacker-stop').addEventListener('click', _event => {
@@ -207,24 +218,25 @@ lockdown();
   // ********************
   // 5. We provide some sample attacks
   // ********************
+  /* eslint-disable no-await-in-loop */
 
   // IMPORTANT: These function are stingnified and evaluated in the compartent.
   // They pass the linter since the function guess() does resolved inside this file.
 
-  function* allZeros() {
-    guess('0000000000');
+  async function* allZeros() {
+    await guess('0000000000');
     yield;
   }
 
-  function* counter() {
+  async function* counter() {
     for (let i = 0; true; i++) {
       const guessedCode = i.toString(36).toUpperCase().padStart(10, '0');
-      guess(guessedCode);
+      await guess(guessedCode);
       yield;
     }
   }
 
-  function* timing() {
+  async function* timing() {
     function toChar(c) {
       return c.toString(36).toUpperCase();
     }
@@ -257,7 +269,7 @@ lockdown();
         for (let c = 0; c < 36; c++) {
           const guessedCode = buildCode(base, offset, c);
           const start = Date.now();
-          guess(guessedCode);
+          await guess(guessedCode);
           const elapsed = Date.now() - start;
           delays.set(toChar(c), elapsed);
           yield; // allow UI to refresh
@@ -275,6 +287,71 @@ lockdown();
     }
   }
 
+  async function* zalgo() {
+    function toChar(c) {
+      return c.toString(36).toUpperCase();
+    }
+
+    function insert(into, offset, char) {
+      return into.slice(0, offset) + char + into.slice(offset + 1, into.length);
+    }
+
+    function buildCode(base, offset, c) {
+      // keep the first 'offset' chars of base, set [offset] to c, fill the
+      // rest with random-looking junk to make the demo look cool
+      // (random-looking, not truly random, because we're deterministic)
+      let code = insert(base, offset, toChar(c));
+      for (let off2 = offset + 1; off2 < 10; off2++) {
+        code = insert(code, off2, toChar((off2 * 3 + c * 7) % 36));
+      }
+      return code;
+    }
+
+    function fastestChar(delays) {
+      const pairs = Array.from(delays.entries());
+      pairs.sort((a, b) => b[1] - a[1]);
+      return pairs[0][0];
+    }
+
+    async function countTurns(stop) {
+      let turns = 0;
+      let keepGoing = true;
+      while (keepGoing === true) {
+        keepGoing = await Promise.race([stop, Promise.resolve(true)]);
+        turns += 1;
+        if (turns > 10000) {
+          throw new Error('Possible infinite loop');
+        }
+      }
+      return turns;
+    }
+
+    let base = '0000000000';
+    while (true) {
+      for (let offset = 0; offset < 10; offset++) {
+        const turns = new Map();
+        for (let c = 0; c < 36; c++) {
+          const guessedCode = buildCode(base, offset, c);
+          const guessed = Promise.resolve()
+            .then(() => guess(guessedCode))
+            .then(() => false);
+          const elapsed = await countTurns(guessed);
+          turns.set(toChar(c), elapsed);
+          yield; // allow UI to refresh
+          // if our guess was right, then on the last character
+          // (offset===9) we never actually reach here, since we guessed
+          // correctly earlier, and when the attacker guesses correctly,
+          // the defender stops calling go()
+        }
+        console.log(turns);
+        const nextChar = fastestChar(turns);
+        base = insert(base, offset, nextChar);
+        console.log(`Setting code[${offset}]=${nextChar} -> ${base}`);
+      }
+      console.log('we must have measured the timings wrong, try again');
+    }
+  }
+
   $('#sample-0').addEventListener('click', () => {
     setProgram(`${allZeros}`);
   });
@@ -285,6 +362,10 @@ lockdown();
 
   $('#sample-timing').addEventListener('click', () => {
     setProgram(`${timing}`);
+  });
+
+  $('#sample-zalgo').addEventListener('click', () => {
+    setProgram(`${zalgo}`);
   });
 
   console.log('loaded');
