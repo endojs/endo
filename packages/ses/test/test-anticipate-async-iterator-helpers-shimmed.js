4816b774dfd44fcf7fc4efb125a7e144182a6f5f
Mathieu Hofman <mathieu@agoric.com>
2023-07-06 21:20:54 +0000

    chore(ses): make async-iterator test more similar to sync-iterator

--- packages/ses/test/test-anticipate-async-iterator-helpers-shimmed.js
+++ packages/ses/test/test-anticipate-async-iterator-helpers-shimmed.js
@@ -19,8 +19,21 @@ AsyncFunctionInstance.caller = {};
 // so it creates a new one, causing us to fail because lockdown correctly
 // detects the conflicting definitions.
 // TODO report the bug to core-js
-test.skip('shimmed async-iterator helpers', t => {
+test.skip('shimmed async-iterator helpers', async t => {
   lockdown();
+  
+  t.deepEqual(
+    await (async function* g(i) {
+      // eslint-disable-next-line no-plusplus
+      while (true) yield i++;
+    })(1)
+      .drop(1)
+      .take(5)
+      .filter(it => it % 2)
+      .map(it => it ** 2)
+      .toArray(),
+    [9, 25],
+  );
 
   const AsyncIteratorHelperPrototype = Object.getPrototypeOf(
     AsyncIterator.from([]).take(0),
