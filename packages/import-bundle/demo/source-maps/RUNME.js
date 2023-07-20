528c12b02ff0b07fb45f2f1af3c3d037f26f35b4
Kris Kowal <kris@agoric.com>
2023-07-20 15:09:31 -0700

    feat(import-bundle): Add source map demo

---
+++ packages/import-bundle/demo/source-maps/RUNME.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node --inspect-brk
+
+// This script demonstrates the use of computeSourceMapLocation in Node.js to
+// employ the Endo source map cache for debugging.
+
+/* eslint-disable */
+
+import 'ses';
+import url from 'url';
+import bundleSource from '@endo/bundle-source';
+import { importBundle } from '../../src/index.js';
+import { computeSourceMapLocation } from '../../src/source-map-node.js';
+
+lockdown();
+
+const start = url.fileURLToPath(new URL('start.js', import.meta.url));
+
+const bundle = await bundleSource(start, {
+  cacheSourceMaps: true,
+});
+await importBundle(
+  bundle,
+  {
+    endowments: { console },
+  },
+  {
+    computeSourceMapLocation,
+  },
+);
---
+++ packages/import-bundle/demo/source-maps/debug.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+console.log('debug (before breakpoint)');
+const n = 10;
+debugger;
+console.log('debug (after breakpoint)');
