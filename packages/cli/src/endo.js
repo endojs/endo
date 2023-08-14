1921888dc24bf26953a4f0357c6537e8c45e6bc4
Kris Kowal <kris@agoric.com>
2023-08-14 16:25:01 -0700

    refactor(cli): Move mkguest to command library

--- packages/cli/src/endo.js
+++ packages/cli/src/endo.js
@@ -413,23 +413,8 @@ export const main = async rawArgs => {
     .description('creates new guest powers, pet store, and mailbox')
     .action(async (name, cmd) => {
       const { as: partyNames } = cmd.opts();
-      const { getBootstrap } = await provideEndoClient(
-        'cli',
-        sockPath,
-        cancelled,
-      );
-      try {
-        const bootstrap = getBootstrap();
-        let party = E(bootstrap).host();
-        for (const partyName of partyNames) {
-          party = E(party).provide(partyName);
-        }
-        const newGuest = await E(party).provideGuest(name);
-        console.log(newGuest);
-      } catch (error) {
-        console.error(error);
-        cancel(error);
-      }
+      const { mkguest } = await import('./mkguest.js');
+      return mkguest({ cancel, cancelled, sockPath, name, partyNames });
     });
 
   program
