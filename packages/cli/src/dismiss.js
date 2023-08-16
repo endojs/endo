3d8ee2c996e7a3b987df9e9073acfe697286120d
Kris Kowal <kris@agoric.com>
2023-08-15 17:25:44 -0700

    feat(cli): Add dismiss message command

---
+++ packages/cli/src/dismiss.js
@@ -0,0 +1,18 @@
+/* global process */
+import os from 'os';
+import { E } from '@endo/far';
+import { withEndoParty } from './context.js';
+
+export const dismissCommand = async ({
+  cancel,
+  cancelled,
+  sockPath,
+  messageNumberText,
+  message,
+  partyNames,
+}) =>
+  withEndoParty(partyNames, { os, process }, async ({ party }) => {
+    // TODO less bad number parsing.
+    const messageNumber = Number(messageNumberText);
+    await E(party).dismiss(messageNumber);
+  });
