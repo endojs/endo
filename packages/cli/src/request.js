6d95690c5697e7521c96ae8a5baead6fbea8a7a2
Kris Kowal <kris@agoric.com>
2023-08-17 11:20:20 -0700

    feat(cli): Generalize request method so it is suitable for the host

--- packages/cli/src/request.js
+++ packages/cli/src/request.js
@@ -8,20 +8,12 @@ export const request = async ({
   cancelled,
   sockPath,
   description,
+  toName,
   resultName,
   partyNames,
 }) => {
-  if (partyNames.length === 0) {
-    console.error('Specify the name of a guest with -a or --as <guest>');
-    process.exitCode = 1;
-    return;
-  }
-  // The last party name must be a guest and will be created if not already
-  // present.
-  const lastPartyName = partyNames.pop();
   await withEndoParty(partyNames, { os, process }, async ({ party }) => {
-    const guest = E(party).provideGuest(lastPartyName);
-    const result = await E(guest).request(description, resultName);
+    const result = await E(party).request(toName, description, resultName);
     console.log(result);
   });
 };
