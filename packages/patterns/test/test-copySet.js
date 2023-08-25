cc34881b4f68a414394c5ab591ea9f871e0e1688
Richard Gibson <richard.gibson@gmail.com>
2023-08-25 11:40:36 -0400

    test(patterns): Improve copySet and copyBag testing

--- packages/patterns/test/test-copySet.js
+++ packages/patterns/test/test-copySet.js
@@ -1,7 +1,12 @@
 import { test } from './prepare-test-env-ava.js';
 // eslint-disable-next-line import/order
-import { makeTagged } from '@endo/marshal';
-import { makeCopySet } from '../src/keys/checkKey.js';
+import { makeTagged, getTag, passStyleOf } from '@endo/marshal';
+import {
+  isCopySet,
+  assertCopySet,
+  makeCopySet,
+  getCopySetKeys,
+} from '../src/keys/checkKey.js';
 import {
   setIsSuperset,
   setIsDisjoint,
@@ -14,7 +19,73 @@ import { M, matches } from '../src/patterns/patternMatchers.js';
 
 import '../src/types.js';
 
-test('operations on copySets', t => {
+const assertIsCopySet = (t, s) => {
+  t.is(passStyleOf(s), 'tagged');
+  t.is(getTag(s), 'copySet');
+  t.notThrows(() => assertCopySet(s));
+  t.true(isCopySet(s));
+};
+
+const assertIsInvalidCopySet = (t, s, message) => {
+  t.is(passStyleOf(s), 'tagged');
+  t.is(getTag(s), 'copySet');
+  t.throws(() => assertCopySet(s), { message });
+  t.is(isCopySet(s), false);
+};
+
+test('makeCopySet', t => {
+  const s = makeCopySet(['z', 'a', 'b', 'c']);
+  assertIsCopySet(t, s);
+  t.deepEqual(
+    getCopySetKeys(s),
+    ['z', 'c', 'b', 'a'],
+    'keys are reverse-sorted',
+  );
+});
+
+test('backwards-compatible static shape', t => {
+  const manualSet = harden(
+    Object.defineProperties(
+      {},
+      {
+        [Symbol.for('passStyle')]: { value: 'tagged' },
+        [Symbol.toStringTag]: { value: 'copySet' },
+        payload: {
+          enumerable: true,
+          value: ['z', 'c', 'b', 'a'],
+        },
+      },
+    ),
+  );
+  assertIsCopySet(t, manualSet);
+  const sets = {
+    makeTagged: makeTagged('copySet', ['z', 'c', 'b', 'a']),
+    makeCopySet: makeCopySet(['z', 'a', 'b', 'c']),
+  };
+  for (const [label, s] of Object.entries(sets)) {
+    t.deepEqual(
+      Object.getOwnPropertyDescriptors(s),
+      Object.getOwnPropertyDescriptors(manualSet),
+      label,
+    );
+  }
+});
+
+test('key uniqueness', t => {
+  t.throws(() => makeCopySet(['a', 'a']), {
+    message: /value has duplicates/,
+  });
+  assertIsInvalidCopySet(
+    t,
+    makeTagged('copySet', ['a', 'a']),
+    /value has duplicates/,
+  );
+});
+
+// TODO: incorporate fast-check for property-based testing that construction
+// reverse rank sorts keys and validation rejects any other key order.
+
+test('operations', t => {
   const x = makeCopySet(['b', 'a', 'c']);
   const y = makeCopySet(['a', 'b']);
   const z = makeCopySet(['c', 'b']);
@@ -46,4 +117,54 @@ test('operations on copySets', t => {
   t.deepEqual(z, makeTagged('copySet', ['c', 'b']));
   t.deepEqual(xMy, makeTagged('copySet', ['c']));
   t.deepEqual(yIz, makeTagged('copySet', ['b']));
+});
+
+test('matching', t => {
+  const copySet = makeCopySet(['z', 'c', 'b', 'a']);
+  const missingKey = makeCopySet(['z', 'c', 'b']);
+  const extraKey = makeCopySet(['z', 'd', 'c', 'b', 'a']);
+
+  const assertNoMatch = sets => {
+    const [[label1, set1], [label2, set2]] = Object.entries(sets);
+    t.is(
+      matches(set1, set2),
+      false,
+      `${label1} specimen must not be matched by ${label2} pattern`,
+    );
+    t.is(
+      matches(set2, set1),
+      false,
+      `${label2} specimen must not be matched by ${label1} pattern`,
+    );
+  };
+  assertNoMatch({
+    'non-empty': copySet,
+    empty: makeCopySet([]),
+  });
+  assertNoMatch({ copySet, missingKey });
+  assertNoMatch({ copySet, extraKey });
+
+  t.true(matches(copySet, copySet), 'matches itself');
+  t.true(
+    matches(copySet, makeTagged('copySet', [...getCopySetKeys(copySet)])),
+    'matches a manually-cloned pattern',
+  );
+
+  t.throws(
+    () =>
+      matches(
+        copySet,
+        makeTagged('copySet', [...getCopySetKeys(copySet)].reverse()),
+      ),
+    { message: /pattern expected/ },
+    'key-reversed pattern is rejected',
+  );
+  t.is(
+    matches(
+      makeTagged('copySet', [...getCopySetKeys(copySet)].reverse()),
+      copySet,
+    ),
+    false,
+    "key-reversed specimen doesn't match",
+  );
 });
