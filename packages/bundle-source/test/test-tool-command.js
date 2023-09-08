62998df29f070f4e8b72aaeec9fb05cc4436a0cb
Kris Kowal <kris@agoric.com>
2023-09-08 12:55:23 -0700

    fix(bundle-source): Reduce test concurrency for CI

--- packages/bundle-source/test/test-tool-command.js
+++ packages/bundle-source/test/test-tool-command.js
@@ -11,7 +11,7 @@ const opts = { cwd };
 
 test('bundle-source command is concurrency safe', async t => {
   const $ = execava(t, opts);
-  const concurrentJobs = Array.from({ length: 10 }).map(() =>
+  const concurrentJobs = Array.from({ length: 5 }).map(() =>
     $`node bin/bundle-source --cache-json bundles demo/circular/a.js circular demo/dir1/index.js dir1`.expect(),
   );
   await Promise.all(concurrentJobs);
