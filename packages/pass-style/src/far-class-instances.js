dd0088ffda0f3ee9992bed4b90c1e2d45234411e
Mark S. Miller <erights@gmail.com>
2023-09-12 20:14:02 -0700

    feat: review suggestions

--- packages/pass-style/src/far-class-instances.js
+++ packages/pass-style/src/far-class-instances.js
@@ -1,13 +1,13 @@
 import { Far } from './make-far.js';
 
 /**
- * Classes whose instances should be Far objects should inherit from
- * this convenient base class. Note that the constructor of this base class
+ * Classes whose instances should be Far objects may find it convenient to
+ * inherit from this base class. Note that the constructor of this base class
  * freezes the instance in an empty state, so all is interesting attributes
  * can only depend on its identity and what it inherits from.
  * This includes private fields, as those are keyed only on
  * this object's identity. However, we discourage (but cannot prevent) such
- * use of private fields, as they cannot easily be refactored into exo state.
+ * use of private fields, as they cannot easily be refactored into Exo state.
  */
 export const FarBaseClass = class FarBaseClass {
   constructor() {
