82d21fe6b144424e39207d0b1e2fb1a1b5e75c2f
kumavis <aaron@kumavis.me>
2023-12-06 08:51:11 -1000

    feat(familiar-chat): cleanup demo script

--- packages/familiar-chat/src/demo.sh
+++ packages/familiar-chat/src/demo.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+set -e
+
+# fresh start
 endo reset
 
+# make profiles
 endo mkguest alice
 endo mkguest bob
 
+# install 1kce game
 (cd ../1kce/ && src/start.sh)
 
 # demo ux hack
@@ -11,4 +17,5 @@ endo mkguest guest-deck
 endo send guest-deck @alice
 endo adopt 0 alice --as guest-deck
 
+# open chat ui
 endo open familiar-chat ./index.js --powers SELF
