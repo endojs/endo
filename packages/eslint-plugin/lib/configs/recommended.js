15b543ace415c8c5848bbf50aac758bf94d4ce09
Kris Kowal <kris@agoric.com>
2023-12-18 16:05:43 -0800

    feat(eslint-plugin)!: Disallow ?? and ?. operators

--- packages/eslint-plugin/lib/configs/recommended.js
+++ packages/eslint-plugin/lib/configs/recommended.js
@@ -65,5 +65,10 @@ module.exports = {
   rules: {
     '@endo/assert-fail-as-throw': 'error',
     'guard-for-in': 'error',
+    // Agoric still uses Endo dependencies under an emulation of ESM we call RESM
+    // because it is invoked with `node -r esm`.
+    // RESM does not support ?? nor ?. operators, so we must avoid them expressly.
+    '@endo/no-optional-chaining': 'error',
+    '@endo/no-nullish-coalescing': 'error',
   },
 };
