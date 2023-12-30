13aab9040b115184614bd52b3afb49e004ae040c
Kris Kowal <kris@agoric.com>
2023-12-29 16:47:54 -0800

    feat(cli): List all, list special

--- packages/cli/src/list.js
+++ packages/cli/src/list.js
@@ -3,12 +3,16 @@ import os from 'os';
 import { E } from '@endo/far';
 import { withEndoHost } from './context.js';
 
-export const list = async ({ directoryName }) =>
-  withEndoHost({ os, process }, async ({ party }) => {
+export const list = async ({ directoryName, special, all }) =>
+  withEndoHost({ os, process }, async ({ host: party }) => {
     if (directoryName !== undefined) {
       party = E(party).lookup(directoryName);
     }
-    const petNames = await E(party).list();
+    const petNames = await (() => {
+      if (all) return E(party).listAll();
+      if (special) return E(party).listSpecial();
+      return E(party).list();
+    })();
     for await (const petName of petNames) {
       console.log(petName);
     }
