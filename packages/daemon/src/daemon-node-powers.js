3ed55898b7b00ad31fd54614533cf235fbd5ae17
Kris Kowal <kris@agoric.com>
2024-01-01 22:30:15 -0800

    feat(daemon): Node socket server exposes assigned port

--- packages/daemon/src/daemon-node-powers.js
+++ packages/daemon/src/daemon-node-powers.js
@@ -217,7 +217,12 @@ export const makeSocketPowers = ({ net }) => {
       void writeTo.next({ reader, writer, closed });
     });
 
-    return readFrom;
+    const port = await listening;
+
+    return harden({
+      port,
+      connections: readFrom,
+    });
   };
 
   /**
@@ -230,7 +235,7 @@ export const makeSocketPowers = ({ net }) => {
     serveListener(
       server =>
         new Promise(resolve =>
-          server.listen(port, host, () => resolve(undefined)),
+          server.listen(port, host, () => resolve(server.address().port)),
         ),
       cancelled,
     );
@@ -240,8 +245,8 @@ export const makeSocketPowers = ({ net }) => {
    * @param {string} args.path
    * @param {Promise<never>} args.cancelled
    */
-  const servePath = async ({ path, cancelled }) =>
-    serveListener(server => {
+  const servePath = async ({ path, cancelled }) => {
+    const { connections } = await serveListener(server => {
       return new Promise((resolve, reject) =>
         server.listen({ path }, error => {
           if (error) {
@@ -257,6 +262,8 @@ export const makeSocketPowers = ({ net }) => {
         }),
       );
     }, cancelled);
+    return connections;
+  };
 
   return { servePort, servePath };
 };
