96f637c8689876c3b56fbad8486a0c5a0734dbc8
Turadg Aleahmad <turadg@agoric.com>
2024-01-09 10:35:22 -0800

    type makeTagged

---
+++ packages/pass-style/src/types.test-d.ts
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import { expectType } from 'tsd';
+import { Far } from './make-far';
+import { passStyleOf } from './passStyleOf';
+import { makeTagged } from './makeTagged';
+import { CopyTagged } from './types';
+
+const remotable = Far('foo', {});
+
+const copyTagged = makeTagged('someTag', remotable);
+expectType<CopyTagged<'someTag', typeof remotable>>(copyTagged);
+
+expectType<'undefined'>(passStyleOf(undefined));
+expectType<'string'>(passStyleOf('str'));
+expectType<'boolean'>(passStyleOf(true));
+expectType<'number'>(passStyleOf(1));
+expectType<'bigint'>(passStyleOf(1n));
+expectType<'symbol'>(passStyleOf(Symbol.for('foo')));
+expectType<'null'>(passStyleOf(null));
+expectType<'promise'>(passStyleOf(Promise.resolve()));
+expectType<'error'>(passStyleOf(new Error()));
+expectType<'tagged'>(passStyleOf(copyTagged));
+expectType<'copyArray'>(passStyleOf([]));
+expectType<'copyRecord'>(passStyleOf({}));
+expectType<'remotable'>(passStyleOf(remotable));
