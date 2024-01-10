c2d956ae3d0e8c51cd262fe77d2e2dc3811342d1
Dan Connolly <dckc@madmode.com>
2024-01-09 22:18:07 -0600

    chore(marshal): types JSDoc -> ts (WIP)
    superfluous? looks like patterns is where I meant to do this

--- packages/marshal/src/types.d.ts
+++ packages/marshal/src/types.d.ts
@@ -1,49 +1,35 @@
-// @ts-check
+/* eslint-disable no-use-before-define */
 export {};
 
-/**
- * @template Slot
- * @callback ConvertValToSlot
- * @param {import("@endo/pass-style").PassableCap} val
- * @returns {Slot}
- */
+export type ConvertValToSlot<Slot> = (
+  val: import('@endo/pass-style').PassableCap,
+) => Slot;
 
-/**
- * @template Slot
- * @callback ConvertSlotToVal
- * @param {Slot} slot
- * @param {string} [iface]
- * @returns {import("@endo/pass-style").PassableCap}
- */
+export type ConvertSlotToVal<Slot> = (
+  slot: Slot,
+  iface?: string,
+) => import('@endo/pass-style').PassableCap;
 
-/**
- * @template T
- * @typedef {{ '@qclass': T }} EncodingClass
- */
+export type EncodingClass<T> = { '@qclass': T };
 
-/**
- * @typedef {EncodingClass<'NaN'> |
- *           EncodingClass<'undefined'> |
- *           EncodingClass<'Infinity'> |
- *           EncodingClass<'-Infinity'> |
- *           EncodingClass<'bigint'> & { digits: string } |
- *           EncodingClass<'@@asyncIterator'> |
- *           EncodingClass<'symbol'> & { name: string } |
- *           EncodingClass<'error'> & { name: string,
- *                                      message: string,
- *                                      errorId?: string
- *           } |
- *           EncodingClass<'slot'> & { index: number,
- *                                     iface?: string
- *           } |
- *           EncodingClass<'hilbert'> & { original: Encoding,
- *                                        rest?: Encoding
- *           } |
- *           EncodingClass<'tagged'> & { tag: string,
- *                                       payload: Encoding
- *           }
- * } EncodingUnion
- *
+export type EncodingUnion =
+  | EncodingClass<'NaN'>
+  | EncodingClass<'undefined'>
+  | EncodingClass<'Infinity'>
+  | EncodingClass<'-Infinity'>
+  | (EncodingClass<'bigint'> & { digits: string })
+  | EncodingClass<'@@asyncIterator'>
+  | (EncodingClass<'symbol'> & { name: string })
+  | (EncodingClass<'error'> & {
+      name: string;
+      message: string;
+      errorId?: string;
+    })
+  | (EncodingClass<'slot'> & { index: number; iface?: string })
+  | (EncodingClass<'hilbert'> & { original: Encoding; rest?: Encoding })
+  | (EncodingClass<'tagged'> & { tag: string; payload: Encoding });
+
+/*
  * Note that the '@@asyncIterator' encoding is deprecated. Use 'symbol' instead.
  *
  * The 'hilbert' encoding is a reference to the Hilbert Hotel
@@ -53,18 +39,11 @@ export {};
  * a `rest` record containing all other properties.
  */
 
-/**
- * @typedef {boolean | number | null | string | EncodingUnion} EncodingElement
- */
+export type EncodingElement = boolean | number | null | string | EncodingUnion;
+
+export type TreeOf<T> = T | { [x: PropertyKey]: TreeOf<T> };
 
 /**
- * @template T
- * @typedef {T | { [x: PropertyKey]: TreeOf<T> }} TreeOf
- */
-
-/**
- * @typedef {TreeOf<EncodingElement>} Encoding
- *
  * The JSON-representable structure describing the complete shape and
  * pass-by-copy data of a Passable (i.e., everything except the contents of its
  * PassableCap leafs, which are marshalled into referenced Slots).
@@ -73,80 +52,71 @@ export {};
  * it is disallowed in encoding records and any data that has such a property
  * must instead use the 'hilbert' encoding described above.
  */
+type Encoding = TreeOf<EncodingElement>;
+
+export type CapData<Slot> = {
+  body: string; // A JSON.stringify of an Encoding
+  slots: Slot[]; // slots
+};
 
 /**
- * @template Slot
- * @typedef {object} CapData
- * @property {string} body A JSON.stringify of an Encoding
- * @property {Slot[]} slots
+ * @param val a Passable
  */
+export type ToCapData<Slot> = (val: any) => CapData<Slot>;
 
 /**
- * @template Slot
- * @callback ToCapData
- * @param {any} val a Passable
- * @returns {CapData<Slot>}
+ * @returns a Passable
  */
+export type FromCapData<Slot> = (data: CapData<Slot>) => any;
+
+export type Marshal<Slot> = {
+  serialize: ToCapData<Slot>; // use toCapData
+  unserialize: FromCapData<Slot>; // use fromCapData
+  toCapData: ToCapData<Slot>;
+  fromCapData: FromCapData<Slot>;
+};
+
+export type MakeMarshalOptions = {
+  errorTagging?: 'on' | 'off'; //  controls whether serialized errors
+  //   also carry tagging information, made from `marshalName` and numbers
+  //   generated (currently by counting) starting at `errorIdNum`. The
+  //   `errorTagging` option defaults to `'on'`. Serialized
+  //   errors are also logged to `marshalSaveError` only if tagging is `'on'`.
+  marshalName?: string; //  Used to identify sent errors.
+  errorIdNum?: number; //  Ascending numbers staring from here
+  //   identify the sending of errors relative to this marshal instance.
+  marshalSaveError?: (err: Error) => void; //  If `errorTagging` is
+  //   `'on'`, then errors serialized by this marshal instance are also
+  //   logged by calling `marshalSaveError` *after* `assert.note` associated
+  //   that error with its errorId. Thus, if `marshalSaveError` in turn logs
+  //   to the normal console, which is the default, then the console will
+  //   show that note showing the associated errorId.
+  serializeBodyFormat?: 'capdata' | 'smallcaps';
+  //   Formatting to use in the "body" property in objects returned from
+  //   `serialize`. The body string for each case:
+  //  * 'capdata' - a JSON string, from an encoding of passables
+  //    into JSON, where some values are represented as objects with a
+  //    `'@qclass` property.
+  //  * 'smallcaps' - a JSON string prefixed with `'#'`, which is
+  //    an unambiguous signal since a valid JSON string cannot begin with
+  //    `'#'`.
+};
 
 /**
- * @template Slot
- * @callback FromCapData
- * @param {CapData<Slot>} data
- * @returns {any} a Passable
- */
-
-/**
- * @template Slot
- * @typedef {object} Marshal
- * @property {ToCapData<Slot>} serialize use toCapData
- * @property {FromCapData<Slot>} unserialize use fromCapData
- * @property {ToCapData<Slot>} toCapData
- * @property {FromCapData<Slot>} fromCapData
- */
-
-/**
- * @typedef {object} MakeMarshalOptions
- * @property {'on'|'off'} [errorTagging] controls whether serialized errors
- * also carry tagging information, made from `marshalName` and numbers
- * generated (currently by counting) starting at `errorIdNum`. The
- * `errorTagging` option defaults to `'on'`. Serialized
- * errors are also logged to `marshalSaveError` only if tagging is `'on'`.
- * @property {string=} marshalName Used to identify sent errors.
- * @property {number=} errorIdNum Ascending numbers staring from here
- * identify the sending of errors relative to this marshal instance.
- * @property {(err: Error) => void=} marshalSaveError If `errorTagging` is
- * `'on'`, then errors serialized by this marshal instance are also
- * logged by calling `marshalSaveError` *after* `assert.note` associated
- * that error with its errorId. Thus, if `marshalSaveError` in turn logs
- * to the normal console, which is the default, then the console will
- * show that note showing the associated errorId.
- * @property {'capdata'|'smallcaps'} [serializeBodyFormat]
- * Formatting to use in the "body" property in objects returned from
- * `serialize`. The body string for each case:
- *    * 'capdata' - a JSON string, from an encoding of passables
- *      into JSON, where some values are represented as objects with a
- *      `'@qclass` property.
- *    * 'smallcaps' - a JSON string prefixed with `'#'`, which is
- *      an unambiguous signal since a valid JSON string cannot begin with
- *      `'#'`.
- */
-
-/**
- * @typedef {[string, string]} RankCover
  * RankCover represents the inclusive lower bound and *inclusive* upper bound
  * of a string-comparison range that covers all possible encodings for
  * a set of values.
  */
+export type RankCover = [string, string];
 
 /**
- * @typedef {-1 | 0 | 1} RankComparison
  * The result of a `RankCompare` function that defines a rank-order, i.e.,
  * a total preorder in which different elements are always comparable but
  * can be tied for the same rank. See `RankCompare`.
  */
+export type RankComparison = -1 | 0 | 1;
 
 /**
- * @callback RankCompare
  * Returns `-1`, `0`, or `1` depending on whether the rank of `left`
  * is respectively before, tied-with, or after the rank of `right`.
  *
@@ -176,9 +146,9 @@ export {};
  * @param {import("@endo/pass-style").Passable} right
  * @returns {RankComparison}
  */
+export type RankCompare = (left: any, right: any) => RankComparison;
 
 /**
- * @typedef {RankCompare} FullCompare
  * A `FullCompare` function satisfies all the invariants stated below for
  * `RankCompare`'s relation with KeyCompare.
  * In addition, its equality is as precise as the `KeyCompare`
@@ -192,3 +162,4 @@ export {};
  * weird, as some taggeds will be considered keys and other taggeds will be
  * considered non-keys.
  */
+export type FullCompare = RankCompare;
