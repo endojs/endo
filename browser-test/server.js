93a0968ed1b20105df4cf7660c651b9bc21ddce5
naugtur <naugtur@gmail.com>
2024-01-17 13:34:52 +0100

    test(ses): add a smoke test for SES lockdown in chrome dev, rework browser-tests to simplify setup and empower testing

---
+++ browser-test/server.js
@@ -0,0 +1,30 @@
+// @ts-check
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    return res.end('<!doctype html><script src="ses.umd.js"></script>');
+  }
+  if (req.url === '/ses.umd.js') {
+    res.writeHead(200, {
+      // MUST HAVE for SES to work: charset=utf-8
+      'Content-Type': 'text/javascript; charset=utf-8',
+    });
+    return res.end(
+      fs.readFileSync(
+        path.join(__dirname, '..', 'packages', 'ses', 'dist', 'ses.umd.js'),
+        'utf-8',
+      ),
+    );
+  }
+  console.warn('404', req.url);
+  res.writeHead(404);
+  res.end();
+});
+
+server.listen({ host: '127.0.0.1', port: 3000 });
