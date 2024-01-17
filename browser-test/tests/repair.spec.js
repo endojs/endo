1fa1d6999b0547715d7c0b676cb6daebbdd81eaa
naugtur <naugtur@gmail.com>
2024-01-17 13:34:52 +0100

    test(ses): add a smoke test for SES lockdown in chrome dev, rework browser-tests to simplify setup and empower testing

--- browser-test/tests/repair.spec.js
+++ browser-test/tests/repair.spec.js
@@ -1,20 +1,22 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('bundled-ses lockdown runs to completion', async ({
+test.skip('bundled-ses lockdown is consistent across browsers', async ({
   page,
   browser,
   browserName,
 }) => {
-  page.on('pageerror', error => {
-    console.error(`________________
-> Error in page: ${error.message}
-${error.stack}`);
-  });
   console.log(browserName, browser.version());
   await page.goto(`http://127.0.0.1:3000/`);
   const result = await page.evaluate(() => {
-    lockdown();
+    Object.defineProperty(Object.prototype, 'toString', {
+      value: function () {},
+    });
+    try {
+      lockdown();
+    } catch (e) {
+      return e.message;
+    }
     return 'Pass';
   });
   expect(result).toBe('Pass');
