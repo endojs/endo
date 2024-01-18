1ca9ababaa91f79b6023107a47ccedc4e1729f4a
Christopher Hiller <boneskull@boneskull.com>
2024-01-17 16:42:28 -0800

    feat(ses): export Permits
    This exports the entirety of `src/permits.js` as the `Permits` object via the `tools` export.
    
    We are using this information in `lavamoat-tofu` for SES-compat detection, and we would like to keep it up-to-date with SES.
    
    There are other ways we could accomplish this, but exporting it is the easiest way for `lavamoat-tofu` to consume it.
    
    # Conflicts:
    #	packages/ses/src/permits.js

--- packages/ses/src/permits.js
+++ packages/ses/src/permits.js
@@ -6,10 +6,10 @@ import { arrayPush, arrayForEach } from './commons.js';
 /** @import {GenericErrorConstructor} from '../types.js' */
 
 /**
- * @module Exports {@code permits}, a recursively defined
- * JSON record enumerating all intrinsics and their properties
- * according to ECMA specs.
+ * Exports {@link permitted}, a recursively defined JSON record enumerating all
+ * intrinsics and their properties according to ECMA specs.
  *
+ * @module
  * @author JF Paradis
  * @author Mark S. Miller
  */
