43cf82b35f1238ee747f5a24cab54f68f01b9552
Richard Gibson <richard.gibson@gmail.com>
2024-01-17 21:56:28 -0500

    chore(base64): Benchmark the JS implementation
    ```console
    $ echo "Node.js" && node test/bench-main.js && echo "XS" && ~/.esvu/bin/xs -m test/bench-main.js
    Node.js
    encodes 10775.74909090909 characters per millisecond
    JS encodes 23130.176470588234 characters per millisecond
    decodes 48752.36084452975 bytes per millisecond
    JS decodes 43007.671875 bytes per millisecond
    XS
    encodes 577272.2727272727 characters per millisecond
    JS encodes 1335.143596377749 characters per millisecond
    decodes 396777.0553505535 bytes per millisecond
    JS decodes 1093.7360890302066 bytes per millisecond
    ```

--- packages/base64/test/bench-main.js
+++ packages/base64/test/bench-main.js
@@ -6,14 +6,18 @@
 // executed within each benchmark's deadline.
 
 import { encodeBase64, decodeBase64 } from '../index.js';
+import { jsEncodeBase64 } from '../src/encode.js';
+import { jsDecodeBase64 } from '../src/decode.js';
+
+// eslint-disable-next-line no-undef,no-restricted-globals
+const log = (typeof console !== 'undefined' && console.log) || print;
 
 const timeout = 1000; // ms
 
-const string = new Array(10000)
-  .fill(
-    'there once a rich man from nottingham who tried to cross the river. what a dope, he tripped on a rope. now look at him shiver.',
-  )
-  .join(' ');
+const shortString =
+  'there once a rich man from nottingham who tried to cross the river. what a dope, he tripped on a rope. now look at him shiver.';
+const string = new Array(10000).fill(shortString).join(' ');
+const shortData = encodeBase64(shortString);
 const data = encodeBase64(string);
 
 {
@@ -28,9 +32,28 @@ const data = encodeBase64(string);
   }
   const end = Date.now();
   const duration = end - start;
-  console.log(
+  log(
     'encodes',
     (operations * string.length) / duration,
+    'characters per millisecond',
+  );
+}
+
+{
+  const start = Date.now();
+  const deadline = start + timeout / 2;
+  let operations = 0;
+  for (let n = 1; Date.now() < deadline; n *= 2) {
+    for (let i = 0; i < n; i += 1) {
+      jsEncodeBase64(shortString);
+    }
+    operations += n;
+  }
+  const end = Date.now();
+  const duration = end - start;
+  log(
+    'JS encodes',
+    (operations * shortString.length) / duration,
     'characters per millisecond',
   );
 }
@@ -47,9 +70,28 @@ const data = encodeBase64(string);
   }
   const end = Date.now();
   const duration = end - start;
-  console.log(
+  log(
     'decodes',
     (operations * data.length) / duration,
+    'bytes per millisecond',
+  );
+}
+
+{
+  const start = Date.now();
+  const deadline = start + timeout / 2;
+  let operations = 0;
+  for (let n = 1; Date.now() < deadline; n *= 2) {
+    for (let i = 0; i < n; i += 1) {
+      jsDecodeBase64(shortData);
+    }
+    operations += n;
+  }
+  const end = Date.now();
+  const duration = end - start;
+  log(
+    'JS decodes',
+    (operations * shortData.length) / duration,
     'bytes per millisecond',
   );
 }
