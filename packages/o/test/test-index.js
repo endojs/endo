016c5927da5ba7d51196466cbd355847109d0635
Michael FIG <mfig@agoric.com>
2024-02-07 20:40:17 -0600

    test(o): break into subtests

--- packages/o/test/test-index.js
+++ packages/o/test/test-index.js
@@ -1,12 +1,19 @@
 import { test } from './prepare-test-env-ava.js';
-import { prepareOCell } from '../index.js';
+import { prepareOCell, stripFunction } from '../index.js';
 
-test('basics', async t => {
+test.before('setup O', t => {
   const makeOCell = prepareOCell(null);
-  const O = makeOCell(makeOCell);
-  t.true(await O('hello foo bar').slice(6).endsWith('bar'));
+  t.context.O = makeOCell(stripFunction(obj => makeOCell(obj)));
+});
 
+test('slice endsWith', async t => {
+  const { O } = t.context;
+  t.true(await O('hello foo bar').slice(6).endsWith('bar'));
+});
+
+test('no sync exceptions', async t => {
   // Basic test that we don't fail synchronously.
+  const { O } = t.context;
   const retp = O.help.me.out.please;
 
   // Promise methods are known.
@@ -22,16 +29,22 @@ test('basics', async t => {
   t.truthy(z);
 
   t.is(await z, undefined);
+});
 
+test('non-function', async t => {
+  const { O } = t.context;
   await t.throwsAsync(
     () => {
       return O.abc.def.ghi().jkl;
     },
     {
-      message: /Cannot apply non-function "ghi"/,
+      message: /target has no method "ghi"/,
     },
   );
+});
 
+test('this binding', async t => {
+  const { O } = t.context;
   const fn = O({
     myGuy() {
       return 23;
