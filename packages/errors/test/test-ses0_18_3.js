f9e5f480378ba6e216cdfed2227539c262abaaf4
Kris Kowal <kris@agoric.com>
2024-02-20 23:18:25 -0800

    test(errors): Add failing backward compat test

---
+++ packages/errors/test/test-ses0_18_3.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+import 'ses0_18_3';
+
+test.failing('shim bare on ses 0.18.3', async t => {
+  // SES v0.18.3 predates the `bare` export.
+  const namespace = await import('../index.js');
+  t.is(namespace.bare, namespace.quote);
+});
