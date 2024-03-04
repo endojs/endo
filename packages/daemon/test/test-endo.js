f7ecd4eac578af20276d6ed7ffb9a3074a55c4f6
kumavis <aaron@kumavis.me>
2024-03-04 09:48:25 -1000

    test(daemon): test loopback network

--- packages/daemon/test/test-endo.js
+++ packages/daemon/test/test-endo.js
@@ -1215,3 +1215,49 @@ test('guest cannot access host methods', async t => {
   const revealedTarget = await E.get(guestsHost).targetFormulaIdentifier;
   t.is(revealedTarget, undefined);
 });
+
+test('loopback network', async t => {
+  const { promise: cancelled, reject: cancel } = makePromiseKit();
+  t.teardown(() => cancel(Error('teardown')));
+  const locator = makeLocator('tmp', 'loopback');
+
+  await stop(locator).catch(() => {});
+  await purge(locator);
+  await start(locator);
+
+  const { getBootstrap } = await makeEndoClient(
+    'client',
+    locator.sockPath,
+    cancelled,
+  );
+  const bootstrap = getBootstrap();
+  const host = E(bootstrap).host();
+
+  // Creates a guest "guest" from the inviter side.
+  const { powers } = await E(host).invite('guest');
+  // We manually create the invitation to specify the network.
+  const invitation = {
+    addresses: ['loop:'],
+    powers,
+  };
+  // Stores the received value as "peer".
+  const peerFromAccept = await E(host).accept(invitation, 'peer');
+  const peerByName = await E(host).lookup('peer');
+  t.is(peerFromAccept, peerByName);
+
+  const guestId = await E(host).identify('guest');
+  const peerId = await E(host).identify('peer');
+  t.not(guestId, peerId);
+
+  const guest = await E(host).lookup('guest');
+  const peer = peerFromAccept;
+  await E(guest).copy(['SELF'], ['a']);
+  await E(peer).copy(['SELF'], ['b']);
+  const guestNames = await E(guest).list();
+  const peerNames = await E(peer).list();
+  t.deepEqual(guestNames, peerNames);
+  t.assert(guestNames.includes('a'));
+  t.assert(guestNames.includes('b'));
+
+  await stop(locator);
+});
