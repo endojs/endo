e08f03ef11b27d435daaac1e4c5e683e0c3aac37
kumavis <aaron@kumavis.me>
2024-03-07 11:54:17 -1000

    fix(daemon): remove orphaned remote formula type check

--- packages/daemon/src/pet-store.js
+++ packages/daemon/src/pet-store.js
@@ -7,7 +7,7 @@ const { quote: q } = assert;
 
 const validIdPattern = /^[0-9a-f]{128}$/;
 const validFormulaPattern =
-  /^(?:(?:readable-blob|worker|pet-store|pet-inspector|eval|lookup|make-unconfined|make-bundle|host|guest|handle|peer|remote|loopback-network):[0-9a-f]{128}:[0-9a-f]{128}|web-bundle:[0-9a-f]{32}:[0-9a-f]{128})$/;
+  /^(?:(?:readable-blob|worker|pet-store|pet-inspector|eval|lookup|make-unconfined|make-bundle|host|guest|handle|peer|loopback-network):[0-9a-f]{128}:[0-9a-f]{128}|web-bundle:[0-9a-f]{32}:[0-9a-f]{128})$/;
 
 /**
  * @param {string} formulaIdentifier
