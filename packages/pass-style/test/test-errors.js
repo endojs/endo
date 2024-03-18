68a6bc2efe11c27e95de569c38891f205065375d
Mark S. Miller <erights@gmail.com>
2024-03-18 12:43:20 -0700

    fix(pass-style)!: toPassableError fixed. (is/assert)PassableError removed.

--- packages/pass-style/test/test-errors.js
+++ packages/pass-style/test/test-errors.js
@@ -1,7 +1,12 @@
 /* eslint-disable max-classes-per-file */
 import test from '@endo/ses-ava/prepare-endo.js';
 
-import { passStyleOf } from '../src/passStyleOf.js';
+import { makeError } from '@endo/errors';
+import {
+  passStyleOf,
+  isPassable,
+  toPassableError,
+} from '../src/passStyleOf.js';
 
 test('style of extended errors', t => {
   const e1 = Error('e1');
@@ -22,4 +27,20 @@ test('style of extended errors', t => {
     const a4 = harden(AggregateError([e2, u3], 'a4', { cause: e1 }));
     t.is(passStyleOf(a4), 'error');
   }
+});
+
+test('toPassableError rejects unfrozen errors', t => {
+  const e = makeError('test error');
+  // I include this test because I was recently surprised that the errors
+  // make by `makeError` are not frozen, and therefore not passable.
+  t.false(Object.isFrozen(e));
+  t.false(isPassable(e));
+
+  // toPassableError hardens, and then checks whether the hardened argument
+  // is a passable error.
+  const e2 = toPassableError(e);
+
+  t.is(e, e2);
+  t.true(Object.isFrozen(e));
+  t.true(isPassable(e));
 });
