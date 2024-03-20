eff91d87f26792e278fafd295004cecc13aa7526
Kris Kowal <kris@agoric.com>
2024-03-20 16:52:57 -0700

    fix(bundle-source): Relieve execa dependency

--- packages/bundle-source/test/test-tool-command.js
+++ packages/bundle-source/test/test-tool-command.js
@@ -1,15 +1,50 @@
 import test from '@endo/ses-ava/prepare-endo.js';
 
+import { spawn } from 'child_process';
 import url from 'url';
-import { execava } from './execava.js';
+import Buffer from 'buffer';
 
 const cwd = url.fileURLToPath(new URL('..', import.meta.url));
-const opts = { cwd };
+
+const shellOut = () =>
+  new Promise((resolve, reject) => {
+    const errorChunks = [];
+    const child = spawn(
+      'node',
+      [
+        'bin/bundle-source',
+        '--cache-json',
+        'bundles',
+        'demo/circular/a.js',
+        'circular',
+        'demo/dir1/index.js',
+        'dir1',
+      ],
+      {
+        cwd,
+        stdio: ['inherit', 'inherit', 'pipe'],
+      },
+    );
+    child.on('close', code => {
+      if (code !== 0) {
+        reject(
+          new Error(
+            `Exit code: ${code}\nError output: ${new TextDecoder().decode(
+              Buffer.concat(errorChunks),
+            )}`,
+          ),
+        );
+      } else {
+        resolve(undefined);
+      }
+    });
+    child.stderr.on('data', chunk => {
+      errorChunks.push(chunk);
+    });
+  });
 
 test('bundle-source command is concurrency safe', async t => {
-  const $ = execava(t, opts);
-  const concurrentJobs = Array.from({ length: 5 }).map(() =>
-    $`node bin/bundle-source --cache-json bundles demo/circular/a.js circular demo/dir1/index.js dir1`.expect(),
-  );
+  const concurrentJobs = Array.from({ length: 5 }).map(() => shellOut());
   await Promise.all(concurrentJobs);
+  t.pass();
 });
