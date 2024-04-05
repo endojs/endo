e20c1356f7f5da3cbee3294f008417fca0182212
naugtur <naugtur@gmail.com>
2024-04-05 13:12:12 +0200

    feat(ses): importNowHook PoC

---
+++ packages/ses/src/module-load-sync.js
@@ -0,0 +1,330 @@
+// For brevity, in this file, as in module-link.js, the term "moduleRecord"
+// without qualification means "module compartment record".
+// This is a super-set of the "static module record", that is reusable between
+// compartments with different hooks.
+// The "module compartment record" captures the compartment and overlays the
+// module's "imports" with the more specific "resolvedImports" as inferred from
+// the particular compartment's "resolveHook".
+
+import {
+  ReferenceError,
+  TypeError,
+  Map,
+  arrayJoin,
+  arrayMap,
+  arrayPush,
+  create,
+  freeze,
+  mapGet,
+  mapHas,
+  mapSet,
+  values,
+  weakmapGet,
+} from './commons.js';
+import { assert } from './error/assert.js';
+
+const { Fail, details: d, quote: q } = assert;
+
+// `makeAlias` constructs compartment specifier tuples for the `aliases`
+// private field of compartments.
+// These aliases allow a compartment to alias an internal module specifier to a
+// module specifier in an external compartment, and also to create internal
+// aliases.
+// Both are facilitated by the moduleMap Compartment constructor option.
+export const makeAlias = (compartment, specifier) =>
+  freeze({
+    compartment,
+    specifier,
+  });
+
+// `resolveAll` pre-computes resolutions of all imports within the compartment
+// in which a module was loaded.
+const resolveAll = (imports, resolveHook, fullReferrerSpecifier) => {
+  const resolvedImports = create(null);
+  for (const importSpecifier of imports) {
+    const fullSpecifier = resolveHook(importSpecifier, fullReferrerSpecifier);
+    resolvedImports[importSpecifier] = fullSpecifier;
+  }
+  return freeze(resolvedImports);
+};
+
+const loadRecord = (
+  compartmentPrivateFields,
+  moduleAliases,
+  compartment,
+  moduleSpecifier,
+  staticModuleRecord,
+  moduleLoads,
+  errors,
+  importMeta,
+) => {
+  const { resolveHook, moduleRecords } = weakmapGet(
+    compartmentPrivateFields,
+    compartment,
+  );
+
+  // resolve all imports relative to this referrer module.
+  const resolvedImports = resolveAll(
+    staticModuleRecord.imports,
+    resolveHook,
+    moduleSpecifier,
+  );
+  const moduleRecord = freeze({
+    compartment,
+    staticModuleRecord,
+    moduleSpecifier,
+    resolvedImports,
+    importMeta,
+  });
+
+  // Enqueue jobs to load this module's shallow dependencies.
+  for (const fullSpecifier of values(resolvedImports)) {
+    try {
+      // Behold: recursion.
+      // eslint-disable-next-line no-use-before-define
+      memoizedLoadWithErrorAnnotation(
+        compartmentPrivateFields,
+        moduleAliases,
+        compartment,
+        fullSpecifier,
+        moduleLoads,
+        errors,
+      );
+    } catch (error) {
+      arrayPush(errors, error);
+    }
+  }
+
+  // Memoize.
+  mapSet(moduleRecords, moduleSpecifier, moduleRecord);
+  return moduleRecord;
+};
+
+const loadWithoutErrorAnnotation = (
+  compartmentPrivateFields,
+  moduleAliases,
+  compartment,
+  moduleSpecifier,
+  moduleLoads,
+  errors,
+) => {
+  const { importNowHook, moduleMap, moduleMapHook, moduleRecords } = weakmapGet(
+    compartmentPrivateFields,
+    compartment,
+  );
+
+  // Follow moduleMap, or moduleMapHook if present.
+  let aliasNamespace = moduleMap[moduleSpecifier];
+  if (aliasNamespace === undefined && moduleMapHook !== undefined) {
+    aliasNamespace = moduleMapHook(moduleSpecifier);
+  }
+  if (typeof aliasNamespace === 'string') {
+    // eslint-disable-next-line @endo/no-polymorphic-call
+    assert.fail(
+      d`Cannot map module ${q(moduleSpecifier)} to ${q(
+        aliasNamespace,
+      )} in parent compartment, not yet implemented`,
+      TypeError,
+    );
+  } else if (aliasNamespace !== undefined) {
+    const alias = weakmapGet(moduleAliases, aliasNamespace);
+    if (alias === undefined) {
+      // eslint-disable-next-line @endo/no-polymorphic-call
+      assert.fail(
+        d`Cannot map module ${q(
+          moduleSpecifier,
+        )} because the value is not a module exports namespace, or is from another realm`,
+        ReferenceError,
+      );
+    }
+    // Behold: recursion.
+    // eslint-disable-next-line no-use-before-define
+    const aliasRecord = memoizedLoadWithErrorAnnotation(
+      compartmentPrivateFields,
+      moduleAliases,
+      alias.compartment,
+      alias.specifier,
+      moduleLoads,
+      errors,
+    );
+    mapSet(moduleRecords, moduleSpecifier, aliasRecord);
+    return aliasRecord;
+  }
+
+  if (mapHas(moduleRecords, moduleSpecifier)) {
+    return mapGet(moduleRecords, moduleSpecifier);
+  }
+
+  const staticModuleRecord = importNowHook(moduleSpecifier);
+
+  if (staticModuleRecord === null || typeof staticModuleRecord !== 'object') {
+    Fail`importNowHook must return an object, for module ${q(
+      moduleSpecifier,
+    )} in compartment ${q(compartment.name)}`;
+  }
+
+  // check if record is a RedirectStaticModuleInterface
+  if (staticModuleRecord.specifier !== undefined) {
+    // check if this redirect with an explicit record
+    if (staticModuleRecord.record !== undefined) {
+      // ensure expected record shape
+      if (staticModuleRecord.compartment !== undefined) {
+        throw TypeError(
+          'Cannot redirect to an explicit record with a specified compartment',
+        );
+      }
+      const {
+        compartment: aliasCompartment = compartment,
+        specifier: aliasSpecifier = moduleSpecifier,
+        record: aliasModuleRecord,
+        importMeta,
+      } = staticModuleRecord;
+
+      const aliasRecord = loadRecord(
+        compartmentPrivateFields,
+        moduleAliases,
+        aliasCompartment,
+        aliasSpecifier,
+        aliasModuleRecord,
+        moduleLoads,
+        errors,
+        importMeta,
+      );
+      mapSet(moduleRecords, moduleSpecifier, aliasRecord);
+      return aliasRecord;
+    }
+
+    // check if this redirect with an explicit compartment
+    if (staticModuleRecord.compartment !== undefined) {
+      // ensure expected record shape
+      if (staticModuleRecord.importMeta !== undefined) {
+        throw TypeError(
+          'Cannot redirect to an implicit record with a specified importMeta',
+        );
+      }
+      // Behold: recursion.
+      // eslint-disable-next-line no-use-before-define
+      const aliasRecord = memoizedLoadWithErrorAnnotation(
+        compartmentPrivateFields,
+        moduleAliases,
+        staticModuleRecord.compartment,
+        staticModuleRecord.specifier,
+        moduleLoads,
+        errors,
+      );
+      mapSet(moduleRecords, moduleSpecifier, aliasRecord);
+      return aliasRecord;
+    }
+
+    throw TypeError('Unnexpected RedirectStaticModuleInterface record shape');
+  }
+
+  return loadRecord(
+    compartmentPrivateFields,
+    moduleAliases,
+    compartment,
+    moduleSpecifier,
+    staticModuleRecord,
+    moduleLoads,
+    errors,
+  );
+};
+
+const memoizedLoadWithErrorAnnotation = (
+  compartmentPrivateFields,
+  moduleAliases,
+  compartment,
+  moduleSpecifier,
+  moduleLoads,
+  errors,
+) => {
+  const { name: compartmentName } = weakmapGet(
+    compartmentPrivateFields,
+    compartment,
+  );
+
+  // Prevent data-lock from recursion into branches visited in dependent loads.
+  let compartmentLoading = mapGet(moduleLoads, compartment);
+  if (compartmentLoading === undefined) {
+    compartmentLoading = new Map();
+    mapSet(moduleLoads, compartment, compartmentLoading);
+  }
+  let moduleLoading = mapGet(compartmentLoading, moduleSpecifier);
+  if (moduleLoading !== undefined) {
+    return moduleLoading;
+  }
+
+  try {
+    moduleLoading = loadWithoutErrorAnnotation(
+      compartmentPrivateFields,
+      moduleAliases,
+      compartment,
+      moduleSpecifier,
+      moduleLoads,
+      errors,
+    )
+  } catch (error) {
+    // eslint-disable-next-line @endo/no-polymorphic-call
+    assert.note(
+      error,
+      d`${error.message}, loading ${q(moduleSpecifier)} in compartment ${q(
+        compartmentName,
+      )}`,
+    );
+    throw error;
+  }
+
+  mapSet(compartmentLoading, moduleSpecifier, moduleLoading);
+
+  return moduleLoading;
+};
+
+/*
+ * `loadSnc` synchronously gathers the `StaticModuleRecord`s for a module and its
+ * transitive dependencies.
+ * The module records refer to each other by a reference to the dependency's
+ * compartment and the specifier of the module within its own compartment.
+ * This graph is then ready to be synchronously linked and executed.
+ */
+export const loadSync = (
+  compartmentPrivateFields,
+  moduleAliases,
+  compartment,
+  moduleSpecifier,
+) => {
+  const { name: compartmentName } = weakmapGet(
+    compartmentPrivateFields,
+    compartment,
+  );
+
+  /** @type {Map<object, Map<string, Record<any, any>>>} */
+  const moduleLoads = new Map();
+  /** @type {Array<Error>} */
+  const errors = [];
+
+  try {
+    memoizedLoadWithErrorAnnotation(
+      compartmentPrivateFields,
+      moduleAliases,
+      compartment,
+      moduleSpecifier,
+      moduleLoads,
+      errors,
+    );
+  } catch (error) {
+    arrayPush(errors, error);
+  }
+ 
+  
+  // Throw an aggregate error if there were any errors.
+  if (errors.length > 0) {
+    throw TypeError(
+      `Failed to load module ${q(moduleSpecifier)} in package ${q(
+        compartmentName,
+      )} (${errors.length} underlying failures: ${arrayJoin(
+        arrayMap(errors, error => error.message),
+        ', ',
+      )}`,
+    );
+  }
+};
