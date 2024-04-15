71808b2625d00e012c02fa46d1959728cd7e0b17
Erik Marks <rekmarks@protonmail.com>
2024-04-15 14:36:17 -0700

    feat(daemon): Improve ./types.d.ts
    Adds return values for exported functions in `./types.d.ts`, and removes
    redundant definition of `Config` type.

--- packages/daemon/types.d.ts
+++ packages/daemon/types.d.ts
@@ -1,22 +1,21 @@
+import type { Config, EndoBootstrap } from './src/types.js';
+
 export { makeRefReader, makeRefIterator } from './src/ref-reader.js';
 export { makeReaderRef, makeIteratorRef } from './src/reader-ref.js';
 
-export type Config = {
-  statePath: string;
-  ephemeralStatePath: string;
-  cachePath: string;
-  sockPath: string;
-};
-
-export function start(config?: Config);
-export function stop(config?: Config);
-export function restart(config?: Config);
-export function terminate(config?: Config);
-export function clean(config?: Config);
-export function purge(config?: Config);
+export type { Config };
+export function start(config?: Config): Promise<void>;
+export function stop(config?: Config): Promise<void>;
+export function restart(config?: Config): Promise<void>;
+export function terminate(config?: Config): Promise<void>;
+export function clean(config?: Config): Promise<void>;
+export function purge(config?: Config): Promise<void>;
 export function makeEndoClient<TBootstrap>(
   name: string,
   sockPath: string,
   cancelled: Promise<void>,
   bootstrap?: TBootstrap,
-);
+): Promise<{
+  getBootstrap: () => Promise<EndoBootstrap>;
+  closed: Promise<void>;
+}>;
