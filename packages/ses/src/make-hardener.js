770d56183c7e0328a492853a506d7e4d6c8e98dd
Mark S. Miller <erights@gmail.com>
2024-04-22 12:09:06 -0700

    fixup! adding link to review conversation

--- packages/ses/src/make-hardener.js
+++ packages/ses/src/make-hardener.js
@@ -247,6 +247,7 @@ export const makeHardener = () => {
                     // hazards we think we understand.
                     // @ts-expect-error TS should know FERAL_STACK_GETTER
                     // cannot be `undefined` here.
+                    // See https://github.com/endojs/endo/pull/2232#discussion_r1575179471
                     value: apply(FERAL_STACK_GETTER, obj, []),
                   });
                 }
