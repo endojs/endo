85582f3a2e38a8ce116009e7bb8523162236db40
Christopher Hiller <boneskull@boneskull.com>
2024-04-23 13:45:19 -0700

    feat(evasive-transform): expose evadeCensorSync
    This change replaces [source-map](https://npm.im/source-map) with [source-map-js](https://npm.im/source-map-js), which is a fork of the former.  Crucially, `source-map-js` is a synchronous, pure-JS implementation.
    
    A consequence of this is that `makeLocationUnmapper` is now synchronous.  It is internal, however.
    
    `evadeCensor` now just wraps `evadeCensorSync` in a `Promise`.  Tests have been changed to use `evadeCensorSync` directly.
    
    In the `makeLocationUnmapper` implementation, an assertion for the truthiness of `ast.loc` has been moved _before_ instantiation of `SourceMapConsumer`, where it maybe should have been in the first place.

--- packages/evasive-transform/src/index.js
+++ packages/evasive-transform/src/index.js
@@ -4,12 +4,16 @@
  * @module
  */
 
+/**
+ * @import {TransformedResult} from './generate.js'
+ */
+
 import { transformAst } from './transform-ast.js';
 import { parseAst } from './parse-ast.js';
 import { generate } from './generate.js';
 
 /**
- * Options for {@link evadeCensor}
+ * Options for {@link evadeCensorSync}
  *
  * @typedef EvadeCensorOptions
  * @property {string|import('source-map').RawSourceMap} [sourceMap] - Original source map in JSON string or object form
@@ -31,16 +35,13 @@ import { generate } from './generate.js';
  * @template {EvadeCensorOptions} T
  * @param {string} source - Source code to transform
  * @param {T} [options] - Options for the transform
- * @returns {Promise<import('./generate.js').TransformedResult<T['sourceUrl']>>} Object containing new code and optionally source map object (ready for stringification)
+ * @returns {TransformedResult<T['sourceUrl']>} Object containing new code and optionally source map object (ready for stringification)
  * @public
  */
-export async function evadeCensor(source, options) {
+export function evadeCensorSync(source, options) {
   // TODO Use options ?? {} when resolved:
   // https://github.com/Agoric/agoric-sdk/issues/8671
   const { sourceMap, sourceUrl, useLocationUnmap, sourceType } = options || {};
-
-  // See "Chesterton's Fence"
-  await null;
 
   // Parse the rolled-up chunk with Babel.
   // We are prepared for different module systems.
@@ -52,13 +53,32 @@ export async function evadeCensor(source, options) {
     typeof sourceMap === 'string' ? sourceMap : JSON.stringify(sourceMap);
 
   if (sourceMap && useLocationUnmap) {
-    await transformAst(ast, { sourceMap: sourceMapJson, useLocationUnmap });
+    transformAst(ast, { sourceMap: sourceMapJson, useLocationUnmap });
   } else {
-    await transformAst(ast);
+    transformAst(ast);
   }
 
   if (sourceUrl) {
     return generate(ast, { sourceUrl });
   }
   return generate(ast);
+}
+
+/**
+ * Apply SES censorship evasion transforms on the given code `source`
+ *
+ * If the `sourceUrl` option is provided, the `map` property of the fulfillment
+ * value will be a source map object; otherwise it will be `undefined`.
+ *
+ * If the `sourceMap` option is _not_ provided, the `useLocationUnmap` option
+ * will have no effect.
+ *
+ * @template {EvadeCensorOptions} T
+ * @param {string} source - Source code to transform
+ * @param {T} [options] - Options for the transform
+ * @returns {Promise<TransformedResult<T['sourceUrl']>>} Object containing new code and optionally source map object (ready for stringification)
+ * @public
+ */
+export async function evadeCensor(source, options) {
+  return evadeCensorSync(source, options);
 }
