3b5df290df983b83741e17abb29a8919219bda37
Kris Kowal <kris@agoric.com>
2024-05-08 16:33:17 -0700

    feat(module-source): Specialize for XS native ModuleSource

---
+++ packages/module-source/src-xs/index.js
@@ -0,0 +1,59 @@
+// @ts-check
+/* global globalThis */
+/* eslint-disable @endo/no-nullish-coalescing */
+
+/** @typedef {{ import: string, as?: string, from: string } & { importAllFrom: string, as: string, from: string } & { export: string, as?: string, from?: string } & { exportAllFrom: string, as?: string } & {importFrom: string }} Binding */
+
+/** @param {Binding[]} bindings */
+function* getImports(bindings) {
+  for (const binding of bindings) {
+    if (binding.import !== undefined) {
+      yield binding.from;
+    } else if (binding.importFrom !== undefined) {
+      yield binding.importFrom;
+    } else if (binding.importAllFrom !== undefined) {
+      yield binding.importAllFrom;
+    } else if (binding.exportAllFrom !== undefined) {
+      yield binding.exportAllFrom;
+    }
+  }
+}
+
+/** @param {Binding[]} bindings */
+function* getExports(bindings) {
+  for (const binding of bindings) {
+    if (binding.export !== undefined) {
+      yield binding.as ?? binding.export;
+    }
+  }
+}
+
+/** @param {Binding[]} bindings */
+function* getReexports(bindings) {
+  for (const binding of bindings) {
+    if (binding.exportAllFrom !== undefined) {
+      yield binding.exportAllFrom;
+    }
+  }
+}
+
+const ModuleSource = globalThis.ModuleSource;
+
+Object.defineProperties(
+  ModuleSource.prototype,
+  Object.getOwnPropertyDescriptors({
+    get imports() {
+      return Array.from(new Set(getImports(this.bindings)));
+    },
+
+    get exports() {
+      return Array.from(new Set(getExports(this.bindings)));
+    },
+
+    get reexports() {
+      return Array.from(new Set(getReexports(this.bindings)));
+    },
+  }),
+);
+
+export { ModuleSource };
