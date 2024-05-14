3b8609b2d5b393eca3c98af67bdc58a9fc6db48c
kumavis <aaron@kumavis.me>
2024-05-14 17:27:23 +0900

    fix(1kce): fix make deck + include deck and game bundles

--- packages/1kce/src/weblet.js
+++ packages/1kce/src/weblet.js
@@ -1,16 +1,21 @@
 import { E } from '@endo/far';
 import { makeRefIterator } from '@endo/daemon/ref-reader.js';
 import { make as makeApp } from './ui/index.js';
+import deckBundleJson from '../bundles/bundle-deck.json';
+import gameBundleJson from '../bundles/bundle-game.json';
+import { makeReaderRef } from '@endo/daemon/reader-ref.js';
 
-// no way of resolving relative paths from the weblet
-const projectRootPath = '../1kce/src';
+const textEncoder = new TextEncoder();
+
 const deckName = 'deck';
 const deckGuestName = 'guest-deck';
 const gameGuestName = 'guest-game';
 
-const makeThing = async (powers, importFullPath, resultName, powersName = 'NONE') => {
+const makeBundle = async (powers, bundleJson, resultName, powersName = 'NONE') => {
   const workerName = 'MAIN';
-  if (powersName !== 'NONE') {
+  // prep powers
+  const isBuiltinPowers = powersName.toUpperCase() === powersName
+  if (isBuiltinPowers === false) {
     // Disabling the overwrite of the guest for now
     // I want to use the premade guest bc it already knows alice
     // and can receive her messages
@@ -19,16 +24,29 @@ const makeThing = async (powers, importFullPath, resultName, powersName = 'NONE'
     //   await E(powers).remove(powersName)
     // }
     // make new guest
-    await E(powers).provideGuest(powersName)
+    const powersHandleName = `handle-${powersName}`
+    await E(powers).provideGuest(powersHandleName, {
+      agentName: powersName,
+    })
+    console.log('made guest', powersName)
   }
+  // submit bundle
+  const temporaryBundleName = `tmp-bundle-${Math.floor(Math.random() * 1000000)}`;
+  const bundleText = JSON.stringify(bundleJson);
+  const bundleBytes = textEncoder.encode(bundleText);
+  const readerRef = makeReaderRef([bundleBytes]);
+  await E(powers).storeBlob(readerRef, temporaryBundleName);
+  console.log('stored bundle', temporaryBundleName)
   // make
-  const deck = await E(powers).importUnsafeAndEndow(
-    workerName,
-    importFullPath,
-    powersName,
-    resultName,
-  );
-  return deck
+  await E(powers).makeBundle(workerName, temporaryBundleName, powersName, resultName);
+  console.log('made bundle', resultName)
+  // clean
+  await E(powers).remove(temporaryBundleName);
+  console.log('cleaned up', temporaryBundleName)
+  // get result
+  const result = await E(powers).lookup(resultName);
+  console.log('got result', resultName, result)
+  return result;
 }
 
 export const make = (powers) => {
@@ -65,12 +83,7 @@ export const make = (powers) => {
       return E(powers).lookup(name)
     },
 
-    async makeNewDeck () {
-      const importFullPath = `${projectRootPath}/deck.js`;
-      const resultName = deckName;
-      const powersName = deckGuestName
-      return makeThing(powers, importFullPath, resultName, powersName)
-    },
+    
     async addCardToDeckByName (cardName) {
       await E(powers).send(
         // destination guest
@@ -83,12 +96,17 @@ export const make = (powers) => {
         [cardName],
       );
     },
+    
+    async makeNewDeck () {
+      const resultName = deckName;
+      const powersName = deckGuestName
+      return makeBundle(powers, deckBundleJson, resultName, powersName)
+    },
 
     async makeGame () {
-      const importFullPath = `${projectRootPath}/game.js`;
       const resultName = 'game';
       const powersName = gameGuestName
-      return makeThing(powers, importFullPath, resultName, powersName)
+      return makeBundle(powers, gameBundleJson, resultName, powersName)
     },
   }
 
