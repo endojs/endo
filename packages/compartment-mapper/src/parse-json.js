3680ed18e39ad7724d50248972d53b76476a1acc
Christopher Hiller <boneskull@boneskull.com>
2024-06-12 16:02:56 -0700

    feat(compartment-mapper): support dynamic imports

--- packages/compartment-mapper/src/parse-json.js
+++ packages/compartment-mapper/src/parse-json.js
@@ -2,19 +2,23 @@
 
 // @ts-check
 
+/** @import {Harden} from 'ses' */
+/** @import {ParseFn} from './types.js' */
+/** @import {ParserImplementation} from './types.js' */
+
 import { parseLocatedJson } from './json.js';
 
 /**
  * TypeScript cannot be relied upon to deal with the nuances of Readonly, so we
  * borrow the pass-through type definition of harden here.
  *
- * @type {import('ses').Harden}
+ * @type {Harden}
  */
 const freeze = Object.freeze;
 
 const textDecoder = new TextDecoder();
 
-/** @type {import('./types.js').SyncParseFn} */
+/** @type {ParseFn} */
 export const parseJson = (bytes, _specifier, location, _packageLocation) => {
   const source = textDecoder.decode(bytes);
   const imports = freeze([]);
@@ -36,7 +40,7 @@ export const parseJson = (bytes, _specifier, location, _packageLocation) => {
   };
 };
 
-/** @type {import('./types.js').ParserImplementation} */
+/** @type {ParserImplementation} */
 export default {
   parse: parseJson,
   heuristicImports: false,
