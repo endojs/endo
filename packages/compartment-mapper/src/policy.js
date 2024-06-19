44418040c02c57d76fe6ff8479a7732e7561f392
Christopher Hiller <boneskull@boneskull.com>
2024-06-19 13:36:07 -0700

    chore(compartment-mapper): normalize TS imports in policy.js

--- packages/compartment-mapper/src/policy.js
+++ packages/compartment-mapper/src/policy.js
@@ -3,19 +3,14 @@
 
 // @ts-check
 
-/**
- * @import {
- *  Policy,
- *  PackagePolicy,
- *  AttenuationDefinition,
- *  PackageNamingKit,
- *  DeferredAttenuatorsProvider,
- *  CompartmentDescriptor,
- *  Attenuator,
- *  SomePolicy,
- *  SomePackagePolicy
- * } from './types.js'
- */
+/** @import {Policy} from './types.js' */
+/** @import {PackagePolicy} from './types.js' */
+/** @import {AttenuationDefinition} from './types.js' */
+/** @import {PackageNamingKit} from './types.js' */
+/** @import {DeferredAttenuatorsProvider} from './types.js' */
+/** @import {CompartmentDescriptor} from './types.js' */
+/** @import {Attenuator} from './types.js' */
+/** @import {SomePolicy} from './types.js' */
 
 import {
   getAttenuatorFromDefinition,
