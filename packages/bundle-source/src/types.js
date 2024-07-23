7e516a08f6fa2bf92d22687d5cd52581cd4222cb
Kris Kowal <kris@agoric.com>
2024-07-23 11:54:14 -0700

    refactor(bundle-source): Rename tags to conditions

--- packages/bundle-source/src/types.js
+++ packages/bundle-source/src/types.js
@@ -70,7 +70,7 @@ export {};
  * @property {boolean} [noTransforms] - when true, generates a bundle with the
  * original sources instead of SES-shim specific ESM and CJS. This may become
  * default in a future major version.
- * @property {string[]} [tags] - conditions for package.json conditional
+ * @property {string[]} [conditions] - conditions for package.json conditional
  * exports and imports.
  */
 
