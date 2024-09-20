7948a69f9dd8677a4e3bc4cea3ba44e789536bd0
Dimitris Marlagkoutsos <info@sirtimid.com>
2024-09-20 10:56:51 +0100

    test(cli): Fix failing cli tests (#2453)
    Closes: https://github.com/endojs/endo/issues/2409
    Refs: https://github.com/endojs/endo/pull/2384
    
    ## Description
    
    This PR updates the readme to properly document the `list` cli command
    and fixes the failing tests
    
    
    ### Security Considerations
    
    none
    ### Scaling Considerations
    
    none
    ### Documentation Considerations
    
    none
    ### Testing Considerations
    
    none
    ### Compatibility Considerations
    
    none
    ### Upgrade Considerations
    
    none

--- packages/cli/test/demo/sending-messages.js
+++ packages/cli/test/demo/sending-messages.js
@@ -11,8 +11,8 @@ export const section = async (execa, testLine) => {
     stdout: /^0\. "HOST" sent "Please enjoy this @doubler\."/,
   });
   await testLine(execa`endo adopt --as alice-agent 0 doubler`);
-  await testLine(execa`endo list --as alice-agent`, {
-    stdout: 'doubler',
+  await testLine(execa`endo list alice-agent`, {
+    stdout: /doubler/,
   });
   await testLine(execa`endo dismiss --as alice-agent 0`);
 };
@@ -22,7 +22,7 @@ export const context = {
   setup: async execa => {
     await execa`endo mkguest alice alice-agent`;
     await execa`endo send alice ${'Please enjoy this @doubler.'}`;
-    await execa`endo adopt alice-agent 0 doubler`;
+    await execa`endo adopt --as alice-agent 0 doubler`;
     await execa`endo dismiss --as alice-agent 0`;
   },
 };
