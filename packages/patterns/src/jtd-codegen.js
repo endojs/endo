c09e4fea9f3eefbdfc6ac61e4c466b88d3b944f6
Turadg Aleahmad <turadg@agoric.com>
2024-10-06 20:00:55 -0700

    feat: add support for optional output file in jtd-codegen

--- packages/patterns/src/jtd-codegen.js
+++ packages/patterns/src/jtd-codegen.js
@@ -1,43 +1,19 @@
 #!/usr/bin/env node
+/* eslint-env node */
 
-import fs from 'fs';
-import { convertJTDToPattern } from './jtd-to-pattern.js';
-
-const main = () => {
-  const [,, inputFile] = process.argv;
-
-  if (!inputFile) {
-    console.error('Usage: jtd-codegen <input-file>');
-    process.exit(1);
-  }
-
-  try {
-    const jtdContent = fs.readFileSync(inputFile, 'utf-8');
-    const jtdSchema = JSON.parse(jtdContent);
-    const pattern = convertJTDToPattern(jtdSchema);
-    console.log(JSON.stringify(pattern, null, 2));
-  } catch (error) {
-    console.error('Error:', error.message);
-    process.exit(1);
-  }
-};
-
-main();
-#!/usr/bin/env node
-
-import fs from 'fs/promises';
+import * as fs from 'fs/promises';
 import { convertJTDToPattern } from './jtd-to-pattern.js';
 
 const main = async () => {
   const args = process.argv.slice(2);
-  let inputFile, outputFile;
+  let outputFile;
 
   if (args.length < 1) {
     console.error('Usage: jtd-codegen <input-file> [-o <output-file>]');
     process.exit(1);
   }
 
-  inputFile = args[0];
+  const inputFile = args[0];
   const outputIndex = args.indexOf('-o');
   if (outputIndex !== -1 && args[outputIndex + 1]) {
     outputFile = args[outputIndex + 1];
@@ -47,9 +23,9 @@ const main = async () => {
     const jtdContent = await fs.readFile(inputFile, 'utf-8');
     const jtdSchema = JSON.parse(jtdContent);
     const pattern = convertJTDToPattern(jtdSchema);
-    
+
     const output = JSON.stringify(pattern, null, 2);
-    
+
     if (outputFile) {
       await fs.writeFile(outputFile, output);
       console.log(`Pattern written to ${outputFile}`);
