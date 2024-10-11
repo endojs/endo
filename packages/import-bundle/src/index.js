87e49be569c5797bb81f8d603a6f2e89bb5cf9be
Kris Kowal <kris@agoric.com>
2024-10-11 10:28:42 -0700

    feat(import-bundle): Thread native flag

--- packages/import-bundle/src/index.js
+++ packages/import-bundle/src/index.js
@@ -31,6 +31,7 @@ export async function importBundle(bundle, options = {}, powers = {}) {
     transforms = [],
     inescapableTransforms = [],
     inescapableGlobalProperties = {},
+    __native__ = false,
     expectedSha512 = undefined,
     importHook = undefined,
   } = options;
@@ -82,6 +83,8 @@ export async function importBundle(bundle, options = {}, powers = {}) {
     const { namespace } = await archive['import']({
       globals: endowments,
       __shimTransforms__: transforms,
+      __native__,
+      // @ts-expect-error TS2740 missing properties from type
       Compartment: CompartmentToUse,
       importHook,
     });
