5250e740e233db1881229e3aa9f0257e7be2b295
Mark S. Miller <erights@gmail.com>
2024-12-29 14:26:32 -0800

    feat(ses): sham of non-trapping shim and ponyfill

---
+++ packages/ses/test/sham-non-trapping/non-trapping-shim.test.js
@@ -0,0 +1,29 @@
+// Uses 'ava' rather than @endo/ses-ava to avoid worries about cyclic
+// dependencies. We will need similar tests is higher level packages, in order
+// to test compat with ses and ses-ava.
+import test from 'ava';
+import '../../src/sham-non-trapping/non-trapping-shim.js';
+
+const { freeze, isFrozen } = Object;
+
+test('non-trapping-pony', async t => {
+  const specimen = { foo: 8 };
+
+  const sillyHandler = freeze({
+    get(target, prop, receiver) {
+      return [target, prop, receiver];
+    },
+  });
+
+  const safeProxy = new Proxy(specimen, sillyHandler);
+
+  t.false(Reflect.isNonTrapping(specimen));
+  t.false(isFrozen(specimen));
+  t.deepEqual(safeProxy.foo, [specimen, 'foo', safeProxy]);
+
+  t.true(Reflect.suppressTrapping(specimen));
+
+  t.true(Reflect.isNonTrapping(specimen));
+  t.true(isFrozen(specimen));
+  t.deepEqual(safeProxy.foo, 8);
+});
