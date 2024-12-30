dc9a3eeccb22d7b3222c42c5a85ec10466ab7174
Turadg Aleahmad <turadg@agoric.com>
2024-12-30 12:26:20 -0500

    chore(types): conform 'nat'

--- packages/nat/test/endo.test.js
+++ packages/nat/test/endo.test.js
@@ -5,6 +5,7 @@ import url from 'url';
 import { importLocation } from '@endo/compartment-mapper';
 import { makeReadPowers } from '@endo/compartment-mapper/node-powers.js';
 
+// @ts-expect-error XXX Node interface munging
 const readPowers = makeReadPowers({ fs, url });
 const entry = new URL('../src/index.js', import.meta.url).toString();
 
