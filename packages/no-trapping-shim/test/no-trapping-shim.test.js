70bdc7916ae051d48e4c907ad2c52b8c2a2e4329
Mark S. Miller <erights@gmail.com>
2024-12-29 16:24:13 -0800

    feat(non-trapping-shim): shim of non-trapping integrity trait

---
+++ packages/no-trapping-shim/test/no-trapping-shim.test.js
@@ -0,0 +1,29 @@
+// Uses 'ava' rather than @endo/ses-ava to avoid worries about cyclic
+// dependencies. We will need similar tests is higher level packages, in order
+// to test compat with ses and ses-ava.
+import test from 'ava';
+import '../shim.js';
+
+const { freeze, isFrozen } = Object;
+
+test('no-trapping-pony', async t => {
+  const specimen = { foo: 8 };
+
+  const sillyHandler = freeze({
+    get(target, prop, receiver) {
+      return [target, prop, receiver];
+    },
+  });
+
+  const safeProxy = new Proxy(specimen, sillyHandler);
+
+  t.false(Reflect.isNoTrapping(specimen));
+  t.false(isFrozen(specimen));
+  t.deepEqual(safeProxy.foo, [specimen, 'foo', safeProxy]);
+
+  t.true(Reflect.suppressTrapping(specimen));
+
+  t.true(Reflect.isNoTrapping(specimen));
+  t.true(isFrozen(specimen));
+  t.deepEqual(safeProxy.foo, 8);
+});
