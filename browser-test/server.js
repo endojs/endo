23a404c210073d8ed5941a2383bec0f0e38572a3
naugtur <naugtur@gmail.com>
2025-01-10 13:28:20 +0100

    test(browser): added a test under CSP

--- browser-test/server.js
+++ browser-test/server.js
@@ -3,24 +3,44 @@ const path = require('path');
 const http = require('http');
 const fs = require('fs');
 
+const testPayloadSource = fs.readFileSync(
+  path.join(__dirname, 'inpage.js'),
+  'utf-8',
+);
+const SESSource = fs.readFileSync(
+  path.join(__dirname, '..', 'packages', 'ses', 'dist', 'ses.umd.js'),
+  'utf-8',
+);
+
+const pageSource =
+  '<!doctype html><body><script src="ses.umd.js"></script><script src="test.js"></script></body>';
+
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
+  if (req.url === '/csp') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Security-Policy': "script-src 'self'",
+    });
+    return res.end(pageSource);
+  }
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    return res.end('<!doctype html><script src="ses.umd.js"></script>');
+    return res.end(pageSource);
+  }
+  if (req.url === '/test.js') {
+    res.writeHead(200, {
+      'Content-Type': 'text/javascript',
+    });
+    return res.end(testPayloadSource);
   }
   if (req.url === '/ses.umd.js') {
     res.writeHead(200, {
       // MUST HAVE for SES to work: charset=utf-8
       'Content-Type': 'text/javascript; charset=utf-8',
     });
-    return res.end(
-      fs.readFileSync(
-        path.join(__dirname, '..', 'packages', 'ses', 'dist', 'ses.umd.js'),
-        'utf-8',
-      ),
-    );
+    return res.end(SESSource);
   }
   console.warn('404', req.url);
   res.writeHead(404);
