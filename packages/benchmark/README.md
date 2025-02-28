c0c75e4beb428f246aaaabfca318ca30cfbaf3cf
Muhammad Ahmad <muhammadahmad@Muhammads-MacBook-Pro.local>
2025-02-28 13:11:10 +0500

    refactor(engine installation): Created a separate script to install engines and updated the ci.yml to run that script before yarn test

--- packages/benchmark/README.md
+++ packages/benchmark/README.md
@@ -6,12 +6,6 @@ This package just provides a minimalistic ava-like interface to run benchmark te
 Run the command `yarn test` in the `packages/benchmark` folder.
 
 ## Pre-reqs of running `yarn test`
-### Install [`esvu`](https://www.npmjs.com/package/esvu) globally 
-Select the `v8` and `xs` from CLI so that it can download the binaries of those engines. 
-
-### Install [`eshost-cli`](https://www.npmjs.com/package/eshost) globally.
-Run the following commands to configure the binaries of engines with `eshost`.
-* `eshost --add "v8" d8 "$HOME/.esvu/engines/v8/d8"`
-* `eshost --add "xs" xs "$HOME/.esvu/engines/xs/xst"`
-
-Now you are good to go to run `yarn test`
+Run the command `yarn install-engines` in the `packages/benchmark` folder. This command will install the `eshost` and `esvu`. 
+* `esvu` is used to insall the binaries of `v8` and `xs` engines
+* `eshost` is used to configure the binaries of engines
--- packages/benchmark/run-tests.sh
+++ packages/benchmark/run-tests.sh
@@ -5,41 +5,14 @@ set -e
 echo "yarn version: $(yarn --version)"
 
 are_engines_installed() {
-    [ -d "$HOME/.esvu/engines/xs" ] && [ -d "$HOME/.esvu/engines/v8" ]
+    [ -f "$HOME/.esvu/bin/xs" ] && [ -f "$HOME/.esvu/bin/v8" ]
 }
 
-if are_engines_installed; then
-    echo "Engines already installed. Skipping installation."
-else
-    echo "Installing engines..."
-    INSTALL_OUTPU_XS=$(yarn dlx esvu install xs 2>&1) || INSTALL_STATUS_XS=$?
-    INSTALL_OUTPU_V8=$(yarn dlx esvu install v8 2>&1) || INSTALL_STATUS_V8=$?
+
+if ! are_engines_installed; then
+    echo "xs and/or v8 not found in $HOME/.esvu/bin; please run 'yarn install-engines' to install them."
+    exit 127
 fi
-
-if [ -n "$INSTALL_STATUS_XS" ] || [ -n "$INSTALL_STATUS_V8" ]; then 
-    if are_engines_installed; then
-        echo "Engines installed successfully despite esvu error."
-    else
-        echo "Error installing XS or V8:"
-        echo "$INSTALL_OUTPU_XS"
-        echo "$INSTALL_OUTPU_V8"
-        exit 1
-    fi
-fi
-
-
-echo $(ls -la "$HOME/.esvu/engines")
-echo $(ls -la "$HOME/.esvu/engines/xs")
-echo $(ls -la "$HOME/.esvu/engines/v8")
-
-
-chmod +x "$HOME/.esvu/engines/xs/xst"
-chmod +x "$HOME/.esvu/engines/v8/d8"
-
-yarn eshost --add "xs" xs "$HOME/.esvu/engines/xs/xst"
-yarn eshost --add "v8" d8 "$HOME/.esvu/engines/v8/d8"
-
-yarn eshost --list
 
 yarn rollup -c
 
