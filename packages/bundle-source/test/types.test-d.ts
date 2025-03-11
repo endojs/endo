4d0f616b44c9279fc2a04917205098cf5bd506d7
Turadg Aleahmad <turadg@agoric.com>
2025-03-11 11:20:08 -0700

    test: types

---
+++ packages/bundle-source/test/types.test-d.ts
@@ -0,0 +1,26 @@
+/* eslint-disable  */
+import { expectType } from 'tsd';
+import { type BundleSourceResult } from '../src';
+
+expectType<BundleSourceResult<'endoZipBase64'>>({
+  moduleFormat: 'endoZipBase64',
+  endoZipBase64: '',
+  endoZipBase64Sha512: '',
+});
+
+expectType<BundleSourceResult<'endoZipBase64'>>({
+  moduleFormat: 'endoZipBase64',
+  endoZipBase64: '',
+  endoZipBase64Sha512: '',
+});
+
+expectType<BundleSourceResult<'endoZipBase64'>>({
+  moduleFormat: 'endoZipBase64',
+  endoZipBase64: '',
+  // @ts-expect-error must be string
+  endoZipBase64Sha512: undefined,
+});
+
+// a 'test' bundle is importable but not a BundleSource result
+// @ts-expect-error Type '"test"' does not satisfy the constraint 'ModuleFormat'.
+type TestBundle = BundleSourceResult<'test'>;
