df4755e2b2cbcba70fcb23d74924cd4ba2b15bbb
Christopher Hiller <boneskull@boneskull.com>
2025-04-10 16:38:28 -0700

    fix(compartment-mapper): specifier of module descriptor returned from findRedirect fixed
    `findRedirect`, which is part of `importNowHook`, is called when we encounter a dynamic require where the specifier is an absolute path.
    
    Previous to this change, the `RedirectStaticModuleInterface` (a `ModuleDescriptor`) could contain an absolute path. Absolute paths are not generally assumed by `ses` or `@endo/compartment-mapper`. In the situation where the module referenced in the `RedirectStaticModuleInterface` itself imports _another_ package (via mutual recursion & trampolining), these assumptions were violated and we made an attempt to call `resolve()` (from the `node-module-specifier` module) using its absolute path as the referrer, which is explicitly disallowed by `resolve()`; it throws an exception.
    
    To that end, we need to ensure that whatever `ModuleDescriptor` we return from `importNowHook` with is one where its `specifier` is relative to its package location.
    
    The fact that this nominally "worked" against our tests in both `@endo/compartment-mapper` _and_ `@lavamoat/node` was due to a) chance, and b) lack of a test case against a fixture representing this configuration.
    
    In addition, I removed the `packageLocation` parameter from `findRedirect`'s options bucket since it was serving no practical purpose (as noticed by @naugtur).
    
    I need to add two more tests against fixtures:
    
    1. A fixture which is similar to the configuration mentioned above
    2. A fixture where the dynamically-required file is in a different compartment (which should be subject to policy enforcement)

--- packages/compartment-mapper/src/import-hook.js
+++ packages/compartment-mapper/src/import-hook.js
@@ -96,6 +96,28 @@ const nodejsConventionSearchSuffixes = [
 ];
 
 /**
+ * Computes the relative path to a module from its compartment location (including a leading `./`)
+ *
+ * @param {string} absoluteModuleSpecifier Absolute path (not URL) to a module specifier
+ * @param {string} compartmentLocation Absolute path to compartment location
+ * @returns {string} Redirect static module interface; the `specifier` prop of this value _must_ be a relative path with leading `./`
+ */
+const relativeSpecifier = (moduleSpecifierLocation, location) => {
+  return `./${moduleSpecifierLocation.replace(location, '')}`;
+};
+
+/**
+ * Returns `true` if `absoluteModuleSpecifier` is within the path `compartmentLocation`.
+ * @param {string} absoluteModudeSpecifier Absolute path to module specifier
+ * @param {string} compartmentLocation Absolute path to compartment location
+ * @returns {boolean}
+ */
+const isLocationWithinCompartment = (
+  absoluteModudeSpecifier,
+  compartmentLocation,
+) => absoluteModudeSpecifier.startsWith(compartmentLocation);
+
+/**
  * Given a module specifier which is an absolute path, attempt to match it with
  * an existing compartment; return a {@link RedirectStaticModuleInterface} if found.
  *
@@ -108,12 +130,9 @@ const findRedirect = ({
   compartmentDescriptors,
   compartments,
   absoluteModuleSpecifier,
-  packageLocation,
 }) => {
-  const moduleSpecifierLocation = new URL(
-    absoluteModuleSpecifier,
-    packageLocation,
-  ).href;
+  const moduleSpecifierLocation = new URL(absoluteModuleSpecifier, 'file:')
+    .href;
 
   // a file:// URL string
   let someLocation = new URL('./', moduleSpecifierLocation).href;
@@ -139,7 +158,7 @@ const findRedirect = ({
       // is a dependency of the compartment descriptor
       if (compartmentDescriptor.compartments.has(location)) {
         return {
-          specifier: absoluteModuleSpecifier,
+          specifier: relativeSpecifier(moduleSpecifierLocation, location),
           compartment: compartments[location],
         };
       }
@@ -160,7 +179,7 @@ const findRedirect = ({
           },
         );
         return {
-          specifier: absoluteModuleSpecifier,
+          specifier: relativeSpecifier(moduleSpecifierLocation, location),
           compartment: compartments[location],
         };
       }
@@ -693,7 +712,6 @@ export function makeImportNowHookMaker(
           compartmentDescriptors,
           compartments,
           absoluteModuleSpecifier: moduleSpecifier,
-          packageLocation,
         });
         if (record) {
           return record;
