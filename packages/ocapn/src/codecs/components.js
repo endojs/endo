d7b42eaf256bf6288aea3e2f1aa97620d88ba0ee
kumavis <aaron@kumavis.me>
2025-04-14 21:46:35 -1000

    feat(ocapn): Initial codecs for OCapN messages and Passable

---
+++ packages/ocapn/src/codecs/components.js
@@ -0,0 +1,76 @@
+import { makeRecordUnionCodec } from '../syrup/codec.js';
+import { makeOCapNRecordCodecFromDefinition } from './util.js';
+
+/** @typedef {import('../syrup/codec.js').SyrupCodec} SyrupCodec */
+
+const { freeze } = Object;
+
+/*
+ * OCapN Components are used in both OCapN Messages and Descriptors
+ */
+
+/**
+ * @param {string} expectedLabel
+ * @returns {SyrupCodec}
+ */
+export const makeOCapNSignatureValueComponentCodec = expectedLabel => {
+  const read = syrupReader => {
+    const label = syrupReader.readSelectorAsString();
+    if (label !== expectedLabel) {
+      throw Error(`Expected label ${expectedLabel}, got ${label}`);
+    }
+    const value = syrupReader.readBytestring();
+    return value;
+  };
+  const write = (value, syrupWriter) => {
+    syrupWriter.writeSelector(expectedLabel);
+    syrupWriter.writeBytestring(value);
+  };
+  return freeze({ read, write });
+};
+
+const OCapNSignatureRValue = makeOCapNSignatureValueComponentCodec('r');
+const OCapNSignatureSValue = makeOCapNSignatureValueComponentCodec('s');
+
+export const OCapNSignature = makeOCapNRecordCodecFromDefinition('sig-val', [
+  ['scheme', 'selector'],
+  ['r', OCapNSignatureRValue],
+  ['s', OCapNSignatureSValue],
+]);
+
+export const OCapNNode = makeOCapNRecordCodecFromDefinition('ocapn-node', [
+  ['transport', 'selector'],
+  ['address', 'bytestring'],
+  ['hints', 'boolean'],
+]);
+
+export const OCapNSturdyRef = makeOCapNRecordCodecFromDefinition(
+  'ocapn-sturdyref',
+  [
+    ['node', OCapNNode],
+    ['swissNum', 'string'],
+  ],
+);
+
+export const OCapNPublicKey = makeOCapNRecordCodecFromDefinition('public-key', [
+  ['scheme', 'selector'],
+  ['curve', 'selector'],
+  ['flags', 'selector'],
+  ['q', 'bytestring'],
+]);
+
+export const OCapNComponentUnionCodec = makeRecordUnionCodec({
+  OCapNNode,
+  OCapNSturdyRef,
+  OCapNPublicKey,
+  OCapNSignature,
+});
+
+export const readOCapComponent = syrupReader => {
+  return OCapNComponentUnionCodec.read(syrupReader);
+};
+
+export const writeOCapComponent = (component, syrupWriter) => {
+  OCapNComponentUnionCodec.write(component, syrupWriter);
+  return syrupWriter.getBytes();
+};
--- packages/ocapn/test/index.test.js
+++ packages/ocapn/test/index.test.js
@@ -1,5 +1,88 @@
+// @ts-check
+
 import test from '@endo/ses-ava/prepare-endo.js';
 
-test('placeholder', async t => {
-  t.fail('TODO: add tests');
+import { makeSyrupReader } from '../src/syrup/decode.js';
+import { makeSyrupWriter } from '../src/syrup/encode.js';
+import { OCapNComponentUnionCodec } from '../src/codecs/components.js';
+import { OCapNDescriptorUnionCodec } from '../src/codecs/descriptors.js';
+import { OCapNMessageUnionCodec } from '../src/codecs/operations.js';
+import {
+  componentsTable,
+  descriptorsTable,
+  operationsTable,
+} from './_table.js';
+import { OCapNPassableUnionCodec } from '../src/codecs/passable.js';
+
+const textEncoder = new TextEncoder();
+const sym = s => `${s.length}'${s}`;
+
+const testBidirectionally = (t, codec, syrup, value, testName) => {
+  const syrupBytes = textEncoder.encode(syrup);
+  const syrupReader = makeSyrupReader(syrupBytes, { name: testName });
+  let result;
+  t.notThrows(() => {
+    result = codec.read(syrupReader);
+  }, testName);
+  t.deepEqual(result, value, testName);
+  const syrupWriter = makeSyrupWriter();
+  t.notThrows(() => {
+    codec.write(value, syrupWriter);
+  }, testName);
+  const bytes2 = syrupWriter.getBytes();
+  const syrup2 = new TextDecoder().decode(bytes2);
+  t.deepEqual(syrup2, syrup, testName);
+};
+
+test('affirmative component cases', t => {
+  const codec = OCapNComponentUnionCodec;
+  for (const { syrup, value } of componentsTable) {
+    testBidirectionally(t, codec, syrup, value, `for ${JSON.stringify(syrup)}`);
+  }
+});
+
+test('affirmative descriptor cases', t => {
+  const codec = OCapNDescriptorUnionCodec;
+  for (const { syrup, value } of descriptorsTable) {
+    testBidirectionally(t, codec, syrup, value, `for ${JSON.stringify(syrup)}`);
+  }
+});
+
+test('affirmative operation cases', t => {
+  const codec = OCapNMessageUnionCodec;
+  for (const { syrup, value } of operationsTable) {
+    testBidirectionally(t, codec, syrup, value, `for ${JSON.stringify(syrup)}`);
+  }
+});
+
+test('error on unknown record type in passable', t => {
+  const codec = OCapNPassableUnionCodec;
+  const syrup = `<${sym('unknown-record-type')}>`;
+  const syrupBytes = textEncoder.encode(syrup);
+  const syrupReader = makeSyrupReader(syrupBytes, {
+    name: 'unknown record type',
+  });
+  t.throws(
+    () => {
+      codec.read(syrupReader);
+    },
+    { message: 'Unexpected record type: "unknown-record-type"' },
+  );
+});
+
+test('descriptor fails with negative integer', t => {
+  const codec = OCapNDescriptorUnionCodec;
+  const syrup = `<${sym('desc:import-object')}1-}>`;
+  const syrupBytes = textEncoder.encode(syrup);
+  const syrupReader = makeSyrupReader(syrupBytes, {
+    name: 'import-object with negative integer',
+  });
+  t.throws(
+    () => {
+      codec.read(syrupReader);
+    },
+    {
+      message: 'PositiveIntegerCodec: value must be positive',
+    },
+  );
 });
