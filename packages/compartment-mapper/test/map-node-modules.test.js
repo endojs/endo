a2dcb938c4ce37c8fc0fdf0496882e2dddcd7d7e
Christopher Hiller <boneskull@boneskull.com>
2025-04-22 15:06:30 -0700

    fix(compartment-mapper): implement recursive shortest-path-updating fix

--- packages/compartment-mapper/test/map-node-modules.test.js
+++ packages/compartment-mapper/test/map-node-modules.test.js
@@ -11,18 +11,26 @@ import { makeReadPowers } from '../src/node-powers.js';
  * @import {CompartmentDescriptor} from '../src/types.js'
  */
 
-// since we cannot dynamically define tests in AVA (🤦‍♂️), this test will run in a loop.
-test(`mapNodeModules() should return compartment descriptor containing shortest path`, async t => {
-  await null;
-  /** @type {string[] | undefined} */
-  let expectedPath;
-  for (let i = 0; i < 100; i++) {
-    t.log(`iteration ${i}`);
+const n = 50;
+for (let i = 0; i < n; i += 1) {
+  test(`mapNodeModules() should return compartment descriptors containing shortest path (iteration ${i}/${n})`, async t => {
+    t.timeout(5000);
+    /** @type {string[] | undefined} */
+    let expectedPath;
     const readPowers = makeReadPowers({ fs, url });
+    const { maybeRead } = readPowers;
+
+    // inserts a random delay before the read
+    readPowers.maybeRead = async specifier => {
+      await scheduler.wait(Math.random() * 50);
+      return maybeRead(specifier);
+    };
+
     const moduleLocation = new URL(
       'fixtures-shortest-path/node_modules/app/index.js',
       import.meta.url,
     ).href;
+
     const compartmentMap = await mapNodeModules(readPowers, moduleLocation);
 
     const compartmentDescriptor = Object.values(
@@ -37,12 +45,16 @@ test(`mapNodeModules() should return compartment descriptor containing shortest 
       t.deepEqual(
         /** @type {CompartmentDescriptor} */ (compartmentDescriptor).path,
         expectedPath,
-        `compartment descriptor should have had path: ${expectedPath.join('>')}`,
+        `compartment descriptor should have had path: ${expectedPath.join('>')} (iteration ${i})`,
       );
     } else {
       expectedPath = /** @type {CompartmentDescriptor} */ (
         compartmentDescriptor
       ).path;
+      t.assert(expectedPath, 'expectedPath should exist');
+      t.log(
+        `Expected path: ${/** @type {string[]} */ (expectedPath).join('>')}`,
+      );
     }
   });
 }
