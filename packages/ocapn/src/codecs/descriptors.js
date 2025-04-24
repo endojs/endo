4f971c561a1757b5e0aa55e2284ac7cce279ee70
kumavis <aaron@kumavis.me>
2025-04-24 13:16:02 -1000

    refactor(ocapn): SyrupRecordDefinition as object

--- packages/ocapn/src/codecs/descriptors.js
+++ packages/ocapn/src/codecs/descriptors.js
@@ -11,66 +11,74 @@ import { OCapNNode, OCapNPublicKey, OCapNSignature } from './components.js';
 export const DescImportObject = makeOCapNRecordCodecFromDefinition(
   'DescImportObject',
   'desc:import-object',
-  [['position', PositiveIntegerCodec]],
+  {
+    position: PositiveIntegerCodec,
+  },
 );
 
 export const DescImportPromise = makeOCapNRecordCodecFromDefinition(
   'DescImportPromise',
   'desc:import-promise',
-  [['position', PositiveIntegerCodec]],
+  {
+    position: PositiveIntegerCodec,
+  },
 );
 
 export const DescExport = makeOCapNRecordCodecFromDefinition(
   'DescExport',
   'desc:export',
-  [['position', PositiveIntegerCodec]],
+  {
+    position: PositiveIntegerCodec,
+  },
 );
 
 export const DescAnswer = makeOCapNRecordCodecFromDefinition(
   'DescAnswer',
   'desc:answer',
-  [['position', PositiveIntegerCodec]],
+  {
+    position: PositiveIntegerCodec,
+  },
 );
 
 export const DescHandoffGive = makeOCapNRecordCodecFromDefinition(
   'DescHandoffGive',
   'desc:handoff-give',
-  [
-    ['receiverKey', OCapNPublicKey],
-    ['exporterLocation', OCapNNode],
-    ['session', 'bytestring'],
-    ['gifterSide', OCapNPublicKey],
-    ['giftId', 'bytestring'],
-  ],
+  {
+    receiverKey: OCapNPublicKey,
+    exporterLocation: OCapNNode,
+    session: 'bytestring',
+    gifterSide: OCapNPublicKey,
+    giftId: 'bytestring',
+  },
 );
 
 export const DescSigGiveEnvelope = makeOCapNRecordCodecFromDefinition(
   'DescSigGiveEnvelope',
   'desc:sig-envelope',
-  [
-    ['object', DescHandoffGive],
-    ['signature', OCapNSignature],
-  ],
+  {
+    object: DescHandoffGive,
+    signature: OCapNSignature,
+  },
 );
 
 export const DescHandoffReceive = makeOCapNRecordCodecFromDefinition(
   'DescHandoffReceive',
   'desc:handoff-receive',
-  [
-    ['receivingSession', 'bytestring'],
-    ['receivingSide', 'bytestring'],
-    ['handoffCount', PositiveIntegerCodec],
-    ['signedGive', DescSigGiveEnvelope],
-  ],
+  {
+    receivingSession: 'bytestring',
+    receivingSide: 'bytestring',
+    handoffCount: PositiveIntegerCodec,
+    signedGive: DescSigGiveEnvelope,
+  },
 );
 
 export const DescSigReceiveEnvelope = makeOCapNRecordCodecFromDefinition(
   'DescSigReceiveEnvelope',
   'desc:sig-envelope',
-  [
-    ['object', DescHandoffReceive],
-    ['signature', OCapNSignature],
-  ],
+  {
+    object: DescHandoffReceive,
+    signature: OCapNSignature,
+  },
 );
 
 // Note: this may only be useful for testing
