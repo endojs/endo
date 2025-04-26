2bac9bed872114c0c289067f1e4445b11aae7bc8
Mark S. Miller <erights@gmail.com>
2025-04-26 12:15:13 -0700

    fixup! self review suggestion

--- packages/marshal/src/marshal-justin.js
+++ packages/marshal/src/marshal-justin.js
@@ -313,10 +313,6 @@ const decodeToJustin = (encoding, shouldIndent = false, slots = []) => {
           assert.typeof(digits, 'string');
           return out.next(`${BigInt(digits)}n`);
         }
-        case '@@asyncIterator': {
-          // TODO deprecated. Eventually remove. TODO now
-          return out.next('Symbol.asyncIterator');
-        }
         case 'symbol': {
           const { name } = rawTree;
           assert.typeof(name, 'string');
