5e27e3300b06e2ea63e8fa13e344993e6cdef397
kumavis <aaron@kumavis.me>
2025-04-29 13:51:17 -1000

    feat(ocapn): OCapN client, with partial python test suite compat

---
+++ packages/ocapn/src/netlayers/types.js
@@ -0,0 +1,55 @@
+// @ts-check
+
+/**
+ * @typedef {import('../codecs/components.js').OCapNLocation} OCapNLocation
+ * @typedef {import('../cryptography.js').OCapNPublicKey} OCapNPublicKey
+ * @typedef {import('../cryptography.js').OCapNSignature} OCapNSignature
+ * @typedef {import('../cryptography.js').OCapNKeyPair} OCapNKeyPair
+ */
+
+/**
+ * @typedef {object} NetLayer
+ * @property {OCapNLocation} location
+ */
+
+/**
+ * @typedef {object} Session
+ * @property {object} tables
+ * @property {Map<string, any>} tables.swissnumTable
+ * @property {Map<bigint, any>} tables.importTable
+ * @property {Map<bigint, any>} tables.exportTable
+ * @property {bigint} tables.exportCount
+ * @property {Map<bigint, Promise<any>>} tables.answerTable
+ * @property {object} peer
+ * @property {OCapNPublicKey} peer.publicKey
+ * @property {OCapNLocation} peer.location
+ * @property {OCapNSignature} peer.locationSignature
+ * @property {object} self
+ * @property {OCapNKeyPair} self.keyPair
+ * @property {OCapNLocation} self.location
+ * @property {OCapNSignature} self.locationSignature
+ */
+
+/**
+ * @typedef {object} Connection
+ * @property {NetLayer} netlayer
+ * @property {Session | undefined} session
+ * @property {(bytes: Uint8Array) => void} write
+ * @property {() => void} end
+ */
+
+/**
+ * @typedef {object} Client
+ * @property {() => Map<string, any>} makeDefaultSwissnumTable
+ * @property {Map<string, Session>} activeSessions
+ * Used to store a session once it's been fully initiated and set up.
+ * Anytime the user needs to open a connection to a new peer, the user will be able
+ * to check this table to see if a connection already exists, permitting
+ * reuse of already established sessions.
+ * @property {Map<string, Session>} outgoingSessions
+ * Used to help the user mitigate the crossed hellos problem
+ * Anytime the user needs to open a connection to a new peer, the user will be able
+ * to check this table to see if a connection already exists, permitting
+ * reuse of already established sessions.
+ * @property {(connection: Connection) => void} handleMessage
+ */
