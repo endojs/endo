b3c9fefab89f4469c06ff2f304f80b773a441a5c
LeoTM <1881059+leotm@users.noreply.github.com>
2025-05-07 13:57:38 +0100

    feat(immutable-arraybuffer): restore pony console warn

--- packages/immutable-arraybuffer/shim-pony.js
+++ packages/immutable-arraybuffer/shim-pony.js
@@ -18,11 +18,11 @@ const arrayBufferMethods = {
   },
 };
 
-// if ('transfer' in arrayBufferPrototype) {
-//   console.warn(
-//     'Could have use the full shim, rather than the limited one for Hermes',
-//   );
-// }
+if ('transfer' in arrayBufferPrototype) {
+  console.warn(
+    'Could have use the full shim, rather than the limited one for Hermes',
+  );
+}
 
 if ('transferToImmutable' in arrayBufferPrototype) {
   // Modern shim practice frowns on conditional installation, at least for
