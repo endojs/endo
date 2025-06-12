0ba095224c46bb777a7ef670973abfef47569bff
Christopher Hiller <boneskull@boneskull.com>
2025-06-12 14:29:51 -0700

    feat(compartment-mapper): support mapping of undiscoverable packages
    This adds the `additionalPackageLocations` and `additionalPackageDetails` options to `mapNodeModules`. The former is a mapping of module location to one or more additional module locations. The latter is a data structure `mapNodeModules` will populate, which can then be provided to `captureFromMap` (which is in turn provided to `makeImportHookMaker()`.
    
    This is intended to be used when dynamic requires prevent `mapNodeModules` & `captureFromMap` from otherwise creating a complete `CompartmentMapDescriptor` due to its ignorance of what would be dynamically required.  Archival is not a use-case.
    
    Also:
    
    - Fixed signature of `chooseModuleDescriptor`
    - Tweaked ESLint config to ignore my new fixture
    - Add options type for `translateGraph`
    - Fixed deprecation notice for `compartmentMapForNodeModules`; it was previously on the options object, but it should only be on the export.
    - `graphPackages` now accepts an optional `Graph` object to be able to re-use the same graph between runs

--- packages/compartment-mapper/test/map-node-modules.test.js
+++ packages/compartment-mapper/test/map-node-modules.test.js
@@ -7,7 +7,7 @@ import test from 'ava';
 import { mapNodeModules } from '../src/node-modules.js';
 import { makeReadPowers } from '../src/node-powers.js';
 /**
- * @import {CompartmentDescriptor, MaybeReadFn} from '../src/types.js'
+ * @import {AdditionalPackageDetailsMap, CompartmentDescriptor, MaybeReadFn, PackageDetails} from '../src/types.js'
  */
 
 const { keys, values } = Object;
@@ -96,4 +96,93 @@ test('mapNodeModules() should not consider peerDependenciesMeta without correspo
   const compartmentMap = await mapNodeModules(readPowers, moduleLocation);
 
   t.is(keys(compartmentMap.compartments).length, 1);
+});
+
+test('mapNodeModules() should accept additional module locations', async t => {
+  const readPowers = makeReadPowers({ fs, url });
+
+  const entryModuleLocation = new URL(
+    'fixtures-additional-modules/node_modules/goofy/index.js',
+    import.meta.url,
+  ).href;
+
+  const additionalModuleLocations = {
+    [entryModuleLocation]: [
+      new URL('fixtures-additional-modules/config.js', import.meta.url).href,
+    ],
+  };
+
+  /** @type {AdditionalPackageDetailsMap} */
+  const additionalPackageDetails = {};
+  const compartmentMap = await mapNodeModules(readPowers, entryModuleLocation, {
+    additionalModuleLocations,
+    additionalPackageDetails,
+  });
+
+  t.is(
+    keys(additionalPackageDetails).length,
+    1,
+    'additionalPackageDetails should contain one entry',
+  );
+
+  for (const [packageLocation, [additionalDetail]] of Object.entries(
+    additionalPackageDetails,
+  )) {
+    t.true(
+      packageLocation.endsWith('goofy/'),
+      'the "goofy" package should contain details about an additional module',
+    );
+    t.true(
+      additionalDetail.packageLocation.endsWith('fixtures-additional-modules/'),
+      'the additional module should refer to the package at the root of the fixture',
+    );
+    t.is(
+      additionalDetail.moduleSpecifier,
+      './config.js',
+      'the additional package details module specifier should be config.js',
+    );
+  }
+
+  const { compartment: entryCompartmentName } = compartmentMap.entry;
+  const entryCompartmentDescriptor =
+    compartmentMap.compartments[entryCompartmentName];
+
+  t.truthy(
+    entryCompartmentName.endsWith(
+      'fixtures-additional-modules/node_modules/goofy/',
+    ),
+    'entry compartment descriptor should correspond to the entry module location package',
+  );
+  t.is(
+    keys(entryCompartmentDescriptor.modules).length,
+    4,
+    'entry compartment descriptor should have no extra modules',
+  );
+
+  t.deepEqual(
+    values(compartmentMap.compartments)
+      .map(compartment => compartment.name)
+      .sort(),
+    ['app', 'pippo', 'goofy', 'gambadilegno', 'paperino'].sort(),
+  );
+
+  // the entry module is the module which should reference the additional module,
+  // which is "app"
+  t.deepEqual(
+    keys(entryCompartmentDescriptor.modules).sort(),
+    ['.', 'app', 'goofy', 'paperino'].sort(),
+    'entry compartment descriptor should reference the expected modules',
+  );
+
+  t.deepEqual(
+    keys(entryCompartmentDescriptor.scopes).sort(),
+    ['app', 'goofy', 'paperino'].sort(),
+    'entry compartment descriptor should reference the expected scopes',
+  );
+
+  t.is(
+    entryCompartmentDescriptor.modules.app.module,
+    './index.js',
+    'entry compartment descriptor should reference the additional package in "app" as index.js',
+  );
 });
