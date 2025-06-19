55d924862640f15be891da442c7a33e0258ae9ea
Dan Connolly <dckc@madmode.com>
2025-06-19 03:11:30 -0500

    test(endo-exec): registry-deposit demo script

--- packages/endo-exec/scripts/registry-deposit.js
+++ packages/endo-exec/scripts/registry-deposit.js
@@ -1,17 +1,32 @@
 #! /usr/bin/env node
+// @ts-check
 import 'endo-exec';
+
+/**
+ * @import {Main} from 'endo-exec';
+ * @import {ImportConfined} from './endo-gogo.js';
+ */
 
 const dbg = x => {
   console.log('@@DEBUG', x);
   return x;
 };
 
-/** @type {import('endo-exec').Main} */
+/** @type {Main<{ importConfined: ImportConfined}>} */
 export const main = async ([_script, amt], _env, { importConfined }) => {
-  const { makeWallet } = await importConfined('./smart-wallet-lib.js');
-  const w1 = makeWallet({ fetch: globalThis.fetch });
-  const offer = { instance: 'registry', proposal: { give: amt } };
-  const seat = w1.executeOffer(offer);
-  const payouts = await seat.getPayouts();
-  console.log({ payouts });
+  const { makeSmartWalletKit } = await importConfined(
+    '@agoric/client-utils/src/smart-wallet-kit.js',
+    () => import('@agoric/client-utils/src/smart-wallet-kit.js'),
+  );
+
+  /** @param {number} ms */
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+  const w1 = makeSmartWalletKit(
+    { fetch: globalThis.fetch, delay },
+    { help: 'me' },
+  );
+  //   const offer = { instance: 'registry', proposal: { give: amt } };
+  //   const seat = w1.executeOffer(offer);
+  //   const payouts = await seat.getPayouts();
+  //   console.log({ payouts });
 };
