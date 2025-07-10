14840f462c55c997e2a06c803f1aeb3d36bbe00e
Kris Kowal <kris@agoric.com>
2025-07-09 17:33:53 -0700

    feat(ses-ava): SES and no-SES test configuration support

---
+++ packages/ses-ava/prepare-endo-config.js
@@ -0,0 +1,3 @@
+// This indirection is necessary because this module exports a function named default
+// and ava calls said function if it exists when used in an ava config.
+import './prepare-endo.js';
