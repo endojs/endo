4e4d2722802b38745c908771abec58fa7db58bda
Mark S. Miller <erights@gmail.com>
2025-08-05 23:05:26 -0700

    feat(errors): self censoring stack frames

---
+++ packages/patterns/test/mismatch-stack-concise-demo.test.js
@@ -0,0 +1,18 @@
+import './_stack-filtering-concise.js';
+import test from '@endo/ses-ava/prepare-endo.js';
+
+import { M, mustMatch } from '../src/patterns/patternMatchers.js';
+
+test('mismatch to see stack', t => {
+  let err;
+  try {
+    mustMatch(
+      harden({ arr: ['foo', ['bar', 'BAR'], ['qux', 'quux']] }),
+      harden({ arr: ['foo', ['bar', 'BAR'], [M.string(), 'qux']] }),
+    );
+  } catch (er) {
+    err = er;
+  }
+  t.log('err', err);
+  t.is(err.name, 'Error');
+});
