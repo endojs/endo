70a1b7b9966d4d408ce4b0168fc765cd1afa3f50
Kris Kowal <kris@agoric.com>
2025-09-30 22:58:04 -0700

    fixup harden
    Co-authored-by: Michael FIG <mfig@agoric.com>

--- packages/harden/test/freezes-after-lockdown.test.js
+++ packages/harden/test/freezes-after-lockdown.test.js
@@ -5,9 +5,12 @@ import harden from '../index.js';
 // This test is framed as a stand-alone module because calling `lockdown` has
 // side-effects on the realm.
 
-test('harden freezes object if locked down', async t => {
+test('harden freezes object if locked down', t => {
+  const { isFrozen: isFrozenPreLockdown } = Object;
   lockdown();
+  const { isFrozen: isFrozenPostLockdown } = Object;
   const object = {};
   harden(object);
-  t.assert(Object.isFrozen(object));
+  t.assert(isFrozenPreLockdown(object));
+  t.assert(isFrozenPostLockdown(object));
 });
