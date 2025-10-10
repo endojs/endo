03b313987d4ac122dc33aea7a57a2483fe14d23a
Kris Kowal <kris@agoric.com>
2025-10-09 19:23:27 -0700

    refactor(harden): Do not export inline commons of make-harden.js

--- packages/harden/make-hardener.js
+++ packages/harden/make-hardener.js
@@ -25,9 +25,8 @@
 // We cannot use globalThis as the local name since it would capture the
 // lexical name.
 const universalThis = globalThis;
-export { universalThis as globalThis };
 
-export const {
+const {
   Array,
   ArrayBuffer,
   Date,
@@ -50,7 +49,7 @@ export const {
   WeakSet,
 } = globalThis;
 
-export const {
+const {
   // The feral Error constructor is safe for internal use, but must not be
   // revealed to post-lockdown code in any compartment including the start
   // compartment since in V8 at least it bears stack inspection capabilities.
@@ -62,7 +61,7 @@ export const {
   AggregateError,
 } = globalThis;
 
-export const {
+const {
   assign,
   create,
   defineProperties,
@@ -85,7 +84,7 @@ export const {
   fromEntries,
 } = Object;
 
-export const {
+const {
   species: speciesSymbol,
   toStringTag: toStringTagSymbol,
   iterator: iteratorSymbol,
@@ -95,14 +94,14 @@ export const {
   for: symbolFor,
 } = Symbol;
 
-export const { isInteger } = Number;
+const { isInteger } = Number;
 
-export const { stringify: stringifyJson } = JSON;
+const { stringify: stringifyJson } = JSON;
 
 // Needed only for the Safari bug workaround below
 const { defineProperty: originalDefineProperty } = Object;
 
-export const defineProperty = (object, prop, descriptor) => {
+const defineProperty = (object, prop, descriptor) => {
   // We used to do the following, until we had to reopen Safari bug
   // https://bugs.webkit.org/show_bug.cgi?id=222538#c17
   // Once this is fixed, we may restore it.
@@ -123,7 +122,7 @@ export const defineProperty = (object, prop, descriptor) => {
   return result;
 };
 
-export const {
+const {
   apply,
   construct,
   get: reflectGet,
@@ -135,27 +134,27 @@ export const {
   set: reflectSet,
 } = Reflect;
 
-export const { isArray, prototype: arrayPrototype } = Array;
-export const { prototype: arrayBufferPrototype } = ArrayBuffer;
-export const { prototype: mapPrototype } = Map;
-export const { revocable: proxyRevocable } = Proxy;
-export const { prototype: regexpPrototype } = RegExp;
-export const { prototype: setPrototype } = Set;
-export const { prototype: stringPrototype } = String;
-export const { prototype: weakmapPrototype } = WeakMap;
-export const { prototype: weaksetPrototype } = WeakSet;
-export const { prototype: functionPrototype } = Function;
-export const { prototype: promisePrototype } = Promise;
-export const { prototype: generatorPrototype } = getPrototypeOf(
+const { isArray, prototype: arrayPrototype } = Array;
+const { prototype: arrayBufferPrototype } = ArrayBuffer;
+const { prototype: mapPrototype } = Map;
+const { revocable: proxyRevocable } = Proxy;
+const { prototype: regexpPrototype } = RegExp;
+const { prototype: setPrototype } = Set;
+const { prototype: stringPrototype } = String;
+const { prototype: weakmapPrototype } = WeakMap;
+const { prototype: weaksetPrototype } = WeakSet;
+const { prototype: functionPrototype } = Function;
+const { prototype: promisePrototype } = Promise;
+const { prototype: generatorPrototype } = getPrototypeOf(
   // eslint-disable-next-line no-empty-function, func-names
   function* () {},
 );
-export const iteratorPrototype = getPrototypeOf(
+const iteratorPrototype = getPrototypeOf(
   // eslint-disable-next-line @endo/no-polymorphic-call
   getPrototypeOf(arrayPrototype.values()),
 );
 
-export const typedArrayPrototype = getPrototypeOf(Uint8Array.prototype);
+const typedArrayPrototype = getPrototypeOf(Uint8Array.prototype);
 
 const { bind } = functionPrototype;
 
@@ -170,7 +169,7 @@ const { bind } = functionPrototype;
  *
  * @type {<F extends (this: any, ...args: any[]) => any>(fn: F) => ((thisArg: ThisParameterType<F>, ...args: Parameters<F>) => ReturnType<F>)}
  */
-export const uncurryThis = bind.bind(bind.call); // eslint-disable-line @endo/no-polymorphic-call
+const uncurryThis = bind.bind(bind.call); // eslint-disable-line @endo/no-polymorphic-call
 
 // See https://github.com/endojs/endo/issues/2930
 if (!('hasOwn' in Object)) {
@@ -192,62 +191,62 @@ if (!('hasOwn' in Object)) {
   });
 }
 
-export const { hasOwn } = Object;
+const { hasOwn } = Object;
 
 /**
  * @deprecated Use `hasOwn` instead
  */
-export const objectHasOwnProperty = hasOwn;
+const objectHasOwnProperty = hasOwn;
 //
-export const arrayFilter = uncurryThis(arrayPrototype.filter);
-export const arrayForEach = uncurryThis(arrayPrototype.forEach);
-export const arrayIncludes = uncurryThis(arrayPrototype.includes);
-export const arrayJoin = uncurryThis(arrayPrototype.join);
+const arrayFilter = uncurryThis(arrayPrototype.filter);
+const arrayForEach = uncurryThis(arrayPrototype.forEach);
+const arrayIncludes = uncurryThis(arrayPrototype.includes);
+const arrayJoin = uncurryThis(arrayPrototype.join);
 /** @type {<T, U>(thisArg: readonly T[], callbackfn: (value: T, index: number, array: T[]) => U, cbThisArg?: any) => U[]} */
-export const arrayMap = /** @type {any} */ (uncurryThis(arrayPrototype.map));
-export const arrayFlatMap = /** @type {any} */ (
+const arrayMap = /** @type {any} */ (uncurryThis(arrayPrototype.map));
+const arrayFlatMap = /** @type {any} */ (
   uncurryThis(arrayPrototype.flatMap)
 );
-export const arrayPop = uncurryThis(arrayPrototype.pop);
+const arrayPop = uncurryThis(arrayPrototype.pop);
 /** @type {<T>(thisArg: T[], ...items: T[]) => number} */
-export const arrayPush = uncurryThis(arrayPrototype.push);
-export const arraySlice = uncurryThis(arrayPrototype.slice);
-export const arraySome = uncurryThis(arrayPrototype.some);
-export const arraySort = uncurryThis(arrayPrototype.sort);
-export const iterateArray = uncurryThis(arrayPrototype[iteratorSymbol]);
+const arrayPush = uncurryThis(arrayPrototype.push);
+const arraySlice = uncurryThis(arrayPrototype.slice);
+const arraySome = uncurryThis(arrayPrototype.some);
+const arraySort = uncurryThis(arrayPrototype.sort);
+const iterateArray = uncurryThis(arrayPrototype[iteratorSymbol]);
 //
-export const arrayBufferSlice = uncurryThis(arrayBufferPrototype.slice);
+const arrayBufferSlice = uncurryThis(arrayBufferPrototype.slice);
 /** @type {(b: ArrayBuffer) => number} */
-export const arrayBufferGetByteLength = uncurryThis(
+const arrayBufferGetByteLength = uncurryThis(
   // @ts-expect-error we know it is there on all conforming platforms
   getOwnPropertyDescriptor(arrayBufferPrototype, 'byteLength').get,
 );
 //
-export const typedArraySet = uncurryThis(typedArrayPrototype.set);
+const typedArraySet = uncurryThis(typedArrayPrototype.set);
 //
-export const mapSet = uncurryThis(mapPrototype.set);
-export const mapGet = uncurryThis(mapPrototype.get);
-export const mapHas = uncurryThis(mapPrototype.has);
-export const mapDelete = uncurryThis(mapPrototype.delete);
-export const mapEntries = uncurryThis(mapPrototype.entries);
-export const iterateMap = uncurryThis(mapPrototype[iteratorSymbol]);
+const mapSet = uncurryThis(mapPrototype.set);
+const mapGet = uncurryThis(mapPrototype.get);
+const mapHas = uncurryThis(mapPrototype.has);
+const mapDelete = uncurryThis(mapPrototype.delete);
+const mapEntries = uncurryThis(mapPrototype.entries);
+const iterateMap = uncurryThis(mapPrototype[iteratorSymbol]);
 //
-export const setAdd = uncurryThis(setPrototype.add);
-export const setDelete = uncurryThis(setPrototype.delete);
-export const setForEach = uncurryThis(setPrototype.forEach);
-export const setHas = uncurryThis(setPrototype.has);
-export const iterateSet = uncurryThis(setPrototype[iteratorSymbol]);
+const setAdd = uncurryThis(setPrototype.add);
+const setDelete = uncurryThis(setPrototype.delete);
+const setForEach = uncurryThis(setPrototype.forEach);
+const setHas = uncurryThis(setPrototype.has);
+const iterateSet = uncurryThis(setPrototype[iteratorSymbol]);
 //
-export const regexpTest = uncurryThis(regexpPrototype.test);
-export const regexpExec = uncurryThis(regexpPrototype.exec);
-export const matchAllRegExp = uncurryThis(regexpPrototype[matchAllSymbol]);
+const regexpTest = uncurryThis(regexpPrototype.test);
+const regexpExec = uncurryThis(regexpPrototype.exec);
+const matchAllRegExp = uncurryThis(regexpPrototype[matchAllSymbol]);
 //
-export const stringEndsWith = uncurryThis(stringPrototype.endsWith);
-export const stringIncludes = uncurryThis(stringPrototype.includes);
-export const stringIndexOf = uncurryThis(stringPrototype.indexOf);
-export const stringMatch = uncurryThis(stringPrototype.match);
-export const generatorNext = uncurryThis(generatorPrototype.next);
-export const generatorThrow = uncurryThis(generatorPrototype.throw);
+const stringEndsWith = uncurryThis(stringPrototype.endsWith);
+const stringIncludes = uncurryThis(stringPrototype.includes);
+const stringIndexOf = uncurryThis(stringPrototype.indexOf);
+const stringMatch = uncurryThis(stringPrototype.match);
+const generatorNext = uncurryThis(generatorPrototype.next);
+const generatorThrow = uncurryThis(generatorPrototype.throw);
 
 /**
  * @type { &
@@ -255,41 +254,41 @@ export const generatorThrow = uncurryThis(generatorPrototype.throw);
  *   ((thisArg: string, searchValue: { [Symbol.replace](string: string, replacer: (substring: string, ...args: any[]) => string): string; }, replacer: (substring: string, ...args: any[]) => string) => string)
  * }
  */
-export const stringReplace = /** @type {any} */ (
+const stringReplace = /** @type {any} */ (
   uncurryThis(stringPrototype.replace)
 );
-export const stringSearch = uncurryThis(stringPrototype.search);
-export const stringSlice = uncurryThis(stringPrototype.slice);
-export const stringSplit =
+const stringSearch = uncurryThis(stringPrototype.search);
+const stringSlice = uncurryThis(stringPrototype.slice);
+const stringSplit =
   /** @type {(thisArg: string, splitter: string | RegExp | { [Symbol.split](string: string, limit?: number): string[]; }, limit?: number) => string[]} */ (
     uncurryThis(stringPrototype.split)
   );
-export const stringStartsWith = uncurryThis(stringPrototype.startsWith);
-export const iterateString = uncurryThis(stringPrototype[iteratorSymbol]);
+const stringStartsWith = uncurryThis(stringPrototype.startsWith);
+const iterateString = uncurryThis(stringPrototype[iteratorSymbol]);
 //
-export const weakmapDelete = uncurryThis(weakmapPrototype.delete);
+const weakmapDelete = uncurryThis(weakmapPrototype.delete);
 /** @type {<K extends {}, V>(thisArg: WeakMap<K, V>, ...args: Parameters<WeakMap<K,V>['get']>) => ReturnType<WeakMap<K,V>['get']>} */
-export const weakmapGet = uncurryThis(weakmapPrototype.get);
-export const weakmapHas = uncurryThis(weakmapPrototype.has);
-export const weakmapSet = uncurryThis(weakmapPrototype.set);
+const weakmapGet = uncurryThis(weakmapPrototype.get);
+const weakmapHas = uncurryThis(weakmapPrototype.has);
+const weakmapSet = uncurryThis(weakmapPrototype.set);
 //
-export const weaksetAdd = uncurryThis(weaksetPrototype.add);
-export const weaksetHas = uncurryThis(weaksetPrototype.has);
+const weaksetAdd = uncurryThis(weaksetPrototype.add);
+const weaksetHas = uncurryThis(weaksetPrototype.has);
 //
-export const functionToString = uncurryThis(functionPrototype.toString);
-export const functionBind = uncurryThis(bind);
+const functionToString = uncurryThis(functionPrototype.toString);
+const functionBind = uncurryThis(bind);
 //
 const { all } = Promise;
-export const promiseAll = promises => apply(all, Promise, [promises]);
-export const promiseCatch = uncurryThis(promisePrototype.catch);
+const promiseAll = promises => apply(all, Promise, [promises]);
+const promiseCatch = uncurryThis(promisePrototype.catch);
 /** @type {<T, TResult1 = T, TResult2 = never>(thisArg: T, onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null) => Promise<TResult1 | TResult2>} */
-export const promiseThen = /** @type {any} */ (
+const promiseThen = /** @type {any} */ (
   uncurryThis(promisePrototype.then)
 );
 //
-export const finalizationRegistryRegister =
+const finalizationRegistryRegister =
   FinalizationRegistry && uncurryThis(FinalizationRegistry.prototype.register);
-export const finalizationRegistryUnregister =
+const finalizationRegistryUnregister =
   FinalizationRegistry &&
   uncurryThis(FinalizationRegistry.prototype.unregister);
 
@@ -299,7 +298,7 @@ export const finalizationRegistryUnregister =
  *
  * @param {Function} fn
  */
-export const getConstructorOf = fn =>
+const getConstructorOf = fn =>
   reflectGet(getPrototypeOf(fn), 'constructor');
 
 /**
@@ -315,7 +314,7 @@ export const getConstructorOf = fn =>
  * | string
  * | symbol)}
  */
-export const isPrimitive = val =>
+const isPrimitive = val =>
   !val || (typeof val !== 'object' && typeof val !== 'function');
 
 /**
@@ -329,38 +328,52 @@ export const isPrimitive = val =>
  *
  * @param {any} value
  */
-export const isError = value => value instanceof FERAL_ERROR;
+const isError = value => value instanceof FERAL_ERROR;
 
 /**
  * @template T
  * @param {T} x
  */
-export const identity = x => x;
+const identity = x => x;
 
 // The original unsafe untamed eval function, which must not escape.
 // Sample at module initialization time, which is before lockdown can
 // repair it.  Use it only to build powerless abstractions.
 // eslint-disable-next-line no-eval
-export const FERAL_EVAL = eval;
+const FERAL_EVAL = eval;
 
 // The original unsafe untamed Function constructor, which must not escape.
 // Sample at module initialization time, which is before lockdown can
 // repair it.  Use it only to build powerless abstractions.
-export const FERAL_FUNCTION = Function;
+const FERAL_FUNCTION = Function;
 
-export const noEvalEvaluate = () => {
+const noEvalEvaluate = () => {
   // See https://github.com/endojs/endo/blob/master/packages/ses/error-codes/SES_NO_EVAL.md
   throw TypeError('Cannot eval with evalTaming set to "no-eval" (SES_NO_EVAL)');
 };
 
 // ////////////////// FERAL_STACK_GETTER FERAL_STACK_SETTER ////////////////////
 
-const er1StackDesc = getOwnPropertyDescriptor(Error('er1'), 'stack');
-const er2StackDesc = getOwnPropertyDescriptor(TypeError('er2'), 'stack');
+// The error repair mechanism is very similar to code in ses/src/commons.js
+// and these implementations should be kept in sync.
+
+const makeTypeError = () => {
+  try {
+    // @ts-expect-error deliberate TypeError
+    null.null;
+    throw TypeError('obligatory'); // To convince the type flow inferrence.
+  } catch (error) {
+    return error;
+  }
+};
+
+const typeErrorStackDesc = getOwnPropertyDescriptor(makeTypeError(), 'stack');
+const errorStackDesc = getOwnPropertyDescriptor(Error('obligatory'), 'stack');
 
 let feralStackGetter;
 let feralStackSetter;
-if (er1StackDesc && er2StackDesc && er1StackDesc.get) {
+
+if (typeErrorStackDesc !== undefined && typeErrorStackDesc.get !== undefined) {
   // We should only encounter this case on v8 because of its problematic
   // error own stack accessor behavior.
   // Note that FF/SpiderMonkey, Moddable/XS, and the error stack proposal
@@ -371,10 +384,11 @@ if (er1StackDesc && er2StackDesc && er1StackDesc.get) {
     // accessor property, but within the same realm, all these accessor
     // properties have the same getter and have the same setter.
     // This is therefore the case that we repair.
-    typeof er1StackDesc.get === 'function' &&
-    er1StackDesc.get === er2StackDesc.get &&
-    typeof er1StackDesc.set === 'function' &&
-    er1StackDesc.set === er2StackDesc.set
+    errorStackDesc !== undefined &&
+    typeof typeErrorStackDesc.get === 'function' &&
+    typeErrorStackDesc.get === errorStackDesc.get &&
+    typeof typeErrorStackDesc.set === 'function' &&
+    typeErrorStackDesc.set === errorStackDesc.set
   ) {
     // Otherwise, we have own stack accessor properties that are outside
     // our expectations, that therefore need to be understood better
@@ -398,7 +412,7 @@ if (er1StackDesc && er2StackDesc && er1StackDesc.get) {
  *
  * @type {(() => any) | undefined}
  */
-export const FERAL_STACK_GETTER = feralStackGetter;
+const FERAL_STACK_GETTER = feralStackGetter;
 
 /**
  * If on a v8 with the problematic error own stack accessor behavior,
@@ -409,7 +423,7 @@ export const FERAL_STACK_GETTER = feralStackGetter;
  *
  * @type {((newValue: any) => void) | undefined}
  */
-export const FERAL_STACK_SETTER = feralStackSetter;
+const FERAL_STACK_SETTER = feralStackSetter;
 
 const getAsyncGeneratorFunctionInstance = () => {
   // Test for async generator function syntax support.
@@ -448,7 +462,7 @@ const getAsyncGeneratorFunctionInstance = () => {
  *
  * @type {AsyncGeneratorFunction | undefined}
  */
-export const AsyncGeneratorFunctionInstance =
+const AsyncGeneratorFunctionInstance =
   getAsyncGeneratorFunctionInstance();
 
 /** @type {(condition: any) => asserts condition} */
@@ -478,7 +492,7 @@ assert(getTypedArrayToStringTag);
  *
  * @param {unknown} object
  */
-export const isTypedArray = object => {
+const isTypedArray = object => {
   // The object must pass a brand check or toStringTag will return undefined.
   const tag = apply(getTypedArrayToStringTag, object, []);
   return tag !== undefined;
