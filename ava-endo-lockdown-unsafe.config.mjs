077fba876a187956f91900e269d8424d6cdcacfe
Kris Kowal <kris@agoric.com>
2025-10-10 12:43:31 -0700

    fixup use node conditions in configs

--- ava-endo-lockdown-unsafe.config.mjs
+++ ava-endo-lockdown-unsafe.config.mjs
@@ -1,8 +1,9 @@
 export default {
-  require: ['@endo/ses-ava/prepare-endo-config.js'],
-  files: ['test/**/*.test.*'],
+  nodeArguments: ['-C', 'ses-ava:endo'],
   environmentVariables: {
     LOCKDOWN_HARDEN_TAMING: 'unsafe',
   },
+  require: ['@endo/ses-ava/prepare-endo-config.js'],
+  files: ['test/**/*.test.*'],
   timeout: '2m',
 };
--- ava-endo-lockdown.config.mjs
+++ ava-endo-lockdown.config.mjs
@@ -1,4 +1,5 @@
 export default {
+  nodeArguments: ['-C', 'ses-ava:endo'],
   require: ['@endo/ses-ava/prepare-endo-config.js'],
   files: ['test/**/*.test.*'],
   timeout: '2m',
