cd47f74678a2c0bb30e58d629aaecca1f95501b5
Kris Kowal <kris@agoric.com>
2025-10-09 17:38:31 -0700

    fixup ses-ava

--- packages/ses-ava/src/command.js
+++ packages/ses-ava/src/command.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-await-in-loop, no-continue */
 
 /* The ses-ava command allows a single package to run the same test suite with
- * multiple configurations named in package.json under `avaConfigs`.
+ * multiple configurations named in package.json under `sesAvaConfigs`.
  * Each of these configurations requires a separate Ava config file like
  * test/_ava.special.config.js and a name.
  * The ses-ava command will by default run all tests in every mode but allows
@@ -19,17 +19,18 @@ import fs from 'node:fs';
 export const main = async () => {
   const descriptorText = await fs.promises.readFile('package.json', 'utf8');
   const descriptor = JSON.parse(descriptorText);
-  const { ava = undefined, avaConfigs = {} } = descriptor;
-  if (ava) avaConfigs.default = undefined;
-  const keys = Object.keys(avaConfigs);
+  const { ava = undefined, sesAvaConfigs = {} } = descriptor;
+  if (ava) sesAvaConfigs.default = undefined;
+  const keys = Object.keys(sesAvaConfigs);
   const all = new Set(keys);
-  const noFlags = new Map(keys.map(key => [`--no-${key}`, key]));
-  const onlyFlags = new Map(keys.map(key => [`--only-${key}`, key]));
+  const noFlags = new Map(keys.map(key => [`--ses-ava-no-${key}`, key]));
+  const onlyFlags = new Map(keys.map(key => [`--ses-ava-only-${key}`, key]));
   const args = [];
   const no = new Set();
   const only = new Set();
   const argsIterator = process.argv.slice(2)[Symbol.iterator]();
 
+  // Parse arguments.
   for (const arg of argsIterator) {
     if (arg === '--') {
       args.push(...argsIterator);
@@ -46,12 +47,44 @@ export const main = async () => {
     }
   }
 
-  for (const config of only.size > 0 ? only : all) {
-    if (no.has(config)) {
-      continue;
+  // Select configurations.
+  const configs = new Set();
+  for (const config of no) {
+    if (only.has(config)) {
+      // Ask not the advice of wizards, for they will say both --no-config- and
+      // --only-config-.
+      throw new Error(
+        `ses-ava cannot respect both --no-config-${config} and --only-config-${config}`,
+      );
     }
+  }
+  for (const config of all) {
+    if (!no.has(config)) {
+      configs.add(config);
+    }
+  }
+  if (configs.size === 0) {
+    throw new Error(
+      `ses-ava requires at least one Ava configuration, either "ava" or "sesAvaConfigs" in package.json`,
+    );
+  }
+
+  for (const config of configs) {
+    // Ava silently proceeds with default configuration if the given --config
+    // file does not exist.
+    // I know, right?
+    const configFile = sesAvaConfigs[config];
+    if (configFile !== undefined && !fs.existsSync(configFile)) {
+      throw new Error(
+        `ses-ava config ${config} file does not exist: ${sesAvaConfigs[config]}`,
+      );
+    }
+  }
+
+  // Execute configurations serially.
+  for (const config of configs) {
     const avaArgs = [
-      ...(avaConfigs[config] ? ['--config', avaConfigs[config]] : []),
+      ...(sesAvaConfigs[config] ? ['--config', sesAvaConfigs[config]] : []),
       ...args,
     ];
     const child = popen.spawn('ava', avaArgs, {
