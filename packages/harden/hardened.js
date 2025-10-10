dcf0a699892ced9b3f199fbead151dfd64005208
Kris Kowal <kris@agoric.com>
2025-10-10 07:10:30 -0700

    feat(harden): Presumed hardened mode

---
+++ packages/harden/hardened.js
@@ -0,0 +1,24 @@
+/* This implementation of harden asserts that there is a harden implementation
+ * on the global or shared intrinsics as provided by a valid HardenedJS
+ * environment.
+ * Select this implementation of @endo/harden with -C hardened
+ * with tools like Node.js or Endo's bundle-source bundler.
+ */
+
+/* global globalThis */
+
+const harden = globalThis.harden ?? Object[Symbol.for('harden')];
+
+if (harden === undefined) {
+  throw new Error(
+    'Cannot initialize @endo/harden. This program was initialized with the "hardened" condition (-C hardened) but not executed in a hardened JavaScript environment',
+  );
+}
+
+export default harden;
+
+/** @param {object} object */
+export const isFrozenIfLockdown = object => {
+  return Object.isFrozen(object);
+};
+harden(isFrozenIfLockdown);
