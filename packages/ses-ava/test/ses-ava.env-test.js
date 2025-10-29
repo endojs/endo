08c6108852643f6e9fec76bb6fa0d0baeb7d819c
Richard Gibson <richard.gibson@gmail.com>
2025-10-28 20:42:18 -0400

    chore(ses-ava): Replace dynamic CLI options with static ones
    Fixes #2998
    
    Replace --no-config-$name and --only-config-$name with --exclude $name
    and --only $name (and respective shorthands -x $name and -o $name).

--- packages/ses-ava/test/ses-ava.env-test.js
+++ packages/ses-ava/test/ses-ava.env-test.js
@@ -3,6 +3,6 @@
 import test from 'ava';
 
 test('ses-ava env', t => {
-  // ses-ava --only-config-* flag sees this test and the corresponding config
+  // ses-ava `--only env` flag sees this test and the corresponding config
   t.is(process.env.SES_AVA, '1');
 });
--- packages/ses-ava/test/ses-ava.must-not-test.js
+++ packages/ses-ava/test/ses-ava.must-not-test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
 
-test('ses-ava --no-config-* prevents this test from being discovered', t => {
-  t.fail('should not be reachable because --no-config-two');
+test('ses-ava --exclude prevents this test from being discovered', t => {
+  t.fail('should not be reachable because --exclude two');
 });
