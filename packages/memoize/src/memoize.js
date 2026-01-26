be47113cf4a3cca718f346e87bc3038d65bc8867
Mark S. Miller <erights@users.noreply.github.com>
2026-01-25 19:18:12 -0800

    fix(memoize): cannot recur through memo with same arg (#3056)
    Closes: #XXXX
    Refs: #3042 
    
    ## Description
    
    Makes the memoize package more robust in two ways:
    - if the `arg` is not WeakKey compatible, it now errors before calling
    the underlying function, not after.
    - if the underlying function recurs by calling its own memoization with
    the same `args`, that causes an error. Otherwise it could have caused an
    infinite regress.
    
    ### Security Considerations
    
    Removes a possibly exploitable vulnerability. Though, even if not fixed,
    this potential vulnerability is highly unlikely to be exploitable.
    
    ### Scaling Considerations
    
    none.
    
    ### Documentation Considerations
    
    I changed the readme to say "before" rather than "after". But I do not
    document anywhere the recursion issue as it is too obscure to call
    anyone's attention to. Rather, it avoids the need to document the
    (previously unnoticed) problem. Likewise, I did not update NEWS.md
    
    ### Testing Considerations
    
    This PR tests these changes well.
    
    ### Compatibility Considerations
    
    In theory, but highly highly unlikely it would actually break anything.
    
    ### Upgrade Considerations
    
    none

--- packages/memoize/src/memoize.js
+++ packages/memoize/src/memoize.js
@@ -1,4 +1,9 @@
 /**
+ * Must not escape this module.
+ */
+const encapsulatedPumpkin = harden({});
+
+/**
  * Given a one-argument function `fn` or a WeakMap-key compatible
  * argument `arg`, returns `memoFn`, a memoizing form of that function
  * that will only call `fn(arg)` for a given `arg` the first time.
@@ -8,18 +13,37 @@
  * See memoize.md for the Memoization Safety properties of `memoize`.
  * (TODO turn into link once there's a URL)
  *
- * @template {{}} A Should be WeakMap-key compatible
- * @template R Can be anything
+ * @template {WeakKey} A
+ * @template R
  * @param {(arg: A) => R} fn
  * @returns {(arg: A) => R}
  */
 export const memoize = fn => {
+  /** @type {WeakMap<A, R>} */
   const memo = new WeakMap();
+  /** @type {(arg: A) => R} */
   const memoFn = arg => {
     if (memo.has(arg)) {
-      return memo.get(arg);
+      // TS doesn't know both that nothing interleavs between the `has` and
+      // the `get`, and that in the absence of interleaving, a `get` on this
+      // branch of `has` must succeed.
+      const memoedResult = /** @type {R} */ (memo.get(arg));
+      if (memoedResult === encapsulatedPumpkin) {
+        throw new TypeError('no recursion through memoization with same arg');
+      }
+      return memoedResult;
     }
-    const result = fn(arg);
+    // This both prevents recursion through memoization,
+    // and errors early on a non-weak-key-compat arg, rather than calling `fn`.
+    memo.set(arg, /** @type {R} */ (encapsulatedPumpkin));
+    let result;
+    try {
+      result = fn(arg);
+    } catch (e) {
+      // if `fn` throws, clear the recursion protection on the way out.
+      memo.delete(arg);
+      throw e;
+    }
     memo.set(arg, result);
     return result;
   };
