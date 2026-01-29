a0fb905898fa49e4d618fb2a761cafa028426bca
Richard Gibson <richard.gibson@gmail.com>
2026-01-29 14:40:11 -0500

    refactor(patterns): Condense `containerHasSplit`

--- packages/patterns/src/patterns/patternMatchers.js
+++ packages/patterns/src/patterns/patternMatchers.js
@@ -1443,56 +1443,46 @@ const makePatternKit = () => {
     const kind = kindOf(specimen);
     switch (kind) {
       case 'copyArray': {
-        if (
-          !confirmElementsHasSplit(
+        return (
+          confirmElementsHasSplit(
             specimen,
             elementPatt,
             bound,
             reject,
             inResults,
             outResults,
-          )
-        ) {
-          // check logic already performed by confirmContainerHasSplit
-          return false;
-        }
-        return [harden(inResults), harden(outResults)];
+          ) && [harden(inResults), harden(outResults)]
+        );
       }
       case 'copySet': {
-        if (
-          !confirmElementsHasSplit(
+        return (
+          confirmElementsHasSplit(
             specimen.payload,
             elementPatt,
             bound,
             reject,
             inResults,
             outResults,
-          )
-        ) {
-          return false;
-        }
-        return [
-          inResults && makeCopySet(inResults),
-          outResults && makeCopySet(outResults),
-        ];
+          ) && [
+            inResults && makeCopySet(inResults),
+            outResults && makeCopySet(outResults),
+          ]
+        );
       }
       case 'copyBag': {
-        if (
-          !pairsHasSplit(
+        return (
+          pairsHasSplit(
             specimen.payload,
             elementPatt,
             bound,
             reject,
             inResults,
             outResults,
-          )
-        ) {
-          return false;
-        }
-        return [
-          inResults && makeCopyBag(inResults),
-          outResults && makeCopyBag(outResults),
-        ];
+          ) && [
+            inResults && makeCopyBag(inResults),
+            outResults && makeCopyBag(outResults),
+          ]
+        );
       }
       default: {
         return reject && reject`unexpected ${q(kind)}`;
