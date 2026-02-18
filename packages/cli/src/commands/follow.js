ad023d0ca607e05ca035d7362bc63733f0ef3921
Kris Kowal <kris@agoric.com>
2026-02-17 22:35:12 -0800

    fixup cli

--- packages/cli/src/commands/follow.js
+++ packages/cli/src/commands/follow.js
@@ -2,14 +2,14 @@
 
 import os from 'os';
 import { E } from '@endo/far';
-import { iterateStream } from '@endo/exo-stream/iterate-stream.js';
+import { iterateReader } from '@endo/exo-stream/iterate-reader.js';
 import { withEndoAgent } from '../context.js';
 import { parsePetNamePath } from '../pet-name.js';
 
 export const followCommand = async ({ name, agentNames }) =>
   withEndoAgent(agentNames, { os, process }, async ({ agent }) => {
     const iterable = await E(agent).lookup(...parsePetNamePath(name));
-    for await (const iterand of iterateStream(iterable)) {
+    for await (const iterand of iterateReader(iterable)) {
       console.log(iterand);
     }
   });
