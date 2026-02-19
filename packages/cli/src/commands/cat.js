1d9c1f81543c830abca870207efa31145294acde
Kris Kowal <kris@agoric.com>
2026-02-19 11:53:59 -0800

    fixup migrate cli

--- packages/cli/src/commands/cat.js
+++ packages/cli/src/commands/cat.js
@@ -9,8 +9,7 @@ import { parsePetNamePath } from '../pet-name.js';
 export const cat = async ({ name, agentNames }) =>
   withEndoAgent(agentNames, { os, process }, async ({ agent }) => {
     const readable = await E(agent).lookup(...parsePetNamePath(name));
-    const readerRef = E(readable).streamBase64();
-    const reader = await iterateBytesReader(readerRef);
+    const reader = iterateBytesReader(readable);
     for await (const chunk of reader) {
       process.stdout.write(chunk);
     }
