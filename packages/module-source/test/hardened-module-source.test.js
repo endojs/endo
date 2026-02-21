c058c04118e1358abe43391303aea7b834fb300b
Kris Kowal <kris@agoric.com>
2026-02-21 08:08:14 -0800

    fixup module-source

--- packages/module-source/test/hardened-module-source.test.js
+++ packages/module-source/test/hardened-module-source.test.js
@@ -22,14 +22,17 @@ const assertModuleSourceHardened = (t, record) => {
   );
   t.true(Object.isFrozen(record), 'ModuleSource instance should be frozen');
   t.true(
+    // eslint-disable-next-line no-underscore-dangle
     Object.isFrozen(record.__liveExportMap__),
     'ModuleSource live export map should be frozen',
   );
   t.true(
+    // eslint-disable-next-line no-underscore-dangle
     Object.isFrozen(record.__reexportMap__),
     'ModuleSource reexport map should be frozen',
   );
   t.true(
+    // eslint-disable-next-line no-underscore-dangle
     Object.isFrozen(record.__fixedExportMap__),
     'ModuleSource fixed export map should be frozen',
   );
