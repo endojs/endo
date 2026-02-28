ecea31c00a3c605780e45d660a5af8b0507f03ce
Mark S. Miller <erights@gmail.com>
2026-02-28 00:56:51 -0800

    fixup: @endo/errors no longer pulls in ses

--- packages/errors/test/index.test0-18-3.js
+++ packages/errors/test/index.test0-18-3.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 
-test('shim bare on ses 0.18.3', async t => {
+// I don't think we care anymore about SES v0.18.3
+test.skip('shim bare on ses 0.18.3', async t => {
   // SES v0.18.3 predates the `bare` export.
   const namespace = await import('../index.js');
   t.is(namespace.bare, namespace.quote);
