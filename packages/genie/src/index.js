8bc5bd4f03141f967af23d604e52a436d11e1a58
Joshua T Corbin <jcorbin@wunjo.org>
2026-03-12 14:31:54 -0400

    enhance(genie/agent) stand up around pi-agent-core

--- packages/genie/src/index.js
+++ packages/genie/src/index.js
@@ -2,4 +2,14 @@
  * @endo/genie - Main Entry Point
  */
 
-export { default as buildSystemPrompt } from './system/index.js';
+export {
+  default as makeAgent,
+  makeToolCallStart,
+  makeToolCallEnd,
+  makeMessage,
+  makeError,
+} from './agent/index.js';
+
+export {
+  default as buildSystemPrompt
+} from './system/index.js';
