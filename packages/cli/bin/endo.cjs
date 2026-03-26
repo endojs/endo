0046eaf6e5970bbd62d76c6368543646673f66f2
Turadg Aleahmad <turadg@agoric.com>
2026-03-26 14:12:42 -0700

    feat: import .ts by syntax erasure

--- packages/cli/bin/endo.cjs
+++ packages/cli/bin/endo.cjs
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
+/* global process */
 (async () => {
+  // Keep ts-blank-space until Node 22 is the least supported Node version.
+  await import('ts-blank-space/register');
   const { main } = await import('../src/endo.js');
   await main(process.argv.slice(2));
 })().catch(error => {
