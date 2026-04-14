b521e1b9d08ddd7fee91a4e3a654197b63738e0a
Turadg Aleahmad <turadg@agoric.com>
2026-04-14 10:16:40 -0700

    lint(types): include tests in root typecheck

--- packages/daemon/test/remote-control.test.js
+++ packages/daemon/test/remote-control.test.js
@@ -154,7 +154,7 @@ test('remote control establishes new connection when reconnecting after disconne
     () => makeFakeGateway(),
     cancelBob1,
     bob1Cancelled,
-    disposeBob1,
+    /** @type {() => void} */ (disposeBob1),
   );
   cancelBob1(new Error('Disconnect'));
   await t.throwsAsync(() => bob1Cancelled);
