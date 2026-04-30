0b6a34aa7ef900804a09a0b0e297c7964aee17c0
Christopher Hiller <boneskull@boneskull.com>
2026-04-29 17:32:02 -0700

    feat(compartment-mapper): expose Babel-based CJS parser w/ dynamic import support
    Expose Babel-based CJS parser, `parse-cjs-babel`. Expose shared functionality for wrapping CJS functors with `__dirname`, `__filename`, etc. Add support for dynamic `import()` (`parse-cjs-babel` only).
    
    This also fixes a bug wherein dynamic requires of relative paths from package subdirectories were erroneously being resolved from the package root.

--- packages/compartment-mapper/import-parsers.js
+++ packages/compartment-mapper/import-parsers.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line import/export -- just types
 export * from './src/types-external.js';
 
-export { defaultParserForLanguage } from './src/import-parsers.js';
+export {
+  defaultParserForLanguage,
+  parserForLanguageWithCjsBabel,
+} from './src/import-parsers.js';
